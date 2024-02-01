19c605c0e1a51af3e529f0c7a58d3f19f7342a17
Johnny Luo <johnnyluo1980@gmail.com>
2024-02-01 21:01:09 +1100

    WIP: trying to wire up keygen process

---
+++ voltixApp/VoltixApp/Views/New Wallet/Keygen.swift
@@ -0,0 +1,18 @@
+//
+//  Keygen.swift
+//  VoltixApp
+//
+//  Created by Johnny Luo on 1/2/2024.
+//
+
+import SwiftUI
+
+struct Keygen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Keygen()
+}
