0a66ee78f69916c8bfc06b06476b9a8cbdc1fbb7
Johnny Luo <johnnyluo1980@gmail.com>
2024-02-07 19:42:40 +1100

    update git actions

--- Mediator/Package.swift
+++ Mediator/Package.swift
@@ -1,13 +1,14 @@
-// swift-tools-version: 5.7.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Mediator",
+
     platforms: [
         .macOS(.v12),
-        .iOS(.v14)
+        .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -16,7 +17,7 @@ let package = Package(
             targets: ["Mediator"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/httpswift/swifter", from: "1.5.0")
+        .package(url: "https://github.com/httpswift/swifter", from: "1.5.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -27,5 +28,4 @@ let package = Package(
         .testTarget(
             name: "MediatorTests",
             dependencies: ["Mediator"]),
-    ]
-)
+    ])
