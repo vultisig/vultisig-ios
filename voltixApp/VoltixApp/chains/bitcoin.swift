fd485cf71ce10e688dcb8e6cb1c7d5c555e1ba58
Johnny Luo <johnnyluo1980@gmail.com>
2024-02-13 13:54:21 +1100

    Add bitcoin support 1. Add WalletCore library , the recommended way to use SPM to integreate wallet core 2. Add method to construct a bitcoin transaction with external signed signature

---
+++ voltixApp/VoltixApp/chains/bitcoin.swift
@@ -0,0 +1,91 @@
+//
+//  bitcoin.swift
+//  VoltixApp
+//
+
+import Foundation
+import WalletCore
+
+struct UtxoInfo {
+    let hash: String
+    let amount: Int64
+    let index: UInt32
+}
+
+struct Bitcoin {
+    enum BitcoinTransactionError: String, Error {
+        case invalidPubKey = "invalid public key"
+        case failToGetKeyHash = "fail to get key hash"
+        case invalidSignature = "Signature is invalid"
+    }
+
+    private func getSignedBitcoinTransaction(utxos: [UtxoInfo],
+                                             hexPubKey: String,
+                                             fromAddress: String,
+                                             toAddress: String,
+                                             toAmount: Int64,
+                                             signatureProvider: (Data) -> Data) -> Result<String, Error>
+    {
+        guard let pubkeyData = Data(hexString: hexPubKey) else {
+            return .failure(BitcoinTransactionError.invalidPubKey)
+        }
+        guard let publicKey = PublicKey(data: pubkeyData, type: .secp256k1) else {
+            return .failure(BitcoinTransactionError.invalidPubKey)
+        }
+        do {
+            let coin = CoinType.bitcoin
+            var input = BitcoinSigningInput.with {
+                $0.hashType = BitcoinSigHashType.all.rawValue
+                $0.amount = toAmount
+                $0.toAddress = toAddress
+                $0.changeAddress = fromAddress
+                $0.coinType = coin.rawValue
+                $0.scripts = [String: Data]()
+            }
+            for inputUtxo in utxos {
+                let lockScript = BitcoinScript.lockScriptForAddress(address: fromAddress, coin: .bitcoin)
+                print("lock script: \(lockScript.scriptHash.hexString)")
+                let keyHash = lockScript.matchPayToWitnessPublicKeyHash()
+                guard let keyHash else {
+                    return .failure(BitcoinTransactionError.failToGetKeyHash)
+                }
+                let redeemScript = BitcoinScript.buildPayToPublicKeyHash(hash: keyHash)
+                input.scripts[keyHash.hexString] = redeemScript.data
+                let utxo = BitcoinUnspentTransaction.with {
+                    $0.outPoint = BitcoinOutPoint.with {
+                        $0.hash = Data(hexString: inputUtxo.hash)!
+                        $0.index = inputUtxo.index
+                        $0.sequence = UInt32.max
+                    }
+                    $0.amount = inputUtxo.amount
+                    $0.script = lockScript.data
+                }
+                input.utxo.append(utxo)
+            }
+
+            let inputData = try input.serializedData()
+            let preHashes = TransactionCompiler.preImageHashes(coinType: .bitcoin, txInputData: inputData)
+            let preSignOutputs = try BitcoinPreSigningOutput(serializedData: preHashes)
+
+            let allSignatures = DataVector()
+            let publicKeys = DataVector()
+
+            for h in preSignOutputs.hashPublicKeys {
+                let preImageHash = h.dataHash
+                let pubkeyHash = h.publicKeyHash
+                let signature = signatureProvider(preImageHash)
+                if !publicKey.verifyAsDER(signature: signature, message: preImageHash) {
+                    return .failure(BitcoinTransactionError.invalidSignature)
+                }
+                allSignatures.add(data: signature)
+                publicKeys.add(data: pubkeyData)
+            }
+            let compileWithSignatures = TransactionCompiler.compileWithSignatures(coinType: .bitcoin, txInputData: inputData, signatures: allSignatures, publicKeys: publicKeys)
+            let output = try BitcoinSigningOutput(serializedData: compileWithSignatures)
+            return .success(output.encoded.hexString)
+        } catch {
+            print("fail to construct transaction,err:\(error)")
+            return .failure(error)
+        }
+    }
+}
