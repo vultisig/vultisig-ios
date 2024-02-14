e67eff9f4ed3e100c4612df45c5d0aef86176167
Enrique Souza <enriquesouza@live.com>
2024-02-14 20:16:59 -0300

    [ x ] - Removing #if os(iOS) when I see them [ x ] - Implementing QRCode reader to import wallet [ x ] - Creation of the structure DocumentPicker [ x ] - Implementing the document picker [  x  ] - handling the imported document           [  x  ] - Read the file content           [  x  ] - Fill the text vault text area

--- voltixApp/VoltixApp/Views/New Wallet/ImportWalletView.swift
+++ voltixApp/VoltixApp/Views/New Wallet/ImportWalletView.swift
@@ -1,84 +1,154 @@
 import SwiftUI
+import OSLog
+import CodeScanner
+import UniformTypeIdentifiers
 
+private let logger = Logger(subsystem: "import-wallet", category: "communication")
 struct ImportWalletView: View {
-  @Binding var presentationStack: [CurrentScreen]
-  @State private var vaultText = ""
-
-  var body: some View {
-    GeometryReader { geometry in
-
-      VStack(alignment: .leading) {
-        Spacer().frame(height: 30)
-
-        ZStack(alignment: .bottomTrailing) {
-          TextEditor(text: $vaultText)
-            .font(.custom("AmericanTypewriter", size: geometry.size.width * 0.05))
-            .scrollContentBackground(.hidden)
-            .foregroundColor(.black)
-            .frame(height: geometry.size.height * 0.4)
-            .padding()
-            .background(Color(red: 0.96, green: 0.96, blue: 0.96))
-            .cornerRadius(12)
-
-          HStack {
-            Button(action: {}) {
-              Image(systemName: "camera")
+    @Binding var presentationStack: [CurrentScreen]
+    @State private var vaultText = ""
+    @State private var errorMsg: String = ""
+    @State private var isShowingScanner = false
+    @State private var showingPicker = false
+    @State private var pickedURLs: [URL] = []
+    
+    @State private var isShowingFileImporter = false
+    
+    
+    var body: some View {
+        GeometryReader { geometry in
+            
+            VStack(alignment: .leading) {
+                Spacer().frame(height: 30)
+                
+                ZStack(alignment: .bottomTrailing) {
+                    TextEditor(text: $vaultText)
+                        .font(.custom("AmericanTypewriter", size: geometry.size.width * 0.05))
+                        .scrollContentBackground(.hidden)
+                        .foregroundColor(.black)
+                        .frame(height: geometry.size.height * 0.4)
+                        .padding()
+                        .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                        .cornerRadius(12)
+                    
+                    HStack {
+                        Button("", systemImage: "camera") {
+                            self.isShowingScanner = true
+                        }
+                        .sheet(isPresented: self.$isShowingScanner, content: {
+#if os(iOS)
+                            CodeScannerView(codeTypes: [.qr], completion: self.handleScan)
+#endif
+                        })
+                        .padding(.trailing, 8)
+                        .buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {
+                            isShowingFileImporter = true
+                        }) {
+                            Image(systemName: "doc.text.viewfinder")
+                        }
+                        .padding(.all, 20)
+                        .buttonStyle(PlainButtonStyle())
+                        .fileImporter(
+                            isPresented: $isShowingFileImporter,
+                            allowedContentTypes: [UTType.content], // Adjust based on the file types you want to allow
+                            allowsMultipleSelection: false
+                        ) { result in
+                            switch result {
+                            case .success(let urls):
+                                guard let selectedFileURL = urls.first else { return }
+                                
+                                print(selectedFileURL)
+                                // Read the content of the file
+                                readContent(of: selectedFileURL)
+                            case .failure(let error):
+                                // Handle the error
+                                print("Error selecting file: \(error.localizedDescription)")
+                            }
+                        }                    }
+                }
+                
+                Text("ENTER YOUR PREVIOUSLY CREATED VAULT SHARE")
+                    .font(.system(size: geometry.size.width * 0.04, weight: .medium))
+                    .padding(.top, 8)
+                    .foregroundColor(.black)
+                
+                Spacer()
+                
+                BottomBar(
+                    content: "CONTINUE",
+                    onClick: {
+                        self.presentationStack.append(.newWalletInstructions)
+                    }
+                )
+                .padding(.bottom)
             }
-            .padding(.trailing, 8)
-            .buttonStyle(PlainButtonStyle())
-
-            Button(action: {}) {
-              Image(systemName: "doc.text.viewfinder")
+            .padding([.leading, .trailing], geometry.size.width * 0.05)
+            // Conditionally apply navigationBarTitleDisplayMode for non-macOS targets
+            .navigationTitle("IMPORT")
+            .modifier(InlineNavigationBarTitleModifier())
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    NavigationButtons.backButton(presentationStack: $presentationStack)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationButtons.questionMarkButton
+                }
             }
-            .padding(.all, 20)
-            .buttonStyle(PlainButtonStyle())
-          }
         }
-
-        Text("ENTER YOUR PREVIOUSLY CREATED VAULT SHARE")
-          .font(.system(size: geometry.size.width * 0.04, weight: .medium))
-          .padding(.top, 8)
-          .foregroundColor(.black)
-
-        Spacer()
-
-        BottomBar(
-          content: "CONTINUE",
-          onClick: {
-            self.presentationStack.append(.newWalletInstructions)
-          }
-        )
-        .padding(.bottom)
-      }
-      .padding([.leading, .trailing], geometry.size.width * 0.05)
-      // Conditionally apply navigationBarTitleDisplayMode for non-macOS targets
-      .navigationTitle("IMPORT")
-      .modifier(InlineNavigationBarTitleModifier())
-      .toolbar {
-        #if os(iOS)
-          ToolbarItem(placement: .navigationBarLeading) {
-            NavigationButtons.backButton(presentationStack: $presentationStack)
-          }
-          ToolbarItem(placement: .navigationBarTrailing) {
-            NavigationButtons.questionMarkButton
-          }
-        #else
-          ToolbarItem {
-            NavigationButtons.backButton(presentationStack: $presentationStack)
-          }
-          ToolbarItem {
-            NavigationButtons.questionMarkButton
-          }
-        #endif
-      }
-
+        .background(Color.white)
+        .navigationBarBackButtonHidden(true)
     }
-    .background(Color.white)
-    .navigationBarBackButtonHidden(true)
-  }
-
+    private func readContent(of url: URL) {
+        let success = url.startAccessingSecurityScopedResource()
+        defer { url.stopAccessingSecurityScopedResource() }
+        
+        guard success else {
+            print("Permission denied for accessing the file.")
+            return
+        }
+        
+        do {
+            let fileContent = try String(contentsOf: url, encoding: .utf8)
+            DispatchQueue.main.async {
+                self.vaultText = fileContent
+            }
+        } catch {
+            print("Failed to read file: \(error.localizedDescription)")
+            DispatchQueue.main.async {
+                self.vaultText = "Failed to read file"
+            }
+        }
+    }
+    
+    
+#if os(iOS)
+    private func handleScan(result: Result<ScanResult, ScanError>) {
+        switch result {
+        case .success(let result):
+            let qrCodeResult = result.string
+            self.vaultText = qrCodeResult
+            self.isShowingScanner = false
+//
+//            let decoder = JSONDecoder()
+//            if let data = qrCodeResult.data(using: .utf8) {
+//                do {
+//                    self.vaultText = String(contentsOf: qrCodeResult, encoding: .utf8)
+//                    print(data)
+//                } catch {
+//                    logger.error("fail to decode keysign message,error:\(error.localizedDescription)")
+//                    self.errorMsg = error.localizedDescription
+//                }
+//            }
+        case .failure(let err):
+            logger.error("fail to scan QR code,error:\(err.localizedDescription)")
+        }
+    }
+#endif
+    
 }
 
 #Preview {
-  ImportWalletView(presentationStack: .constant([]))
+    ImportWalletView(presentationStack: .constant([]))
 }
