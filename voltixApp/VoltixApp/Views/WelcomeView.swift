a170378c8b65fc68334125db44463d20935f50a8
Johnny Luo <johnnyluo1980@gmail.com>
2024-02-15 14:13:11 +1100

    Vault creating workflow #1

--- voltixApp/VoltixApp/Views/WelcomeView.swift
+++ voltixApp/VoltixApp/Views/WelcomeView.swift
@@ -6,74 +6,62 @@ struct WelcomeView: View {
     
     var body: some View {
         GeometryReader { geometry in
-            VStack(alignment: .center) {
-                
-                VStack {
-                    
-                    Spacer()
-                    Logo(width: geometry.size.width * 0.25, height: geometry.size.width * 0.25)
-                        .padding(.top, geometry.size.height * 0.02)
-                    
-                    Text("SECURE CRYPTO VAULT")
-                        .font(.system(size: geometry.size.width * 0.05, weight: .bold))
-                        .padding(.top, geometry.size.height * 0.01)
-                        .foregroundColor(.black)
-                    
-                    VStack(spacing: geometry.size.height * 0.01) {
-                        featureText("TWO FACTOR AUTHENTICATION", geometry: geometry)
-                        featureText("SECURE, TRUSTED DEVICES", geometry: geometry)
-                        featureText("FULLY SELF-CUSTODIAL", geometry: geometry)
-                        featureText("NO TRACKING, NO REGISTRATION", geometry: geometry)
-                        featureText("FULLY OPEN-SOURCE", geometry: geometry)
-                        featureText("AUDITED", geometry: geometry)
-                    }
-                    .padding(.top, geometry.size.height * 0.02)
-                    
-                    Spacer()
-                    
-                    BottomBar(
-                        content: "START",
-                        onClick: {
-                            
-                            self.presentationStack.append(.vaultAssets(TransactionDetailsViewModel()))
-                            //                            let isNewVault:Bool = ((appState.currentVault?.name) == nil)
-                            //                            if isNewVault {
-                            //                                self.presentationStack.append(.startScreen)
-                            //                            } else {
-                            //                                self.presentationStack.append(.vaultAssets(TransactionDetailsViewModel()))
-                            //                            }
+            ScrollView {
+                VStack(alignment: .center) {
+                    VStack {
+                        Spacer()
+                        Logo(width: geometry.size.width * 0.25, height: geometry.size.width * 0.25)
+                            .padding(.top, geometry.size.height * 0.02)
+                        
+                        Text("SECURE CRYPTO VAULT")
+                            .font(.system(size: geometry.size.width * 0.05, weight: .bold))
+                            .padding(.top, geometry.size.height * 0.01)
+                            .foregroundColor(.black)
+                        
+                        VStack(spacing: geometry.size.height * 0.01) {
+                            featureText("TWO FACTOR AUTHENTICATION", geometry: geometry)
+                            featureText("SECURE, TRUSTED DEVICES", geometry: geometry)
+                            featureText("FULLY SELF-CUSTODIAL", geometry: geometry)
+                            featureText("NO TRACKING, NO REGISTRATION", geometry: geometry)
+                            featureText("FULLY OPEN-SOURCE", geometry: geometry)
+                            featureText("AUDITED", geometry: geometry)
                         }
-                    )
-                    .padding(.bottom, geometry.size.height * 0.02)
-                }.onAppear {
-                    
+                        .padding(.top, geometry.size.height * 0.02)
+                        
+                        Spacer()
+                        
+                        BottomBar(
+                            content: "START",
+                            onClick: {
+                                let isNewVault: Bool = ((appState.currentVault?.name) == nil)
+                                if isNewVault {
+                                    self.presentationStack.append(.startScreen)
+                                } else {
+                                    self.presentationStack.append(.vaultAssets(TransactionDetailsViewModel()))
+                                }
+                            }
+                        )
+                        .padding(.bottom, geometry.size.height * 0.02)
+                    }.onAppear {}
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .modifier(InlineNavigationBarTitleModifier())
+                        .toolbar {
+
+                            ToolbarItem(placement: .navigationBarLeading) {
+                                NavigationButtons.backButton(presentationStack: $presentationStack)
+                            }
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                NavigationButtons.questionMarkButton
+                            }
+                        }
+                        .navigationTitle("VOLTIX")
                 }
-                .frame(width: geometry.size.width, height: geometry.size.height)
-                .modifier(InlineNavigationBarTitleModifier())
-                .toolbar {
-#if os(iOS)
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        NavigationButtons.backButton(presentationStack: $presentationStack)
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationButtons.questionMarkButton
-                    }
-#else
-                    ToolbarItem {
-                        NavigationButtons.backButton(presentationStack: $presentationStack)
-                    }
-                    ToolbarItem {
-                        NavigationButtons.questionMarkButton
-                    }
-#endif
-                }
-                .navigationTitle("VOLTIX")
-                
             }
         }
         .background(Color.white)
         .navigationBarBackButtonHidden(true)
     }
+
     // Use a computed property to determine if the device is an iPad
     private var isIpad: Bool {
 #if os(iOS)
@@ -81,7 +69,6 @@ struct WelcomeView: View {
 #else
         return false
 #endif
-        
     }
     
     private func featureText(_ text: String, geometry: GeometryProxy) -> some View {
