0806b5b7c3600cdc4def6661fe8bc96f21ff1f54
Johnny Luo <johnnyluo1980@gmail.com>
2024-02-23 17:01:26 +1100

    Fix QRCode

--- voltixApp/VoltixApp/util/Utils.swift
+++ voltixApp/VoltixApp/util/Utils.swift
@@ -150,17 +150,4 @@ enum Utils {
     public static func getLocalDeviceIdentity() -> String {
         return UIDevice.current.name
     }
-    public static func generateHighQualityQRCode(from string: String, withScale scale: CGFloat = 3.0) -> UIImage? {
-        let filter = CIFilter.qrCodeGenerator()
-        filter.message = Data(string.utf8)
-        
-        guard let qrImage = filter.outputImage else { return nil }
-        let transform = CGAffineTransform(scaleX: scale, y: scale) // Scale the QR code
-        let scaledQRImage = qrImage.transformed(by: transform)
-        
-        let context = CIContext()
-        guard let cgImage = context.createCGImage(scaledQRImage, from: scaledQRImage.extent) else { return nil }
-        
-        return UIImage(cgImage: cgImage)
-    }
 }
