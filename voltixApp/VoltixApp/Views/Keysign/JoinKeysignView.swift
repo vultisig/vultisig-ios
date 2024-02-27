98f60d7e6ee898e6c3a5791a8c507ae923d94dae
Johnny Luo <johnnyluo1980@gmail.com>
2024-02-27 16:38:52 +1100

    prepare keysign messages

--- voltixApp/VoltixApp/Views/Keysign/JoinKeysignView.swift
+++ voltixApp/VoltixApp/Views/Keysign/JoinKeysignView.swift
@@ -222,9 +222,7 @@ struct JoinKeysignView: View {
                     self.sessionID = keysignMsg.sessionID
                     self.keysignPayload = keysignMsg.payload
                     logger.info("QR code scanned successfully. Session ID: \(self.sessionID)")
-                    if keysignMsg.payload.coin.ticker == "BTC" || keysignMsg.payload.coin.ticker == "ETH" {
-                        self.prepareKeysignMessages(keysignPayload: keysignMsg.payload)
-                    }
+                    self.prepareKeysignMessages(keysignPayload: keysignMsg.payload)
                 } catch {
                     logger.error("Failed to decode keysign message. Error: \(error.localizedDescription)")
                     self.errorMsg = "Error decoding keysign message: \(error.localizedDescription)"
