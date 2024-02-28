28c90324fa2b638a4540a776359105566c63bbf2
Enrique Souza <enriquesouza@live.com>
2024-02-27 21:12:44 -0300

    Support to ETH and ERC20 tokens

--- voltixApp/VoltixApp/Services/UnspentOutputsService.swift
+++ voltixApp/VoltixApp/Services/UnspentOutputsService.swift
@@ -1,31 +1,54 @@
 import Foundation
 import SwiftUI
 
-@MainActor  // Ensures all updates are on the main thread
+@MainActor
 public class UnspentOutputsService: ObservableObject {
     @Published var walletData: BitcoinTransaction?
     @Published var errorMessage: String?
     
-    // Replace with your actual function to fetch unspent outputs
+        // Cache structure to hold data and timestamp
+    private struct CacheEntry {
+        let data: BitcoinTransaction
+        let timestamp: Date
+    }
+    
+        // Dictionary to store cache entries with address as the key
+    private var cache: [String: CacheEntry] = [:]
+    
+        // Function to check if cache for a given address is valid (not older than 1 minutes)
+    private func isCacheValid(for address: String) -> Bool {
+        if let entry = cache[address], -entry.timestamp.timeIntervalSinceNow < 60 {
+            return true // Cache is valid if less than 5 minutes old
+        }
+        return false
+    }
+    
+        // Replace with your actual function to fetch unspent outputs
     func fetchUnspentOutputs(for address: String) async {
-        guard
-            let url = URL(
-                string: "https://api.blockcypher.com/v1/btc/main/addrs/\(address)?unspentOnly=true")
-        else {
-            // Handle the case for an invalid URL
+            // Use cache if it's valid for the requested address
+        if isCacheValid(for: address), let cachedData = cache[address]?.data {
+            self.walletData = cachedData
+            return
+        }
+        
+            // Construct the URL
+        guard let url = URL(string: "https://api.blockcypher.com/v1/btc/main/addrs/\(address)?unspentOnly=true") else {
             print("Invalid URL")
             return
         }
         
         do {
+                // Fetch data from the URL
             let (data, _) = try await URLSession.shared.data(from: url)
-            // Decode the JSON data into WalletUnspentOutput
             let decoder = JSONDecoder()
             let decodedData = try decoder.decode(BitcoinTransaction.self, from: data)
-            // Update your published property with the decoded data
-            self.walletData = decodedData            
+            
+                // Update the cache with new data and current timestamp for the address
+            cache[address] = CacheEntry(data: decodedData, timestamp: Date())
+            
+                // Update your published property with the decoded data
+            self.walletData = decodedData
         } catch {
-            // Handle any errors
             print("Fetch failed: \(error.localizedDescription)")
             self.errorMessage = error.localizedDescription
         }
--- voltixApp/VoltixApp/Views/Vault/AssetsList.swift
+++ voltixApp/VoltixApp/Views/Vault/AssetsList.swift
@@ -74,8 +74,6 @@ struct AssetsList: View {
                                     logger.info("fail to get bitcoin address,error:\(err.localizedDescription)")
                             }
 
-                        case Chain.Ethereum.name:
-                            vault.coins.append(Coin(chain: Chain.Ethereum, ticker: "ETH", logo: "", address: "TO BE IMPLEMENTED"))
                         default:
                             print("do it later")
                     }
