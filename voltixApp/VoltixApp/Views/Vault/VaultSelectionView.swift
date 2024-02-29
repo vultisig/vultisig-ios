2ca2227ea36789ddf38364d5636714d6baa43719
Enrique Souza <enriquesouza@live.com>
2024-02-29 05:40:59 -0300

    Support to ETH and ERC20 tokens:: paste button and tests

--- voltixApp/VoltixApp/Views/Vault/VaultSelectionView.swift
+++ voltixApp/VoltixApp/Views/Vault/VaultSelectionView.swift
@@ -15,101 +15,150 @@ struct VaultSelectionView: View {
     @State private var vaultToEdit: Vault? = nil
     @State private var vaultEditName: String = ""
     @State private var expandedVaults: Set<Int> = []
+    @Environment(\.colorScheme) var colorScheme
+    
+    private var cardBackgroundColor: Color {
+        switch colorScheme {
+            case .light:
+                return Color(UIColor.systemFill)
+            case .dark:
+                return Color(UIColor.secondarySystemGroupedBackground)
+            @unknown default:
+                return Color(UIColor.systemBackground)
+        }
+    }
     
     var body: some View {
-        List(selection: $appState.currentVault) {
-            ForEach(vaults.indices, id: \.self) { index in
-                let vault = vaults[index]
-                VStack {
-                    HStack {
-                        Button(action: {
-                            self.appState.currentVault = vault
-                            self.presentationStack.append(.listVaultAssetView)
-                        }) {
-                            HStack {
-                                Text(vault.name.uppercased())
-                                    .font(Font.custom("Menlo", size: 20).weight(.bold))
-                                    .fontWeight(.black)
-                                Spacer()
-                            }
-                            .padding() // Ensure there's some padding around the text for a larger clickable area
-                            .frame(maxWidth: .infinity) // Ensure the HStack fills the button horizontally
-                            .contentShape(Rectangle()) // Makes the entire area within the frame tappable
-                        }
-                        .buttonStyle(PlainButtonStyle()) // Maintains the button's style without additional effects
-                        
-                        Spacer()
-                        Image(systemName: expandedVaults.contains(index) ? "chevron.up" : "chevron.down")
-                            .onTapGesture {
-                                if expandedVaults.contains(index) {
-                                    expandedVaults.remove(index)
-                                } else {
-                                    expandedVaults.insert(index)
+        ScrollView {
+            LazyVStack {
+                ForEach(vaults.indices, id: \.self) { index in
+                    let vault = vaults[index]
+                    VStack {
+                        HStack {
+                            Button(action: {
+                                self.appState.currentVault = vault
+                                self.presentationStack.append(.listVaultAssetView)
+                            }) {
+                                HStack {
+                                    Text(vault.name.uppercased())
+                                        .font(Font.custom("Menlo", size: 20).weight(.bold))
+                                    Spacer()
                                 }
+                                .padding() // Ensure there's some padding around the text for a larger clickable area
+                                .frame(maxWidth: .infinity) // Ensure the HStack fills the button horizontally
+                                .contentShape(Rectangle()) // Makes the entire area within the frame tappable
+
                             }
-                    }
-                    if expandedVaults.contains(index) {
-                        Divider()
-                        HStack {
-                            Image(systemName: "arrow.down.circle")
-                            Text("Backup your vault").font(Font.custom("Menlo", size: 15))
+                            .buttonStyle(PlainButtonStyle())
                             Spacer()
-                        }
-                        .frame(maxWidth: .infinity)
-                        .contentShape(Rectangle()) // Makes the entire area within the frame tappable
-                        .padding()
-                        .onTapGesture {
-                            vaultToExport = vault
-                            self.showingExporter = true
+                            Image(systemName: expandedVaults.contains(index) ? "chevron.up" : "chevron.down")
+                                .onTapGesture {
+                                    if expandedVaults.contains(index) {
+                                        expandedVaults.remove(index)
+                                    } else {
+                                        expandedVaults.insert(index)
+                                    }
+                                }
                         }
                         
-                        HStack {
-                            Image(systemName: "pencil")
-                            Text("Edit your vault's name").font(Font.custom("Menlo", size: 15))
-                            Spacer()
-                        }
-                        .frame(maxWidth: .infinity)
-                        .contentShape(Rectangle()) // Makes the entire area within the frame tappable
-                        .padding()
-                        .onTapGesture {
-                            self.vaultToEdit = vault
-                            vaultEditName = vault.name
-                            showEditVaultName = true
-                        }
-                        
-                        HStack {
-                            Image(systemName: "trash")
-                            Button("Delete your vault permanently", role: .destructive) {
-                                self.itemToDelete = vault
-                                showingDeleteAlert = true
+                        if expandedVaults.contains(index) {
+                            VStack(spacing: 15) {
+                                Divider()
+                                
+                                Button(action: {
+                                    vaultToExport = vault
+                                    showingExporter = true
+                                }) {
+                                    HStack {
+                                        Image(systemName: "arrow.down.circle")
+                                        Text("Backup your vault")
+                                            .font(Font.custom("Menlo", size: 15))
+                                        Spacer()
+                                    }
+                                }
+                                .buttonStyle(PlainButtonStyle())
+                                .padding()
+                                .background(cardBackgroundColor)
+                                .cornerRadius(10)
+                                
+                                Button(action: {
+                                    vaultToEdit = vault
+                                    vaultEditName = vault.name
+                                    showEditVaultName = true
+                                }) {
+                                    HStack {
+                                        Image(systemName: "pencil")
+                                        Text("Edit your vault's name")
+                                            .font(Font.custom("Menlo", size: 15))
+                                        Spacer()
+                                    }
+                                }
+                                .padding()
+                                .buttonStyle(PlainButtonStyle())
+                                .background(cardBackgroundColor)
+                                .cornerRadius(10)
+                                
+                                Button(action: {
+                                    itemToDelete = vault
+                                    showingDeleteAlert = true
+                                }) {
+                                    HStack {
+                                        Image(systemName: "trash")
+                                        Text("Delete your vault permanently")
+                                            .font(Font.custom("Menlo", size: 15))
+                                        Spacer()
+                                    }
+                                }
+                                .buttonStyle(PlainButtonStyle())
+                                .padding()
+                                .background(cardBackgroundColor)
+                                .foregroundColor(.red)
+                                .cornerRadius(10)
                             }
-                            .font(Font.custom("Menlo", size: 15))
-                            Spacer()
+                            .padding(.top, 10)
                         }
-                        .frame(maxWidth: .infinity)
-                        .contentShape(Rectangle()) // Makes the entire area within the frame tappable
-                        .padding()
                     }
+                    .padding(.vertical, 15)
+                    .padding()
+                    .background(cardBackgroundColor)
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+                    .animation(.default, value: expandedVaults.contains(index))
                 }
+            }
+        }
+        .padding()
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden()
+        .navigationTitle("MY VAULTS")
+        .modifier(InlineNavigationBarTitleModifier())
+        .toolbar {
+            ToolbarItemGroup(placement: .automatic) {
+                
+                Button("New vault", systemImage: "plus.square.on.square") {
+                    let vault = Vault(name: "Vault #\(vaults.count + 1)")
+                    appState.creatingVault = vault
+                    self.presentationStack.append(.startScreen)
+                }
+                .buttonStyle(PlainButtonStyle())
             }
         }
         .sheet(isPresented: $showEditVaultName) {
             VStack {
                 Form {
-                    TextField("vault name", text: $vaultEditName)
-                        .textFieldStyle(.roundedBorder)
+                    TextField("Vault name", text: $vaultEditName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
                 }.padding()
-                
                 HStack {
                     Button("Save") {
                         if let vaultToEdit = self.vaultToEdit {
                             vaultToEdit.name = vaultEditName
                                 // Implement save functionality here
                         }
-                        showEditVaultName.toggle()
+                        showEditVaultName = false
                     }
                     Button("Cancel") {
-                        showEditVaultName.toggle()
+                        showEditVaultName = false
                     }
                 }
                 Spacer()
@@ -117,8 +166,8 @@ struct VaultSelectionView: View {
         }
         .fileExporter(isPresented: $showingExporter, document: VoltixDocument(vault: vaultToExport), contentType: .data, defaultFilename: "\(vaultToExport?.name ?? "vault").dat") { result in
             switch result {
-                case .failure(let err):
-                    print("Fail to export, error: \(err.localizedDescription)")
+                case .failure(let error):
+                    print("Fail to export, error: \(error.localizedDescription)")
                 case .success(let url):
                     print("Exported to \(url)")
             }
@@ -132,20 +181,15 @@ struct VaultSelectionView: View {
         } message: {
             Text("Are you sure you want to delete this vault? This operation is not reversible.")
         }
-        .navigationTitle("MY VAULTS".uppercased())
-        .navigationBarBackButtonHidden()
-        .modifier(InlineNavigationBarTitleModifier())
-        .toolbar {
-            ToolbarItemGroup(placement: .automatic) {
-                
-                Button("New vault", systemImage: "plus.square.on.square") {
-                    let vault = Vault(name: "Vault #\(vaults.count + 1)")
-                    appState.creatingVault = vault
-                    self.presentationStack.append(.startScreen)
-                }
-                .buttonStyle(PlainButtonStyle())
-            }
-        }
+//        .toolbar {
+//            ToolbarItemGroup(placement: .navigationBarTrailing) {
+//                Button("New vault", action: {
+//                    let vault = Vault(name: "Vault #\(vaults.count + 1)")
+//                    appState.creatingVault = vault
+//                    presentationStack.append(.startScreen)
+//                })
+//            }
+//        }
     }
     
     func deleteVault(vault: Vault) {
