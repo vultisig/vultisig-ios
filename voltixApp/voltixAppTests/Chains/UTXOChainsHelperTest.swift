9e4d02b53d6723ddd5cd20e8f440ca660e38aba6
Johnny Luo <johnnyluo1980@gmail.com>
2024-03-10 21:47:34 +1100

    WIP - add some tests

---
+++ voltixApp/voltixAppTests/Chains/UTXOChainsHelperTest.swift
@@ -0,0 +1,23 @@
+//
+//  UTXOChainsHelperTest.swift
+//  VoltixAppTests
+//
+//  Created by Johnny Luo on 10/3/2024.
+//
+
+import XCTest
+
+final class UTXOChainsHelperTest: XCTestCase {
+
+    
+    func testGetCoin() throws {
+        
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+
+}
