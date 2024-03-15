ccfb81058b7a4d09c203bbbf54eb29772631aedc
Amol Kumar <amolkmr10@gmail.com>
2024-03-15 00:12:06 -0400

    SendCryptoDetailsView refactored

--- voltixApp/VoltixApp/Views/Send/SendCryptoDetailsView.swift
+++ voltixApp/VoltixApp/Views/Send/SendCryptoDetailsView.swift
@@ -51,19 +51,12 @@ struct SendCryptoDetailsView: View {
                 .cornerRadius(100)
             
             Text("Ethereum")
-                .font(.body16Menlo)
-                .foregroundColor(.neutral0)
-            
             Spacer()
-            
             Text("23.2")
-                .font(.body16Menlo)
-                .foregroundColor(.neutral0)
-            
             Image(systemName: "chevron.down")
-                .font(.body16Menlo)
-                .foregroundColor(.neutral0)
         }
+        .font(.body16Menlo)
+        .foregroundColor(.neutral0)
         .frame(height: 48)
         .padding(.horizontal, 12)
         .background(Color.blue600)
@@ -91,79 +84,15 @@ struct SendCryptoDetailsView: View {
     var toField: some View {
         VStack(spacing: 8) {
             getTitle(for: "to")
-            toTextField
+            AddressTextField(address: $toAddress)
         }
-    }
-    
-    var toTextField: some View {
-        ZStack(alignment: .trailing) {
-            if toAddress.isEmpty {
-                Text(NSLocalizedString("enterAddress", comment: ""))
-                    .foregroundColor(Color.neutral0)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            
-            HStack(spacing: 0) {
-                TextField(NSLocalizedString("enterAddress", comment: "").capitalized, text: $toAddress)
-                    .foregroundColor(.neutral0)
-                    .submitLabel(.next)
-                
-                scanButton
-            }
-        }
-        .font(.body12Menlo)
-        .foregroundColor(.neutral0)
-        .frame(height: 48)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.horizontal, 12)
-        .background(Color.blue600)
-        .cornerRadius(10)
-    }
-    
-    var scanButton: some View {
-        Image(systemName: "camera")
-            .font(.body16Menlo)
-            .foregroundColor(.neutral0)
-            .frame(width: 40, height: 40)
     }
     
     var amountField: some View {
         VStack(spacing: 8) {
             getTitle(for: "amount")
-            amountTextField
+            AmountTextField(amount: $amount)
         }
-    }
-    
-    var amountTextField: some View {
-        ZStack(alignment: .trailing) {
-            if amount.isEmpty {
-                Text(NSLocalizedString("enterAmount", comment: ""))
-                    .foregroundColor(Color.neutral0)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            
-            HStack(spacing: 0) {
-                TextField(NSLocalizedString("enterAmount", comment: "").capitalized, text: $amount)
-                    .foregroundColor(.neutral0)
-                    .submitLabel(.next)
-                
-                maxButton
-            }
-        }
-        .font(.body12Menlo)
-        .foregroundColor(.neutral0)
-        .frame(height: 48)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.horizontal, 12)
-        .background(Color.blue600)
-        .cornerRadius(10)
-    }
-    
-    var maxButton: some View {
-        Text(NSLocalizedString("max", comment: "").uppercased())
-            .font(.body16Menlo)
-            .foregroundColor(.neutral0)
-            .frame(width: 40, height: 40)
     }
     
     var gasField: some View {
@@ -186,7 +115,6 @@ struct SendCryptoDetailsView: View {
             .font(.body14MontserratMedium)
             .foregroundColor(.neutral0)
             .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 16)
     }
 }
 
