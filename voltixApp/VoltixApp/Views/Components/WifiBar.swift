65cff6ee4a66e12323f5d5f9ac6d143bf57327cc
Johnny Luo <johnnyluo1980@gmail.com>
2024-03-15 14:28:24 +1100

    Fix wifibar

--- voltixApp/VoltixApp/Views/Components/WifiBar.swift
+++ voltixApp/VoltixApp/Views/Components/WifiBar.swift
@@ -8,23 +8,17 @@ import SwiftUI
 struct WifiBar: View {
     var body: some View {
         HStack(spacing: 25) {
-            ZStack {
-                Image(systemName: "wifi")
-                    .font(.title30MenloBlack)
-                    .foregroundColor(.neutral0)
-            }
-            .frame(width: 36, height: 29)
+            Image(systemName: "wifi")
+                .font(.title30MenloBlack)
+                .foregroundColor(.neutral0)
+
             Text("Keep devices on same WiFi Network with VOLTIX open")
-                .lineLimit(nil)
-                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
                 .lineSpacing(5)
                 .font(.body15MenloBold)
                 .foregroundColor(.neutral0)
                 .multilineTextAlignment(.center)
         }
-
         .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
-        .frame(height: 71)
     }
 }
 
