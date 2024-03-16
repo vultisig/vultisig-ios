070f1a8f45d1b677259b9d81459aada4c1295bda
Amol Kumar <amolkmr10@gmail.com>
2024-03-15 22:48:46 -0400

    SendCryptoQRCodeView implemented

---
+++ voltixApp/VoltixApp/Views/Send/SendCryptoQRCodeView.swift
@@ -0,0 +1,18 @@
+//
+//  SendCryptoQRCodeView.swift
+//  VoltixApp
+//
+//  Created by Amol Kumar on 2024-03-15.
+//
+
+import SwiftUI
+
+struct SendCryptoQRCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SendCryptoQRCodeView()
+}
