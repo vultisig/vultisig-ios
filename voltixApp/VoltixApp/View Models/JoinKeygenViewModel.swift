128ff8901609370979f3b6e72fe490329479d583
Johnny Luo <johnnyluo1980@gmail.com>
2024-03-18 16:11:24 +1100

    add keysign viewmodel

---
+++ voltixApp/VoltixApp/View Models/JoinKeygenViewModel.swift
@@ -0,0 +1,175 @@
+//
+//  JoinKeygenViewModel.swift
+//  VoltixApp
+//
+//  Created by Johnny Luo on 18/3/2024.
+//
+
+import Foundation
+import OSLog
+import CodeScanner
+
+enum JoinKeygenStatus {
+    case DiscoverSessionID
+    case DiscoverService
+    case JoinKeygen
+    case WaitingForKeygenToStart
+    case KeygenStarted
+    case FailToStart
+}
+
+class JoinKeygenViewModel: ObservableObject {
+    private let logger = Logger(subsystem: "join-keygen", category: "viewmodel")
+    var vault: Vault
+    var serviceDelegate: ServiceDelegate?
+    
+    @Published var tssType: TssType = .Keygen
+    @Published var isShowingScanner = false
+    @Published var sessionID: String? = nil
+    @Published var hexChainCode: String = ""
+    
+    @Published var netService: NetService? = nil
+    @Published var status = JoinKeygenStatus.DiscoverSessionID
+    @Published var keygenCommittee = [String]()
+    @Published var oldCommittee = [String]()
+    @Published var localPartyID: String = ""
+    @Published var serviceName = ""
+    @Published var errorMessage = ""
+    
+    init() {
+        self.vault = Vault(name: "New Vault")
+    }
+    
+    func setData(vault: Vault, serviceDelegate: ServiceDelegate) {
+        self.vault = vault
+        self.serviceDelegate = serviceDelegate
+        if !vault.localPartyID.isEmpty {
+            self.localPartyID = vault.localPartyID
+        } else {
+            self.localPartyID = Utils.getLocalDeviceIdentity()
+            vault.localPartyID = self.localPartyID
+        }
+    }
+    
+    func showBarcodeScanner() {
+        isShowingScanner = true
+    }
+    
+    func setStatus(status: JoinKeygenStatus) {
+        self.status = status
+    }
+    
+    func discoverService(){
+        self.netService = NetService(domain: "local.", type: "_http._tcp.", name: self.serviceName)
+        netService?.delegate = self.serviceDelegate
+        netService?.resolve(withTimeout: 10)
+    }
+    
+    func joinKeygenCommittee() {
+        guard let serverURL = serviceDelegate?.serverURL, let sessionID = sessionID else {
+            logger.error("Required information for joining key generation committee is missing.")
+            return
+        }
+        
+        let urlString = "\(serverURL)/\(sessionID)"
+        let body = [localPartyID]
+        Utils.sendRequest(urlString: urlString, method: "POST", body: body) { success in
+            if success {
+                self.logger.info("Successfully joined the key generation committee.")
+                self.status = .WaitingForKeygenToStart
+            }
+        }
+    }
+    func waitForKeygenStart() async {
+        do{
+            let t = Task {
+                repeat {
+                    checkKeygenStarted()
+                    try await Task.sleep(for: .seconds(1))
+                } while self.status == .WaitingForKeygenToStart
+            }
+            try await t.value
+        }
+        catch{
+            logger.error("Failed to wait for keygen to start.")
+        }
+    }
+    private func checkKeygenStarted() {
+        guard let serverURL = serviceDelegate?.serverURL, let sessionID = sessionID else {
+            logger.error("Required information for checking key generation start is missing.")
+            return
+        }
+        
+        let urlString = "\(serverURL)/start/\(sessionID)"
+        Utils.getRequest(urlString: urlString, headers: [String: String](), completion: { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let peers = try decoder.decode([String].self, from: data)
+                    if peers.contains(self.localPartyID) {
+                        self.keygenCommittee.append(contentsOf: peers)
+                        self.status = .KeygenStarted
+                    }
+                } catch {
+                    self.logger.error("Failed to decode response to JSON: \(data)")
+                }
+            case .failure(let error):
+                self.logger.error("Failed to check if key generation has started, error: \(error)")
+            }
+        })
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        defer {
+            isShowingScanner = false
+        }
+        switch result {
+        case .success(let result):
+            guard let scanData = result.string.data(using: .utf8) else {
+                errorMessage = "Failed to process scan data."
+                status = .FailToStart
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(PeerDiscoveryPayload.self, from: scanData)
+                switch result {
+                case .Keygen(let keysignMsg):
+                    tssType = .Keygen
+                    sessionID = keysignMsg.sessionID
+                    hexChainCode = keysignMsg.hexChainCode
+                    vault.hexChainCode = hexChainCode
+                    serviceName = keysignMsg.serviceName
+                case .Reshare(let reshareMsg):
+                    tssType = .Reshare
+                    oldCommittee = reshareMsg.oldParties
+                    sessionID = reshareMsg.sessionID
+                    hexChainCode = reshareMsg.hexChainCode
+                    serviceName = reshareMsg.serviceName
+                    // this means the vault is new , and it join the reshare to become the new committee
+                    if vault.pubKeyECDSA.isEmpty {
+                        vault.hexChainCode = reshareMsg.hexChainCode
+                    } else {
+                        if vault.pubKeyECDSA != reshareMsg.pubKeyECDSA {
+                            errorMessage = "You choose the wrong vault"
+                            logger.error("The vault's public key doesn't match the reshare message's public key")
+                            status = .FailToStart
+                            return
+                        }
+                    }
+                }
+                
+            } catch {
+                errorMessage = "Failed to decode peer discovery message: \(error.localizedDescription)"
+                status = .FailToStart
+                return
+            }
+            status = .DiscoverService
+        case .failure(let error):
+            errorMessage = "Failed to scan QR code: \(error.localizedDescription)"
+            status = .FailToStart
+            return
+        }
+    }
+}
---
+++ voltixApp/VoltixApp/View Models/JoinKeysignViewModel.swift
@@ -0,0 +1,183 @@
+//
+//  JoinKeysignViewModel.swift
+//  VoltixApp
+//
+
+import Foundation
+import OSLog
+import CodeScanner
+
+enum JoinKeysignStatus {
+    case DiscoverSigningMsg
+    case DiscoverService
+    case JoinKeysign
+    case WaitingForKeysignToStart
+    case KeysignStarted
+    case FailedToStart
+}
+
+class JoinKeysignViewModel: ObservableObject {
+    private let logger = Logger(subsystem: "join-keysign", category: "viewmodel")
+    var vault: Vault
+    var serviceDelegate: ServiceDelegate?
+    
+    @Published var isShowingScanner = false
+    @Published var sessionID: String = ""
+    @Published var keysignMessages = [String]()
+    @Published var netService: NetService? = nil
+    @Published var status = JoinKeysignStatus.DiscoverSigningMsg
+    @Published var keysignCommittee = [String]()
+    @Published var localPartyID: String = ""
+    @Published var errorMsg: String = ""
+    @Published var keysignPayload: KeysignPayload? = nil
+    @Published var serviceName = ""
+    
+    init() {
+        self.vault = Vault(name: "New Vault")
+    }
+    
+    func setData(vault: Vault, serviceDelegate: ServiceDelegate) {
+        self.vault = vault
+        self.serviceDelegate = serviceDelegate
+        
+        if !self.vault.localPartyID.isEmpty {
+            self.localPartyID = self.vault.localPartyID
+        } else {
+            self.localPartyID = Utils.getLocalDeviceIdentity()
+        }
+    }
+    
+    func startScan() {
+        self.isShowingScanner = true
+    }
+    
+    func joinKeysignCommittee() {
+        guard let serverURL = serviceDelegate?.serverURL else {
+            self.logger.error("Server URL could not be found. Please ensure you're connected to the correct network.")
+            return
+        }
+        guard !self.sessionID.isEmpty else {
+            self.logger.error("Session ID has not been acquired. Please scan the QR code again.")
+            return
+        }
+        
+        let urlString = "\(serverURL)/\(sessionID)"
+        let body = [self.localPartyID]
+        
+        Utils.sendRequest(urlString: urlString, method: "POST", body: body) { success in
+            if success {
+                self.logger.info("Successfully joined the keysign committee.")
+                self.status = .WaitingForKeysignToStart
+            } else {
+                self.errorMsg = "Failed to join the keysign committee. Please check your connection and try again."
+                self.status = .FailedToStart
+            }
+        }
+    }
+    
+    func setStatus(status: JoinKeysignStatus) {
+        self.status = status
+    }
+    
+    func discoverService() {
+        self.netService = NetService(domain: "local.", type: "_http._tcp.", name: self.serviceName)
+        self.netService?.delegate = self.serviceDelegate
+        self.netService?.resolve(withTimeout: 10)
+    }
+    
+    func waitForKeysignStart() async {
+        do {
+            let t = Task {
+                repeat {
+                    self.checkKeysignStarted()
+                    try await Task.sleep(for: .seconds(1))
+                } while self.status == .WaitingForKeysignToStart
+            }
+            try await t.value
+        } catch {
+            self.logger.error("Failed to wait for keysign to start.")
+        }
+    }
+    
+    private func checkKeysignStarted() {
+        guard let serverURL = serviceDelegate?.serverURL else {
+            self.logger.error("Server URL could not be found. Please ensure you're connected to the correct network.")
+            return
+        }
+        guard !self.sessionID.isEmpty else {
+            self.logger.error("Session ID has not been acquired. Please scan the QR code again.")
+            return
+        }
+        
+        let urlString = "\(serverURL)/start/\(sessionID)"
+        Utils.getRequest(urlString: urlString, headers: [String: String](), completion: { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let peers = try decoder.decode([String].self, from: data)
+                    if peers.contains(self.localPartyID) {
+                        self.keysignCommittee.append(contentsOf: peers)
+                        self.status = .KeysignStarted
+                        self.logger.info("Keysign process has started successfully.")
+                    }
+                } catch {
+                    self.errorMsg = "There was an issue processing the keysign start response. Please try again."
+                    self.status = .FailedToStart
+                }
+            case .failure(let error):
+                let err = error as NSError
+                if err.code == 404 {
+                    self.logger.info("Waiting for keysign to start. Please stand by.")
+                } else {
+                    self.errorMsg = "Failed to verify keysign start. Error: \(error.localizedDescription)"
+                    self.status = .FailedToStart
+                }
+            }
+        })
+    }
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        defer {
+            self.isShowingScanner = false
+        }
+        switch result {
+        case .success(let result):
+            let qrCodeResult = result.string
+            let decoder = JSONDecoder()
+            if let data = qrCodeResult.data(using: .utf8) {
+                do {
+                    let keysignMsg = try decoder.decode(KeysignMessage.self, from: data)
+                    self.sessionID = keysignMsg.sessionID
+                    self.keysignPayload = keysignMsg.payload
+                    self.serviceName = keysignMsg.serviceName
+                    self.logger.info("QR code scanned successfully. Session ID: \(self.sessionID)")
+                    self.prepareKeysignMessages(keysignPayload: keysignMsg.payload)
+                } catch {
+                    self.errorMsg = "Error decoding keysign message: \(error.localizedDescription)"
+                    self.status = .FailedToStart
+                }
+            }
+        case .failure(let err):
+            self.errorMsg = "QR code scanning failed: \(err.localizedDescription)"
+            self.status = .FailedToStart
+        }
+        self.status = .DiscoverService
+    }
+    
+    func prepareKeysignMessages(keysignPayload: KeysignPayload) {
+        let result = keysignPayload.getKeysignMessages(vault: self.vault)
+        switch result {
+        case .success(let preSignedImageHash):
+            self.logger.info("Successfully prepared messages for keysigning.")
+            self.keysignMessages = preSignedImageHash.sorted()
+            if self.keysignMessages.isEmpty {
+                self.logger.error("")
+                self.errorMsg = "There is no messages to be signed"
+                self.status = .FailedToStart
+            }
+        case .failure(let err):
+            self.errorMsg = "Failed to prepare messages for keysigning. Error: \(err.localizedDescription)"
+            self.status = .FailedToStart
+        }
+    }
+}
