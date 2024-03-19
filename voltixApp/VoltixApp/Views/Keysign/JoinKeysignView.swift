f4da9bfe1d1ee2791276ac0363457dd8bd03886e
Amol Kumar <amolkmr10@gmail.com>
2024-03-18 20:16:53 -0400

    JoinKeysignView refactor

--- voltixApp/VoltixApp/Views/Keysign/JoinKeysignView.swift
+++ voltixApp/VoltixApp/Views/Keysign/JoinKeysignView.swift
@@ -8,69 +8,27 @@ import SwiftUI
 
 struct JoinKeysignView: View {
     let vault: Vault
-    private let logger = Logger(subsystem: "join-keysign", category: "communication")
        
     @StateObject private var serviceDelegate = ServiceDelegate()
     @StateObject var viewModel = JoinKeysignViewModel()
     
-    var background: some View {
-        Color.backgroundBlue
-            .ignoresSafeArea()
-    }
+    let logger = Logger(subsystem: "join-keysign", category: "communication")
 
     var body: some View {
         ZStack {
-            self.background
-            VStack {
-                switch self.viewModel.status {
-                case .DiscoverSigningMsg:
-                    self.discoveringSignMessage
-                case .DiscoverService:
-                    self.discoverService
-                case .JoinKeysign:
-                    self.keysignMessageConfirm
-                case .WaitingForKeysignToStart:
-                    self.waitingForKeySignStart
-                case .KeysignStarted:
-                    HStack {
-                        if self.serviceDelegate.serverURL != nil && !self.viewModel.sessionID.isEmpty {
-                            KeysignView(vault: self.vault,
-                                        keysignCommittee: self.viewModel.keysignCommittee,
-                                        mediatorURL: self.serviceDelegate.serverURL ?? "",
-                                        sessionID: self.viewModel.sessionID,
-                                        keysignType: self.viewModel.keysignPayload?.coin.chain.signingKeyType ?? .ECDSA,
-                                        messsageToSign: self.viewModel.keysignMessages,
-                                        keysignPayload: self.viewModel.keysignPayload)
-                        } else {
-                            Text("Unable to start the keysign process due to missing information.")
-                                .font(.body15MenloBold)
-                                .multilineTextAlignment(.center)
-                        }
-                    }.navigationBarBackButtonHidden(true)
-                case .FailedToStart:
-                    VStack {
-                        Text(NSLocalizedString("keysignFail", comment: "Failed to start the keysign process"))
-                            .font(.body15MenloBold)
-                            .multilineTextAlignment(.center)
-                        Text(self.viewModel.errorMsg).font(.body15MenloBold)
-                            .multilineTextAlignment(.center)
-                    }
-                }
-            }
-            .padding()
-            .cornerRadius(10)
-            .shadow(radius: 5)
+            background
+            states
         }
         .navigationTitle(NSLocalizedString("joinKeySign", comment: "Join Keysign"))
-        .sheet(isPresented: $viewModel.isShowingScanner, content: {
-            CodeScannerView(codeTypes: [.qr], completion: self.viewModel.handleScan)
-        })
         .navigationBarBackButtonHidden(false)
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
                 NavigationButtons.questionMarkButton
             }
         }
+        .sheet(isPresented: $viewModel.isShowingScanner, content: {
+            CodeScannerView(codeTypes: [.qr], completion: self.viewModel.handleScan)
+        })
         .onAppear {
             self.viewModel.setData(vault: self.vault, serviceDelegate: self.serviceDelegate)
         }
@@ -79,54 +37,94 @@ struct JoinKeysignView: View {
         }
     }
     
+    var background: some View {
+        Color.backgroundBlue
+            .ignoresSafeArea()
+    }
+    
+    var states: some View {
+        VStack {
+            switch self.viewModel.status {
+            case .DiscoverSigningMsg:
+                self.discoveringSignMessage
+            case .DiscoverService:
+                self.discoverService
+            case .JoinKeysign:
+                self.keysignMessageConfirm
+            case .WaitingForKeysignToStart:
+                self.waitingForKeySignStart
+            case .KeysignStarted:
+                keysignStartedView
+            case .FailedToStart:
+                keysignFailedText
+            }
+        }
+        .padding()
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+    
+    var keysignStartedView: some View {
+        HStack {
+            if self.serviceDelegate.serverURL != nil && !self.viewModel.sessionID.isEmpty {
+                keysignView
+            } else {
+                Text("Unable to start the keysign process due to missing information.")
+                    .font(.body15MenloBold)
+                    .multilineTextAlignment(.center)
+            }
+        }
+    }
+    
+    var keysignView: some View {
+        KeysignView(
+            vault: self.vault,
+            keysignCommittee: self.viewModel.keysignCommittee,
+            mediatorURL: self.serviceDelegate.serverURL ?? "",
+            sessionID: self.viewModel.sessionID,
+            keysignType: self.viewModel.keysignPayload?.coin.chain.signingKeyType ?? .ECDSA,
+            messsageToSign: self.viewModel.keysignMessages,
+            keysignPayload: self.viewModel.keysignPayload
+        )
+    }
+    
+    var keysignFailedText: some View {
+        VStack {
+            Text(NSLocalizedString("keysignFail", comment: "Failed to start the keysign process"))
+            Text(self.viewModel.errorMsg)
+        }
+        .font(.body15MenloBold)
+        .multilineTextAlignment(.center)
+    }
+    
     var keysignMessageConfirm: some View {
         VStack(alignment: .leading) {
-            VStack {
-                VStack(alignment: .center) {
-                    Text("Confirm to sign the message?")
-                        .font(.body15MenloBold)
-                        .foregroundColor(.neutral0)
-                        .multilineTextAlignment(.center)
-                }
+            Text("Confirm to sign the message?")
                 .frame(maxWidth: .infinity)
-            }
-            .frame(maxWidth: .infinity)
             
             Divider()
             
             HStack {
                 Text("To: ")
-                    .font(.body15MenloBold)
-                    .foregroundColor(.neutral0)
-                    .multilineTextAlignment(.center)
-                
                 Text("\(self.viewModel.keysignPayload?.toAddress ?? "")")
-                    .font(.body15MenloBold)
-                    .foregroundColor(.neutral0)
-                    .multilineTextAlignment(.center)
             }
             .padding(.vertical)
             
             Text("Amount: \(String(self.viewModel.keysignPayload?.toAmount ?? 0))")
-                .font(.body15MenloBold)
-                .foregroundColor(.neutral0)
-                .multilineTextAlignment(.center)
                 .padding(.vertical)
             
-            VStack {
-                VStack(alignment: .center) {
-                    Button(action: {
-                        self.viewModel.joinKeysignCommittee()
-                    }) {
-                        FilledButton(title: "joinKeySign")
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                }
-                .frame(maxWidth: .infinity)
+            Button(action: {
+                self.viewModel.joinKeysignCommittee()
+            }) {
+                FilledButton(title: "joinKeySign")
             }
+            .buttonStyle(PlainButtonStyle())
             .frame(maxWidth: .infinity)
             .padding()
             .padding(.vertical, 15)
+            .font(.body15MenloBold)
+            .foregroundColor(.neutral0)
+            .multilineTextAlignment(.center)
         }
     }
 
@@ -134,39 +132,35 @@ struct JoinKeysignView: View {
         VStack {
             HStack {
                 Text("thisDevice")
-                    .font(.body15MenloBold)
-                    .foregroundColor(.neutral0)
-                    .multilineTextAlignment(.center)
                 Text(self.viewModel.localPartyID)
-                    .font(.body15MenloBold)
-                    .foregroundColor(.neutral0)
-                    .multilineTextAlignment(.center)
             }
+            
             HStack {
                 Text(NSLocalizedString("waitingForKeySignStart", comment: "Waiting for the keysign process to start"))
-                    .font(.body15MenloBold)
-                    .foregroundColor(.neutral0)
-                    .multilineTextAlignment(.center)
                 ProgressView()
                     .progressViewStyle(.circular)
                     .padding(2)
             }
-        }.task {
+        }
+        .font(.body15MenloBold)
+        .foregroundColor(.neutral0)
+        .multilineTextAlignment(.center)
+        .task {
             await self.viewModel.waitForKeysignStart()
         }
     }
     
     var discoveringSignMessage: some View {
-        VStack {
+        VStack(spacing: 24) {
             Text(NSLocalizedString("scanQRCodeJoinKeygen", comment: "Scan the barcode on another VoltixApp device to start"))
                 .font(.body15MenloBold)
                 .foregroundColor(.neutral0)
                 .multilineTextAlignment(.center)
             
             Button(action: {
-                self.viewModel.startScan()
+                viewModel.startScan()
             }) {
-                self.scanButton
+                scanButton
             }
         }
     }
@@ -175,19 +169,11 @@ struct JoinKeysignView: View {
         VStack {
             HStack {
                 Text("thisDevice")
-                    .font(.body15MenloBold)
-                    .foregroundColor(.neutral0)
-                    .multilineTextAlignment(.center)
                 Text(self.viewModel.localPartyID)
-                    .font(.body15MenloBold)
-                    .foregroundColor(.neutral0)
-                    .multilineTextAlignment(.center)
             }
+            
             HStack {
                 Text(NSLocalizedString("discoveringMediator", comment: "Discovering mediator service, please wait..."))
-                    .foregroundColor(.neutral0)
-                    .font(.body15MenloBold)
-                    .multilineTextAlignment(.center)
                 
                 if self.serviceDelegate.serverURL == nil {
                     ProgressView().progressViewStyle(.circular).padding(2)
@@ -197,19 +183,18 @@ struct JoinKeysignView: View {
                     }
                 }
             }
-        }.padding(.vertical, 30)
-            .onAppear {
-                self.viewModel.discoverService()
-            }
+        }
+        .font(.body15MenloBold)
+        .foregroundColor(.neutral0)
+        .multilineTextAlignment(.center)
+        .padding(.vertical, 30)
+        .onAppear {
+            self.viewModel.discoverService()
+        }
     }
     
     var scanButton: some View {
         ZStack {
-            Circle()
-                .foregroundColor(.blue800)
-                .frame(width: 80, height: 80)
-                .opacity(0.8)
-            
             Circle()
                 .foregroundColor(.turquoise600)
                 .frame(width: 60, height: 60)
--- voltixApp/VoltixApp/Views/Vault/VaultDetailView.swift
+++ voltixApp/VoltixApp/Views/Vault/VaultDetailView.swift
@@ -1,4 +1,5 @@
 //
+//
 //  VaultDetailView.swift
 //  VoltixApp
 //
