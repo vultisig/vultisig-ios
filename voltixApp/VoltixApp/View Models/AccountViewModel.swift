9718f861fb66205a7d272c4edce662a5295ac4f0
Amol Kumar <amolkmr10@gmail.com>
2024-03-31 19:21:39 -0400

    Refactor

--- voltixApp/VoltixApp/View Models/AccountViewModel.swift
+++ voltixApp/VoltixApp/View Models/AccountViewModel.swift
@@ -13,45 +13,48 @@ class AccountViewModel: ObservableObject {
     @AppStorage("showOnboarding") var showOnboarding: Bool = true
     @AppStorage("isAuthenticationEnabled") var isAuthenticationEnabled: Bool = true
     
-    @Published var isAuthenticated: Bool = false
+    @Published var isAuthenticated = false
     @Published var showSplashView = true
-    @Published var didUserCancelAuthentication: Bool = false
+    @Published var didUserCancelAuthentication = false
     
     func authenticateUser() {
         let context = LAContext()
         var error: NSError?
 
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            
-            if context.biometryType == .faceID {
-                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Authenticate to check Face ID") { success, error in
-                    DispatchQueue.main.async {
-                        if success {
-                            self.isAuthenticated = true
-                            self.showSplashView = false
-                            self.isAuthenticationEnabled = true
-                            self.didUserCancelAuthentication = false
-                        } else {
-                            if let error = error as? LAError {
-                                switch error.code {
-                                case .biometryLockout, .biometryNotEnrolled, .biometryNotAvailable:
-                                    self.isAuthenticationEnabled = false
-                                    self.showSplashView = false
-                                default:
-                                    self.isAuthenticationEnabled = true
-                                    self.showSplashView = true
-                                }
+            authenticate(context)
+        } else {
+            isAuthenticationEnabled = false
+            isAuthenticated = false
+            showSplashView = false
+            didUserCancelAuthentication = false
+        }
+    }
+    
+    private func authenticate(_ context: LAContext) {
+        if context.biometryType == .faceID {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Authenticate to check Face ID") { success, error in
+                DispatchQueue.main.async {
+                    if success {
+                        self.isAuthenticated = true
+                        self.showSplashView = false
+                        self.isAuthenticationEnabled = true
+                        self.didUserCancelAuthentication = false
+                    } else {
+                        if let error = error as? LAError {
+                            switch error.code {
+                            case .biometryLockout, .biometryNotEnrolled, .biometryNotAvailable:
+                                self.isAuthenticationEnabled = false
+                                self.showSplashView = false
+                            default:
+                                self.isAuthenticationEnabled = true
+                                self.showSplashView = true
                             }
-                            self.isAuthenticated = false
-                            self.didUserCancelAuthentication = true
                         }
+                        self.isAuthenticated = false
+                        self.didUserCancelAuthentication = true
                     }
                 }
-            } else {
-                isAuthenticationEnabled = false
-                isAuthenticated = false
-                showSplashView = false
-                didUserCancelAuthentication = false
             }
         } else {
             isAuthenticationEnabled = false
