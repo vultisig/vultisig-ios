4577aa829ddfad5aafd56a16956b2605a5a1fcfa
Amol Kumar <amolkmr10@gmail.com>
2024-04-05 15:26:10 -0400

    SettingsViewModel set up

---
+++ voltixApp/VoltixApp/Stores/SettingsOptionsStore.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsOptionsStore.swift
+//  VoltixApp
+//
+//  Created by Amol Kumar on 2024-04-05.
+//
+
+import Foundation
+
+enum SettingsLanguage: String {
+    case English = "English (UK)"
+    case Deutsch = "German"
+    case Espanol = "Spanish"
+    case Italiano = "Italian"
+    case Hrvatski = "Croatian"
+}
---
+++ voltixApp/VoltixApp/View Models/SettingsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewModel.swift
+//  VoltixApp
+//
+//  Created by Amol Kumar on 2024-04-05.
+//
+
+import SwiftUI
+
+@MainActor
+class SettingsViewModel: ObservableObject {
+    @Published var selectedLanguage: SettingsLanguage = .English
+}
