395cfaab7451456911d6f63926c532c70906002b
Artur Guseinov <arthur.guseinov@gmail.com>
2024-04-08 02:06:58 +0300

    Swap verify

--- voltixApp/VoltixApp/View Models/SwapCryptoViewModel.swift
+++ voltixApp/VoltixApp/View Models/SwapCryptoViewModel.swift
@@ -11,47 +11,16 @@ import BigInt
 @MainActor
 class SwapCryptoViewModel: ObservableObject {
 
-    private let thorchainService = ThorchainService.shared
-    private let balanceService = BalanceService.shared
-
     private let titles = ["send", "verify", "pair", "keysign", "done"]
 
-    @Published var fromCoin: Coin = .example {
-        didSet {
-            updateFromBalance()
-            updateQuote()
-        }
-    }
-
-    @Published var toCoin: Coin = .example {
-        didSet {
-            updateToBalance()
-            updateQuote()
-        }
-    }
-
-    @Published var fromAmount: String = .empty {
-        didSet {
-            updateQuote()
-        }
-    }
-
     @Published var coins: [Coin] = []
-    @Published var toAmount: String = .empty
-    @Published var fromBalance: String = .empty
-    @Published var toBalance: String = .empty
-    @Published var gas: String = .empty
     @Published var currentIndex = 1
     @Published var currentTitle = "send"
 
-    var feeString: String {
-        return "\(gas) \(fromCoin.feeUnit)"
-    }
-
-    func load(fromCoin: Coin, coins: [Coin]) async throws {
+    func load(tx: SwapTransaction, fromCoin: Coin, coins: [Coin]) {
         self.coins = coins.filter { $0.chain.isSwapSupported }
-        self.fromCoin = fromCoin
-        self.toCoin = coins.first!
+        tx.fromCoin = fromCoin
+        tx.toCoin = coins.first!
     }
 
     // MARK: Progress
@@ -60,50 +29,16 @@ class SwapCryptoViewModel: ObservableObject {
         return Double(currentIndex) / Double(titles.count)
     }
 
+    func validateForm(tx: SwapTransaction) -> Bool {
+        return tx.fromCoin != tx.toCoin
+            && tx.fromCoin != .example
+            && tx.toCoin != .example
+            && !tx.fromAmount.isEmpty
+            && !tx.toAmount.isEmpty
+    }
+
     func moveToNextView() {
         currentIndex += 1
         currentTitle = titles[currentIndex-1]
-    }
-}
-
-private extension SwapCryptoViewModel {
-
-    enum Errors: Error {
-        case swapQuoteParsingFailed
-    }
-
-    func updateFromBalance() {
-        Task { fromBalance = try await fetchBalance(coin: fromCoin) }
-    }
-
-    func updateToBalance() {
-        Task { toBalance = try await fetchBalance(coin: toCoin) }
-    }
-
-    func updateQuote() {
-        Task { try await fetchQuotes() }
-    }
-
-    func fetchQuotes() async throws {
-        guard let amount = Decimal(string: fromAmount), fromCoin.swapAsset != toCoin.swapAsset else {
-            throw Errors.swapQuoteParsingFailed
-        }
-
-        let quote = try await thorchainService.fetchSwapQuotes(
-            address: toCoin.address,
-            fromAsset: fromCoin.swapAsset,
-            toAsset: toCoin.swapAsset,
-            amount: (amount * 100_000_000).description // https://dev.thorchain.org/swap-guide/quickstart-guide.html#admonition-info-2
-        )
-
-        guard let expected = Decimal(string: quote.expectedAmountOut) else {
-            throw Errors.swapQuoteParsingFailed
-        }
-
-        toAmount = (expected / Decimal(100_000_000)).description
-    }
-
-    func fetchBalance(coin: Coin) async throws -> String {
-        return try await balanceService.balance(for: coin).coinBalance
     }
 }
---
+++ voltixApp/VoltixApp/Views/Swap/SwapTransaction.swift
@@ -0,0 +1,119 @@
+//
+//  SwapCryptoTransaction.swift
+//  VoltixApp
+//
+//  Created by Artur Guseinov on 08.04.2024.
+//
+
+import Foundation
+import WalletCore
+
+@MainActor
+class SwapTransaction: ObservableObject {
+
+    private let thorchainService = ThorchainService.shared
+    private let balanceService = BalanceService.shared
+
+    @Published var fromCoin: Coin = .example {
+        didSet {
+            updateFromBalance()
+            updateQuote()
+        }
+    }
+
+    @Published var toCoin: Coin = .example {
+        didSet {
+            updateToBalance()
+            updateQuote()
+        }
+    }
+
+    @Published var fromAmount: String = .empty {
+        didSet {
+            updateQuote()
+        }
+    }
+
+    @Published var toAmount: String = .empty
+    @Published var gas: String = .empty
+
+    @Published var fromBalance: String = .empty
+    @Published var toBalance: String = .empty
+
+    var feeString: String {
+        return "\(gas) \(fromCoin.feeUnit)"
+    }
+}
+
+private extension SwapTransaction {
+
+    enum Errors: Error {
+        case swapQuoteParsingFailed
+    }
+
+    func updateFromBalance() {
+        Task { fromBalance = try await fetchBalance(coin: fromCoin) }
+    }
+
+    func updateToBalance() {
+        Task { toBalance = try await fetchBalance(coin: toCoin) }
+    }
+
+    func updateQuote() {
+        Task { try await fetchQuotes() }
+    }
+
+    func fetchQuotes() async throws {
+        guard let amount = Decimal(string: fromAmount), fromCoin != toCoin else {
+            throw Errors.swapQuoteParsingFailed
+        }
+
+        let quote = try await thorchainService.fetchSwapQuotes(
+            address: toCoin.address,
+            fromAsset: fromCoin.swapAsset,
+            toAsset: toCoin.swapAsset,
+            amount: (amount * 100_000_000).description // https://dev.thorchain.org/swap-guide/quickstart-guide.html#admonition-info-2
+        )
+
+        guard let expected = Decimal(string: quote.expectedAmountOut) else {
+            throw Errors.swapQuoteParsingFailed
+        }
+
+        toAmount = (expected / Decimal(100_000_000)).description
+    }
+
+    func fetchBalance(coin: Coin) async throws -> String {
+        return try await balanceService.balance(for: coin).coinBalance
+    }
+
+    func swapAsset(for coin: Coin) -> THORChainSwapAsset {
+        return THORChainSwapAsset.with {
+            switch coin.chain {
+            case .thorChain:
+                $0.chain = .thor
+            case .ethereum:
+                $0.chain = .eth
+            case .avalanche:
+                $0.chain = .avax
+            case .bscChain:
+                $0.chain = .bsc
+            case .bitcoin:
+                $0.chain = .btc
+            case .bitcoinCash:
+                $0.chain = .bch
+            case .litecoin:
+                $0.chain = .ltc
+            case .dogecoin:
+                $0.chain = .doge
+            case .gaiaChain:
+                $0.chain = .atom
+            case .solana: break
+            }
+            $0.symbol = coin.ticker
+            if !coin.isNativeToken {
+                $0.tokenID = coin.contractAddress
+            }
+        }
+    }
+}
+
