689aad59297c72d75b3390d91e29f61a05d5c119
Johnny Luo <johnnyluo1980@gmail.com>
2024-04-14 16:20:56 +1000

    Add Premium support

---
+++ voltixApp/VoltixApp/Stores/VoltixPremium.swift
@@ -0,0 +1,16 @@
+//
+//  VoltixPremium.swift
+//  VoltixApp
+//
+//  Created by Johnny Luo on 14/4/2024.
+//
+
+import Foundation
+
+class VoltixPremium {
+    var IsPremiumEnabled: Bool {
+        get {
+            return true
+        }
+    }
+}
