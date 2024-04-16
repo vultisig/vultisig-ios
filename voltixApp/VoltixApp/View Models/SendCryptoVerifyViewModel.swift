a40f5f8b4f494c4d521c0b1c93a99f57a0e4fcd2
Johnny Luo <johnnyluo1980@gmail.com>
2024-04-16 12:26:37 +1000

    Change the way how we build keysign payload

--- voltixApp/VoltixApp/View Models/SendCryptoVerifyViewModel.swift
+++ voltixApp/VoltixApp/View Models/SendCryptoVerifyViewModel.swift
@@ -24,6 +24,7 @@ class SendCryptoVerifyViewModel: ObservableObject {
     @Published var utxo = BlockchairService.shared
     @Published var eth = EthService.shared
     var gaia = GaiaService.shared
+    let blockChainService = BlockChainService.shared
     
     var THORChainAccount: THORChainAccountValue? = nil
     var CosmosChainAccount: CosmosAccountValue? = nil
@@ -32,6 +33,24 @@ class SendCryptoVerifyViewModel: ObservableObject {
         return isAddressCorrect && isAmountCorrect && isHackedOrPhished
     }
     
+    func amount(for coin: Coin, tx: SendTransaction) -> Int64 {
+        switch coin.chain {
+        case .thorChain:
+            return tx.amountInSats
+        case .ethereum, .avalanche, .bscChain:
+            if coin.isNativeToken {
+                return tx.amountInGwei
+            } else {
+                return tx.amountInTokenWeiInt64
+            }
+        case .bitcoin, .bitcoinCash, .litecoin, .dogecoin:
+            return tx.amountInSats
+        case .gaiaChain:
+            return tx.amountInCoinDecimal
+        case .solana:
+            return tx.amountInLamports
+        }
+    }
     func validateForm(tx: SendTransaction) async -> KeysignPayload? {
         
         if !isValidForm {
@@ -40,191 +59,39 @@ class SendCryptoVerifyViewModel: ObservableObject {
             isLoading = false
             return nil
         }
-        
+        var keysignPayload: KeysignPayload?
         if tx.coin.chain.chainType == ChainType.UTXO {
             do{
                 _ = try await utxo.fetchBlockchairData(coin: tx.coin)
             }catch{
                 print("fail to fetch utxo data from blockchair , error:\(error.localizedDescription)")
             }
-            let coinName = tx.coin.chain.name.lowercased()
-            let key: String = "\(tx.fromAddress)-\(coinName)"
-            
-            let totalAmountNeeded = tx.amountInSats + tx.feeInSats
-            
-            guard let utxoInfo = utxo.blockchairData[key]?.selectUTXOsForPayment(amountNeeded: Int64(totalAmountNeeded)).map({
-                UtxoInfo(
-                    hash: $0.transactionHash ?? "",
-                    amount: Int64($0.value ?? 0),
-                    index: UInt32($0.index ?? -1)
-                )
-            }), !utxoInfo.isEmpty else {
-                self.errorMessage = "notEnoughBalanceError"
-                showAlert = true
-                isLoading = false
-                return nil
-            }
-            
-            let totalSelectedAmount = utxoInfo.reduce(0) { $0 + $1.amount }
-            
-            if totalSelectedAmount < Int64(totalAmountNeeded) {
-                self.errorMessage = "notEnoughBalanceError"
-                showAlert = true
-                isLoading = false
-                return nil
-            }
-            
-            let keysignPayload = KeysignPayload(
-                coin: tx.coin,
-                toAddress: tx.toAddress,
-                toAmount: tx.amountInSats,
-                chainSpecific: BlockChainSpecific.UTXO(byteFee: tx.feeInSats),
-                utxos: utxoInfo,
-                memo: tx.memo,
-                swapPayload: nil
-            )
-            
-            self.errorMessage = ""
-            return keysignPayload
-            
-            
-            
-        } else if tx.coin.chain.chainType == ChainType.EVM {
-            if tx.coin.isNativeToken {
-                let keysignPayload = KeysignPayload(
-                    coin: tx.coin,
-                    toAddress: tx.toAddress,
-                    toAmount: tx.amountInGwei, // in Gwei
-                    chainSpecific: BlockChainSpecific.Ethereum(maxFeePerGasGwei: Int64(tx.gas) ?? 24, priorityFeeGwei: tx.priorityFeeGwei, nonce: tx.nonce, gasLimit: EVMHelper.defaultETHTransferGasUnit),
-                    utxos: [],
-                    memo: nil,
-                    swapPayload: nil
-                )
-                
-                return keysignPayload
-            } else {
-                
-                let keysignPayload = KeysignPayload(
-                    coin: tx.coin,
-                    toAddress: tx.toAddress,
-                    toAmount: tx.amountInTokenWeiInt64, // The amount must be in the token decimals
-                    chainSpecific: BlockChainSpecific.ERC20(maxFeePerGasGwei: Int64(tx.gas) ?? 42, priorityFeeGwei: tx.priorityFeeGwei, nonce: tx.nonce, gasLimit: EVMHelper.defaultERC20TransferGasUnit, contractAddr: tx.coin.contractAddress),
-                    utxos: [],
-                    memo: nil,
-                    swapPayload: nil
-                )
-                
-                return keysignPayload
-            }
-        } else if tx.coin.chain == .thorChain {
-            do{
-                self.THORChainAccount = try await thor.fetchAccountNumber(tx.fromAddress)
-            } catch {
-                print(" fail to fetch thorchain account number, error: \(error.localizedDescription)")
-            }
-            guard let accountNumberString = THORChainAccount?.accountNumber, let intAccountNumber = UInt64(accountNumberString) else {
-                print("We need the ACCOUNT NUMBER to broadcast a transaction")
-                self.errorMessage = "failToGetAccountNumber"
-                showAlert = true
-                isLoading = false
-                return nil
-            }
-            
-            var sequenceString = "0"
-            if THORChainAccount?.sequence != nil {
-                sequenceString = THORChainAccount!.sequence!
-            }
-            guard  let intSequence = UInt64(sequenceString) else {
-                print("We need the SEQUENCE to broadcast a transaction")
-                self.errorMessage = "failToGetSequenceNo"
-                showAlert = true
-                isLoading = false
-                return nil
-            }
-            
-            let keysignPayload = KeysignPayload(
-                coin: tx.coin,
-                toAddress: tx.toAddress,
-                toAmount: tx.amountInSats,
-                chainSpecific: BlockChainSpecific.THORChain(accountNumber: intAccountNumber, sequence: intSequence),
-                utxos: [],
-                memo: tx.memo, swapPayload: nil
-            )
-            
-            return keysignPayload
-            
-        } else if tx.coin.chain == .gaiaChain {
-            do{
-                self.CosmosChainAccount = try await gaia.fetchAccountNumber(tx.fromAddress)
-            }catch{
-                print("fail to fetch gaia account number,error: \(error.localizedDescription)")
-            }
-            guard let accountNumberString = CosmosChainAccount?.accountNumber, let intAccountNumber = UInt64(accountNumberString) else {
-                self.errorMessage = "failToGetAccountNumber"
-                showAlert = true
-                isLoading = false
-                return nil
-            }
-            
-            var sequenceString = "0"
-            if CosmosChainAccount?.sequence != nil {
-                sequenceString = CosmosChainAccount!.sequence!
-            }
-            guard  let intSequence = UInt64(sequenceString) else {
-                print("We need the SEQUENCE to broadcast a transaction")
-                self.errorMessage = "failToGetSequenceNo"
-                showAlert = true
-                isLoading = false
-                return nil
-            }
-            
-            let keysignPayload = KeysignPayload(
-                coin: tx.coin,
-                toAddress: tx.toAddress,
-                toAmount: tx.amountInCoinDecimal,
-                chainSpecific: BlockChainSpecific.Cosmos(accountNumber: intAccountNumber, sequence: intSequence, gas: 7500),
-                utxos: [],
-                memo: tx.memo, swapPayload: nil
-            )
-            
-            return keysignPayload
-            
-        } else if tx.coin.chain == .solana {
-            do {
-                async let recentBlockHashPromise = sol.fetchRecentBlockhash()
-                async let highPriorityFeePromise = sol.fetchHighPriorityFee(account: tx.coin.address)
-                
-                let (recentBlockHash, feeInLamports) = try await recentBlockHashPromise
-                let highPriorityFee = try await highPriorityFeePromise
-                
-                tx.gas = String(feeInLamports)
-                guard let recentBlockHash else {
-                    print("We need the recentBlockHash to broadcast a transaction")
-                    self.errorMessage = "failToGetRecentBlockHash"
-                    showAlert = true
-                    isLoading = false
-                    return nil
-                }
-                
-                let keysignPayload = KeysignPayload(
-                    coin: tx.coin,
-                    toAddress: tx.toAddress,
-                    toAmount: tx.amountInLamports,
-                    chainSpecific: BlockChainSpecific.Solana(recentBlockHash: recentBlockHash, priorityFee: highPriorityFee, feeInLamports: feeInLamports),
-                    utxos: [],
-                    memo: tx.memo, swapPayload: nil
-                )
-                return keysignPayload
-            } catch {
-                print("fail to get recent blockhash, error:\(error.localizedDescription)")
-                self.errorMessage = "failToGetRecentBlockHash"
-                showAlert = true
-                isLoading = false
-                return nil
-            }
-            
-            
         }
-        return nil
+        do{
+            let chainSpecific = try await blockChainService.fetchSpecific(for: tx.coin)
+            let keysignPayloadFactory = KeysignPayloadFactory()
+            keysignPayload = try await keysignPayloadFactory.buildTransfer(coin: tx.coin, 
+                                                                           toAddress: tx.fromAddress,
+                                                                           amount: amount(for:tx.coin,tx:tx),
+                                                                           memo: tx.memo,
+                                                                           chainSpecific: chainSpecific)
+        } catch {
+            switch error {
+            case KeysignPayloadFactory.Errors.notEnoughBalanceError:
+                self.errorMessage = "notEnoughBalanceError"
+            case KeysignPayloadFactory.Errors.failToGetSequenceNo:
+                self.errorMessage = "failToGetSequenceNo"
+            case KeysignPayloadFactory.Errors.failToGetAccountNumber:
+                self.errorMessage = "failToGetAccountNumber"
+            case KeysignPayloadFactory.Errors.failToGetRecentBlockHash:
+                self.errorMessage = "failToGetRecentBlockHash"
+            default:
+                self.errorMessage = error.localizedDescription
+            }
+            showAlert = true
+            isLoading = false
+            return nil
+        }
+        return keysignPayload
     }
 }
