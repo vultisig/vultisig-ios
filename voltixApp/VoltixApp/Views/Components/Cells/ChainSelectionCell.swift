9e556ed8a8feb76a229e8cf52750f3d2d8911e2e
Amol Kumar <amolkmr10@gmail.com>
2024-04-23 16:39:21 -0400

    Preconditon added fpr removing or adding chains

--- voltixApp/VoltixApp/Views/Components/Cells/ChainSelectionCell.swift
+++ voltixApp/VoltixApp/Views/Components/Cells/ChainSelectionCell.swift
@@ -7,110 +7,12 @@
 
 import SwiftUI
 
-//struct ChainSelectionCell: View {
-//    let assets: [Coin]
-//    @Binding var showAlert: Bool
-//    
-//    @State var isSelected = false
-//    @State var asset: Coin? = nil
-//    
-//    @EnvironmentObject var tokenSelectionViewModel: TokenSelectionViewModel
-//    
-//    var body: some View {
-//        HStack(spacing: 16) {
-//            image
-//            text
-//            Spacer()
-//            toggleContent
-//        }
-//        .frame(height: 72)
-//        .padding(.horizontal, 16)
-//        .background(Color.blue600)
-//        .cornerRadius(10)
-//        .redacted(reason: asset==nil ? .placeholder : [])
-//        .onAppear {
-//            setData()
-//        }
-//    }
-//    
-//    var image: some View {
-//        Image(asset?.logo ?? "Logo")
-//            .resizable()
-//            .frame(width: 32, height: 32)
-//            .cornerRadius(100)
-//    }
-//    
-//    var text: some View {
-//        VStack(alignment: .leading, spacing: 6) {
-//            Text(asset?.ticker ?? "Ticker")
-//                .font(.body16MontserratBold)
-//                .foregroundColor(.neutral0)
-//            
-//            Text(asset?.chain.name ?? "Name")
-//                .font(.body12MontserratSemiBold)
-//                .foregroundColor(.neutral0)
-//        }
-//    }
-//    
-//    var toggleContent: some View {
-//        ZStack {
-//            if assets.count>1, isSelected {
-//                disabledToggle
-//            } else {
-//                enabledToggle
-//            }
-//        }
-//    }
-//    
-//    var enabledToggle: some View {
-//        toggle
-//    }
-//    
-//    var disabledToggle: some View {
-//        Button {
-//            showAlert = true
-//        } label: {
-//            toggle
-//                .disabled(true)
-//        }
-//    }
-//    
-//    var toggle: some View {
-//        Toggle("Is selected", isOn: $isSelected)
-//            .labelsHidden()
-//            .scaleEffect(0.6)
-//    }
-//    
-//    private func setData() {
-//        asset = assets.first ?? Coin.example
-//        
-//        guard let asset else {
-//            return
-//        }
-//        
-//        if tokenSelectionViewModel.selection.contains(asset) {
-//            isSelected = true
-//        } else {
-//            isSelected = false
-//        }
-//    }
-//    
-//    private func handleSelection(_ isSelected: Bool) {
-//        guard let asset else {
-//            return
-//        }
-//        
-//        tokenSelectionViewModel.handleSelection(isSelected: isSelected, asset: asset)
-//    }
-//}
-
-import SwiftUI
-
 struct ChainSelectionCell: View {
     let assets: [Coin]
     @Binding var showAlert: Bool
     
     @State var isSelected = false
+    @State var selectedTokensCount = 0
     @EnvironmentObject var tokenSelectionViewModel: TokenSelectionViewModel
     
     var body: some View {
@@ -125,7 +27,7 @@ struct ChainSelectionCell: View {
     
     var content: some View {
         ZStack {
-            if assets.count>1, isSelected {
+            if selectedTokensCount>1, isSelected {
                 disabledContent
             } else {
                 enabledContent
@@ -162,6 +64,17 @@ struct ChainSelectionCell: View {
             isSelected = true
         } else {
             isSelected = false
+        }
+        
+        countSelectedToken()
+    }
+    
+    private func countSelectedToken() {
+        selectedTokensCount = 0
+        for asset in assets {
+            if tokenSelectionViewModel.selection.contains(asset) {
+                selectedTokensCount += 1
+            }
         }
     }
 }
