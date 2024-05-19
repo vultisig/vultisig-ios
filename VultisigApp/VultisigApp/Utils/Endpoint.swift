a2b020fb3db91fa0a11dc6b0104f2aa441286b18
Johnny Luo <johnnyluo1980@gmail.com>
2024-05-19 20:32:55 +1000

    update build no and affiliate fee address

--- VultisigApp/VultisigApp/Utils/Endpoint.swift
+++ VultisigApp/VultisigApp/Utils/Endpoint.swift
@@ -31,7 +31,7 @@ class Endpoint {
         "https://mayanode.mayachain.info/mayachain/quote/swap?from_asset=\(fromAsset)&to_asset=\(toAsset)&amount=\(amount)&destination=\(address)&streaming_interval=\(interval)".asUrl
     }
     static func fetchSwaoQuoteThorchainNineRealms(address: String, fromAsset: String, toAsset: String, amount: String, interval: String) -> URL {
-        "https://thornode.ninerealms.com/thorchain/quote/swap?from_asset=\(fromAsset)&to_asset=\(toAsset)&amount=\(amount)&destination=\(address)&streaming_interval=\(interval)&affiliate_bps=70".asUrl
+        "https://thornode.ninerealms.com/thorchain/quote/swap?from_asset=\(fromAsset)&to_asset=\(toAsset)&amount=\(amount)&destination=\(address)&streaming_interval=\(interval)&affiliate=vi&affiliate_bps=70".asUrl
     }
 
     static func fetch1InchSwapQuote(chain: String, source: String, destination: String, amount: String, from: String, slippage: String, referrer: String, fee: Double) -> URL {
