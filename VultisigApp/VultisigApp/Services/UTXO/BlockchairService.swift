0352e537ebb7f118f7f2c5a6c731d6877d92d50e
Enrique Souza <enriquesouza@live.com>
2024-05-28 23:39:06 -0300

    revert utxo

--- VultisigApp/VultisigApp/Services/UTXO/BlockchairService.swift
+++ VultisigApp/VultisigApp/Services/UTXO/BlockchairService.swift
@@ -11,13 +11,13 @@ import BigInt
 class BlockchairService {
     
     static let shared = BlockchairService()
-    
+
     var blockchairData: ThreadSafeDictionary<String,Blockchair> = ThreadSafeDictionary()
-    
+
     private var cacheFeePrice: [String: (data: BigInt, timestamp: Date)] = [:]
-    
+
     private init() {}
-    
+
     func fetchBlockchairData(coin: Coin) async throws -> Blockchair? {
         let coinName = coin.chain.name.lowercased()
         let url = Endpoint.blockchairDashboard(coin.address, coinName)
@@ -46,8 +46,7 @@ class BlockchairService {
         let data = try await Utils.asyncGetRequest(urlString: urlString, headers: [:])
         if let result = Utils.extractResultFromJson(fromData: data, path: "data.suggested_transaction_fee_per_byte_sat"),
            let resultNumber = result as? NSNumber {
-            // Apply the 1.5 factor to the fetched sats price
-            let bigIntResult = BigInt(Double(resultNumber.intValue) * 1.5)
+            let bigIntResult = BigInt(resultNumber.intValue)
             self.cacheFeePrice[cacheKey] = (data: bigIntResult, timestamp: Date())
             return bigIntResult
         } else {
@@ -55,5 +54,4 @@ class BlockchairService {
         }
         return BigInt(-1)
     }
-    
 }
