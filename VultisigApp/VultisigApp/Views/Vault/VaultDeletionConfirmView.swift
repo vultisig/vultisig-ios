9cf5676815316c9553557f0d6e5af90e8f589f53
Amol Kumar <amolkmr10@gmail.com>
2024-06-05 00:44:48 -0400

    VaultDeletionConfirmView added

---
+++ VultisigApp/VultisigApp/Views/Vault/VaultDeletionConfirmView.swift
@@ -0,0 +1,18 @@
+//
+//  VaultDeletionConfirmView.swift
+//  VultisigApp
+//
+//  Created by Amol Kumar on 2024-06-05.
+//
+
+import SwiftUI
+
+struct VaultDeletionConfirmView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    VaultDeletionConfirmView()
+}
