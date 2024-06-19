2c5f734124de04a2fdd396d53a1d99ce17a9e953
Enrique Souza <enriquesouza@live.com>
2024-06-19 03:53:19 -0300

    bulk cache and coin paprika removal

--- VultisigApp/VultisigApp/Services/CryptoPriceService.swift
+++ VultisigApp/VultisigApp/Services/CryptoPriceService.swift
@@ -37,11 +37,29 @@ public class CryptoPriceService: ObservableObject {
         
         if let priceCoinGecko = await getAllCryptoPricesCoinGecko() {
             price = priceCoinGecko.prices[priceProviderId]?[SettingsCurrency.current.rawValue.lowercased()] ?? Double.zero
-        } else if let priceCoinPaprika = await getAllCryptoPricesCoinPaprika() {
-            price = priceCoinPaprika.prices[priceProviderId]?[SettingsCurrency.current.rawValue.lowercased()] ?? Double.zero
         }
         
         return price
+    }
+    
+    // this function should prevent any rate limit on the CoinGecko API
+    // it fetches the prices of all tokens in the vault in bulk per chain
+    private func getAllTokenPricesCoinGecko() async -> [String: Double] {
+        let tokens = getCoins().filter { !$0.isNativeToken }
+        let tokenGroups = Dictionary(grouping: tokens, by: { $0.chain })
+        
+        var allTokenPrices: [String: Double] = [:]
+        
+        for (chain, tokensInChain) in tokenGroups {
+            let contractAddresses = tokensInChain.map { $0.contractAddress }
+            let prices = await fetchCoingeckoTokenPrice(contractAddresses: contractAddresses, chain: chain)
+            for (address, price) in prices {
+                let cacheKey = getCacheTokenKey(contractAddress: address, chain: chain)
+                allTokenPrices[cacheKey] = price
+            }
+        }
+        
+        return allTokenPrices
     }
     
     func fetchCoingeckoPoolPrice(chain: Chain, contractAddress: String) async throws -> (image_url: String?, coingecko_coin_id: String?, price_usd: Double?) {
@@ -93,13 +111,16 @@ public class CryptoPriceService: ObservableObject {
     func getTokenPrice(coin: Coin) async -> Double {
         let cacheKey = getCacheTokenKey(contractAddress: coin.contractAddress, chain: coin.chain)
         
-        if let cacheEntry = await getCachedTokenPrice(contractAddress: coin.contractAddress, chain: coin.chain) {
-            print("getTokenPrice > from cache \(cacheKey)")
-            return cacheEntry
-        } else {
+        //Those tokens are the ones in the vault, so we should cache them if not cached
+        let vaultTokens = await getAllTokenPricesCoinGecko()
+        let vaultPrice = vaultTokens[coin.contractAddress]
+        
+        guard let price = vaultPrice else {
             let prices = await fetchCoingeckoTokenPrice(contractAddresses: [coin.contractAddress], chain: coin.chain)
             return prices[cacheKey] ?? .zero
         }
+        
+        return price
     }
     
     private func fetchCoingeckoTokenPrice(contractAddresses: [String], chain: Chain) async -> [String: Double] {
@@ -108,21 +129,22 @@ public class CryptoPriceService: ObservableObject {
         do {
             // Create a cache key for each contract address individually
             for address in contractAddresses {
-                let cacheKey = getCacheTokenKey(contractAddress: address, chain: chain)
-                //if let cacheEntry = cacheTokens[cacheKey], isCacheValid(for: cacheKey) {
                 if let cacheEntry = await getCachedTokenPrice(contractAddress: address, chain: chain) {
-                    print("Price from cache tokens")
-                    print("fetchCoingeckoTokenPrice > cacheKey: \(cacheKey)")
                     tokenPrices[address] = cacheEntry
-                    continue
                 }
+            }
+            
+            // If all cached, then return
+            if contractAddresses.count == tokenPrices.count {
+                return tokenPrices
             }
             
             // If no cache entry is found, fetch the prices for all contract addresses
             let urlString = Endpoint.fetchTokenPrice(network: chain.name, addresses: contractAddresses, fiat: fiat)
             let data = try await Utils.asyncGetRequest(urlString: urlString, headers: [:])
             
-            for address in contractAddresses {
+            // should not get the price to the coins already in the cache
+            for address in contractAddresses.filter({ !tokenPrices.keys.contains($0) }) {
                 if let result = Utils.extractResultFromJson(fromData: data, path: "\(address).\(fiat)"),
                    let resultNumber = result as? NSNumber {
                     let fiatPrice = Double(resultNumber.doubleValue)
@@ -147,40 +169,9 @@ public class CryptoPriceService: ObservableObject {
         return tokenPrices
     }
     
-    
     private func getAllCryptoPricesCoinGecko() async -> CryptoPrice? {
         let coins = getCoins().map { $0.priceProviderId }.joined(separator: ",")
         return await fetchAllCryptoPricesCoinGecko(for: coins, for: SettingsCurrency.current.rawValue.lowercased())
-    }
-    
-    private func getAllCryptoPricesCoinPaprika() async -> CryptoPrice? {
-        do {
-            let coins = getCoins()
-            
-            let key = coins.map { $0.priceProviderId }.joined(separator: ",")
-            
-            let fiat = SettingsCurrency.current.rawValue.lowercased()
-            
-            let cacheKey = "\(key)-\(fiat)"
-            
-            if let cacheEntry = await Utils.getCachedData(cacheKey: cacheKey, cache: cache, timeInSeconds: CACHE_TIMEOUT_IN_SECONDS) {
-                
-                print("Price from cache coin PAPRIKA")
-                return cacheEntry
-            }
-            
-            let coinPaprikaCoins = getCoinPaprikaCoins(coins: coins)
-            let coinPaprikaQuotes = try await fetchAllCryptoPricesCoinPaprika(coins: coinPaprikaCoins)
-            if let pricesCoinPaprika = coinPaprikaQuotes {
-                self.cache[cacheKey] = (data: pricesCoinPaprika, timestamp: Date())
-                return pricesCoinPaprika
-            }
-            return nil
-        } catch {
-            print(error.localizedDescription)
-        }
-        return nil
-        
     }
     
     private func fetchAllCryptoPricesCoinGecko(for coin: String = "bitcoin", for fiat: String = "usd") async -> CryptoPrice? {
@@ -206,60 +197,6 @@ public class CryptoPriceService: ObservableObject {
         } catch {
             return nil
         }
-    }
-    
-    private func fetchAllCryptoPricesCoinPaprika(coins: [CoinPaprikaCoin]) async throws -> CryptoPrice? {
-        
-        let coinPaprikaQuotes: [CoinPaprikaQuote] = try await Utils.fetchArray(from: Endpoint.fetchCoinPaprikaQuotes(SettingsCurrency.current.rawValue.lowercased()))
-        
-        var prices : [String: [String: Double]] = [:]
-        
-        let coinIds = coins.map { $0.id }
-        
-        _ = coinPaprikaQuotes.filter { quote in
-            coinIds.contains(quote.id)
-        }.map { coin in
-            coin.priceProviderId = coins.first{ $0.id == coin.id }?.priceProviderId
-            
-            if let priceProviderId = coin.priceProviderId{
-                let currency = SettingsCurrency.current.rawValue
-                if let price = coin.quotes[currency]?.price {
-                    prices[priceProviderId] = [currency.lowercased(): price]
-                }
-            }
-            
-        }
-        
-        return CryptoPrice(prices: prices)
-    }
-    
-    private func getCoinPaprikaCoins(coins: [Coin]) -> [CoinPaprikaCoin] {
-        guard let url = Bundle.main.url(forResource: "coinpaprika", withExtension: "json") else {
-            print("Failed to locate coinpaprika.json in bundle.")
-            return []
-        }
-        
-        let tickers = coins.map{$0.ticker}
-        
-        do {
-            let data = try Data(contentsOf: url)
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601 // For decoding ISO 8601 date strings
-            let cryptoData = try decoder.decode([CoinPaprikaCoin].self, from: data)
-            
-            let paprikaCoins = cryptoData.filter { crypto in
-                tickers.map({ $0.uppercased()}).contains(crypto.symbol.uppercased())
-            }.map{ paprikaCoin in
-                let systemCoin = coins.first{$0.ticker == paprikaCoin.symbol}
-                paprikaCoin.priceProviderId = systemCoin?.priceProviderId ?? .empty
-                return paprikaCoin
-            }
-            
-            return paprikaCoins
-        } catch {
-            print("Failed to load or decode the JSON:", error)
-        }
-        return []
     }
     
     private func getCoins() -> [Coin]{
