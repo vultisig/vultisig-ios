c0fa2ea5d39d8f00efff5a927eca02adde000339
Enrique Souza <enriquesouza@live.com>
2024-06-21 19:07:00 -0300

    revert

--- VultisigApp/VultisigApp/Views/Vault/ChainDetailView.swift
+++ VultisigApp/VultisigApp/Views/Vault/ChainDetailView.swift
@@ -132,21 +132,13 @@ struct ChainDetailView: View {
         ChainHeaderCell(group: group, isLoading: $isLoading)
     }
     
-    var sortedCoins: [Coin] {
-        group.coins.sorted(by: {
-            if $0.isNativeToken != $1.isNativeToken {
-                return $0.isNativeToken
-            }
-            return $0.balanceInFiatDecimal > $1.balanceInFiatDecimal
-        })
-    }
-    
     var cells: some View {
-        ForEach(sortedCoins, id: \.self) { coin in
+        ForEach(group.coins.sorted(by: {
+            $0.isNativeToken || ($0.balanceInFiatDecimal > $1.balanceInFiatDecimal)
+        }), id: \.self) { coin in
             getCoinCell(coin)
         }
     }
-    
     
     var addButton: some View {
         Button {
