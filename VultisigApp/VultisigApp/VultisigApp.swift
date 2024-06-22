598358569d1abc7ae6698cb0114effdaabee2a24
Amol Kumar <amolkmr10@gmail.com>
2024-06-22 17:20:15 -0400

    SwiftData code reverted back

--- VultisigApp/VultisigApp/VultisigApp.swift
+++ VultisigApp/VultisigApp/VultisigApp.swift
@@ -45,30 +45,30 @@ struct VultisigApp: App {
                 .frame(minWidth: 900, minHeight: 600)
 #endif
         }
-//        .modelContainer(sharedModelContainer)
+        .modelContainer(sharedModelContainer)
     }
     
-//    var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([
-//            Vault.self,
-//            Coin.self,
-//        ])
-//        let modelConfiguration = ModelConfiguration(
-//            schema: schema,
-//            isStoredInMemoryOnly: false
-//        )
-//        do {
-//            let modelContainer = try ModelContainer(
-//                for: schema,
-//                migrationPlan: MigrationPlan.self,
-//                configurations: [modelConfiguration]
-//            )
-//            Storage.shared.modelContext = modelContainer.mainContext
-//            return modelContainer
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }()
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Vault.self,
+            Coin.self,
+        ])
+        let modelConfiguration = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: false
+        )
+        do {
+            let modelContainer = try ModelContainer(
+                for: schema,
+                migrationPlan: MigrationPlan.self,
+                configurations: [modelConfiguration]
+            )
+            Storage.shared.modelContext = modelContainer.mainContext
+            return modelContainer
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
     
     private func continueLogin() {
         accountViewModel.enableAuth()
