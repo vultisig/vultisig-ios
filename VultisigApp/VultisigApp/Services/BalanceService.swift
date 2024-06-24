4084435b070f1680c1147b0ed40c10a7592d9aec
Enrique Souza <enriquesouza@live.com>
2024-06-24 20:31:46 -0300

    Prevent to return zero balance from cache.

--- VultisigApp/VultisigApp/Services/BalanceService.swift
+++ VultisigApp/VultisigApp/Services/BalanceService.swift
@@ -50,9 +50,11 @@ class BalanceService {
         do {
             
             if let cachedValue = await Utils.getCachedData(cacheKey: cacheKey(coin: coin), cache: cache, timeInSeconds: CACHE_TIMEOUT_IN_SECONDS) {
-                coin.rawBalance = cachedValue.rawBalance
-                coin.priceRate = cachedValue.priceRate
-                return
+                if cachedValue.rawBalance != .zero && cachedValue.priceRate != .zero {
+                    coin.rawBalance = cachedValue.rawBalance
+                    coin.priceRate = cachedValue.priceRate
+                    return
+                }
             }
             
             var rawBalance: String = .empty
@@ -113,7 +115,7 @@ class BalanceService {
 private extension BalanceService {
     
     @MainActor func updateCoin(_ coin: Coin, rawBalance: String, priceRate: Double) async throws {
-        guard coin.rawBalance != rawBalance && coin.priceRate != priceRate else { return }
+        guard coin.rawBalance != rawBalance else { return }
         coin.rawBalance = rawBalance
         coin.priceRate = priceRate
         // Swift Data persists on disk io, that is slower than the cache on KEY VALUE RAM
