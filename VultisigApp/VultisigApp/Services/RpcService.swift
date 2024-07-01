e75145fa06324ba12dff342ed4b72035a144b5df
Enrique Souza <enriquesouza@live.com>
2024-07-01 20:55:38 -0300

    nonce too low

--- VultisigApp/VultisigApp/Services/RpcService.swift
+++ VultisigApp/VultisigApp/Services/RpcService.swift
@@ -51,7 +51,7 @@ class RpcService {
             if let error = response["error"] as? [String: Any], let message = error["message"] as? String {
                 if message.contains("known") || message.contains("already known") 
                     || message.contains("Transaction is temporarily banned")
-                    || message.contains("nonce too low: next nonce")
+                    || message.contains("nonce too low")
                     || message.contains("transaction already exists"){
                     return try decode("Transaction already broadcasted.")
                 }
