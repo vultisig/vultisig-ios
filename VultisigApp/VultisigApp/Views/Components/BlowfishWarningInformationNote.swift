a9ce61056b0cf6291eb58993cd29f9875a76c301
Enrique Souza <enriquesouza@live.com>
2024-07-23 15:29:55 -0300

    code organization

--- VultisigApp/VultisigApp/Views/Components/BlowfishWarningInformationNote.swift
+++ VultisigApp/VultisigApp/Views/Components/BlowfishWarningInformationNote.swift
@@ -1,3 +1,10 @@
+//
+//  BlowfishWarningInformationNote.swift
+//  VultisigApp
+//
+//  Created by Enrique Souza Soares on 22/07/24.
+//
+
 import SwiftUI
 
 struct BlowfishWarningInformationNote: View {
@@ -5,74 +12,53 @@ struct BlowfishWarningInformationNote: View {
     @State var blowfishResponse: BlowfishResponse? = nil
     
     var body: some View {
+        guard let response = blowfishResponse else {
+            return AnyView(EmptyView())
+        }
         
-        // We must show nothing if nil
-        if let response = blowfishResponse {
-            if response.warnings.isEmpty {
-                
-                HStack(spacing: 12) {
-                    icon
-                    text
-                }
-                .padding(12)
-                .background(Color.green.opacity(0.35))
-                .cornerRadius(12)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                    #if os(iOS)
-                        .stroke(Color.green, lineWidth: 1)
-                    #elseif os(macOS)
-                        .stroke(Color.green, lineWidth: 2)
-                    #endif
-                )
-                
-            } else {
-                HStack(spacing: 12) {
-                    icon
-                    text
-                }
-                .padding(12)
-                .background(Color.warningYellow.opacity(0.35))
-                .cornerRadius(12)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                    #if os(iOS)
-                        .stroke(Color.warningYellow, lineWidth: 1)
-                    #elseif os(macOS)
-                        .stroke(Color.warningYellow, lineWidth: 2)
-                    #endif
-                )
+        return AnyView(
+            HStack(spacing: 12) {
+                icon(response: response)
+                text(response: response)
             }
-        }
+                .padding(12)
+                .background(response.warnings.isEmpty ? Color.green.opacity(0.35) : Color.warningYellow.opacity(0.35))
+                .cornerRadius(12)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(response.warnings.isEmpty ? Color.green : Color.warningYellow, lineWidth: lineWidth)
+                )
+        )
     }
     
-    var icon: some View {
-        if let response = blowfishResponse, response.warnings.isEmpty {
-            Image(systemName: "checkmark.shield")
-                .foregroundColor(Color.green)
-        } else {
-            Image(systemName: "exclamationmark.triangle")
-                .foregroundColor(Color.warningYellow)
-        }
+    var lineWidth: CGFloat {
+#if os(iOS)
+        return 1
+#elseif os(macOS)
+        return 2
+#endif
     }
     
-    var text: some View {
+    func icon(response: BlowfishResponse) -> some View {
+        Image(systemName: response.warnings.isEmpty ? "checkmark.shield" : "exclamationmark.triangle")
+            .foregroundColor(response.warnings.isEmpty ? Color.green : Color.warningYellow)
+    }
+    
+    func text(response: BlowfishResponse) -> some View {
         VStack(alignment: .leading, spacing: 8) {
-            if let response = blowfishResponse, response.warnings.isEmpty {
+            if response.warnings.isEmpty {
                 Text(NSLocalizedString("scannedByBlowfish", comment: ""))
                     .foregroundColor(.neutral0)
                     .font(.body12MontserratSemiBold)
                     .lineSpacing(8)
                     .multilineTextAlignment(.leading)
             } else {
-                if let response = blowfishResponse {
-                    ForEach(response.warnings) { blowfishMessage in
-                        Text(blowfishMessage.message)
-                            .foregroundColor(.neutral0)
-                            .font(.body12MontserratSemiBold)
-                            .lineSpacing(8)
-                            .multilineTextAlignment(.leading)
-                    }
+                ForEach(response.warnings) { blowfishMessage in
+                    Text(blowfishMessage.message)
+                        .foregroundColor(.neutral0)
+                        .font(.body12MontserratSemiBold)
+                        .lineSpacing(8)
+                        .multilineTextAlignment(.leading)
                 }
             }
         }
