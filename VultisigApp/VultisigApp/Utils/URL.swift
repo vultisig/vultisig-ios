6ed322f1d08ab623b810e2b43746ee50e462ef8a
Amol Kumar <amolkmr10@gmail.com>
2024-08-16 20:16:48 -0400

    Privacy Policy and terms of Service cells added to Settings

--- VultisigApp/VultisigApp/Utils/URL.swift
+++ VultisigApp/VultisigApp/Utils/URL.swift
@@ -12,4 +12,6 @@ class StaticURL {
     static let XVultisigURL = URL(string: "https://twitter.com/vultisig")!
     static let AppStoreVultisigURL = URL(string: "https://www.apple.com/")!
     static let DiscordVultisigURL = URL(string: "https://discord.gg/ngvW8tRRfB")!
+    static let PrivacyPolicyURL = URL(string: "https://docs.google.com/document/d/1gxA5OcCFGupWDyF3VCyKwwojA99CVngH9fLER8irRvI/edit#heading=h.1de5j5yfesa8")!
+    static let TermsOfServiceURL = URL(string: "https://docs.google.com/document/d/101kw3RLzOuO98vxIGe35yLIS2yTlGQOqL_Ga-9BPmz8/edit")!
 }
