3d4b958a4547e0eb6065a4afc9148519ea4f771f
Amol Kumar <amolkmr10@gmail.com>
2024-08-27 22:05:21 -0400

    Drag and drop added for ImportWalletView

--- VultisigApp/VultisigApp/Views/New Wallet/ImportWalletView.swift
+++ VultisigApp/VultisigApp/Views/New Wallet/ImportWalletView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import SwiftData
-import UniformTypeIdentifiers
 
 struct ImportWalletView: View {
     @Environment(\.modelContext) private var context
@@ -36,23 +35,10 @@ struct ImportWalletView: View {
             allowedContentTypes: [.data],
             allowsMultipleSelection: false
         ) { result in
-            switch result {
-            case .success(let urls):
-                guard isValidFormat(urls) else {
-                    showInvalidFormatAlert()
-                    return
-                }
-                
-                if let url = urls.first {
-                    backupViewModel.importedFileName = url.lastPathComponent
-                    backupViewModel.importFile(from: url)
-                }
-            case .failure(let error):
-                print("Error importing file: \(error.localizedDescription)")
-            }
+            backupViewModel.handleFileImporter(result)
         }
-        .onDrop(of: ["public.data"], isTargeted: $isUploading) { providers in
-            handleDrop(providers: providers)
+        .onDrop(of: [.data], isTargeted: $isUploading) { providers -> Bool in
+            backupViewModel.handleOnDrop(providers: providers)
         }
         .navigationDestination(isPresented: $backupViewModel.isLinkActive) {
             HomeView(selectedVault: backupViewModel.selectedVault)
@@ -63,38 +49,6 @@ struct ImportWalletView: View {
         .onDisappear {
             resetData()
         }
-    }
-    
-    private func handleDrop(providers: [NSItemProvider]) -> Bool {
-        var fileUrl: URL? = nil
-        var fileError: Error? = nil
-
-        let group = DispatchGroup()
-
-        let provider = providers.first
-            group.enter()
-        provider?.loadItem(forTypeIdentifier: "public.data", options: nil) { (item, error) in
-                if let error = error {
-                    fileError = error
-                } else if let data = item as? Data, let url = URL(dataRepresentation: data, relativeTo: nil) {
-                    fileUrl = url
-                } else if let url = item as? URL {
-                    fileUrl = url
-                } else {
-                    fileError = NSError(domain: "Invalid URL", code: -1, userInfo: nil)
-                }
-                group.leave()
-            
-            if let fileUrl {
-                backupViewModel.importedFileName = fileUrl.lastPathComponent
-                backupViewModel.importFile(from: fileUrl)
-            } else {
-                print(fileError ?? "ERROR")
-                showInvalidFormatAlert()
-            }
-        }
-
-            return true
     }
     
     var main: some View {
@@ -177,24 +131,6 @@ struct ImportWalletView: View {
     
     private func resetData() {
         backupViewModel.resetData()
-    }
-    
-    private func isValidFormat(_ urls: [URL]) -> Bool {
-        guard let fileExtension = urls.first?.pathExtension.lowercased() else {
-            return false
-        }
-        
-        if fileExtension == "dat" || fileExtension == "bak"{
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    private func showInvalidFormatAlert() {
-        backupViewModel.alertTitle = "invalidFileFormat"
-        backupViewModel.alertMessage = "invalidFileFormatMessage"
-        backupViewModel.showAlert = true
     }
 }
 
