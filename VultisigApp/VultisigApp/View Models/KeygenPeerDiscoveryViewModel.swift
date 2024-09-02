efceaef073bbd80864e07302d96804c2b21ae841
Johnny Luo <johnnyluo1980@gmail.com>
2024-09-03 08:24:33 +1000

    add reshare

--- VultisigApp/VultisigApp/View Models/KeygenPeerDiscoveryViewModel.swift
+++ VultisigApp/VultisigApp/View Models/KeygenPeerDiscoveryViewModel.swift
@@ -75,9 +75,29 @@ class KeygenPeerDiscoveryViewModel: ObservableObject {
             self.vault.localPartyID = self.localPartyID
         }
         self.selections.insert(self.localPartyID)
-        startVultisigner(name: vault.name, sessionID: sessionID, hexEncryptionKey: self.encryptionKeyHex!, hexChainCode: vault.hexChainCode, localPartyID: "signer-1", encryptionPassword: "test123", email: "johnnyluo1980@icloud.com")
+        switch tssType {
+        case .Keygen:
+            startVultisigner(name: vault.name, sessionID: sessionID, hexEncryptionKey: self.encryptionKeyHex!, hexChainCode: vault.hexChainCode, localPartyID: "signer-1", encryptionPassword: "test123", email: "johnnyluo1980@icloud.com")
+        case .Reshare:
+            startVultisignerReshare(name: vault.name, publicKeyECDSA: vault.pubKeyECDSA, sessionID: sessionID, hexEncryptionKey: self.encryptionKeyHex!, hexChainCode: vault.hexChainCode, localPartyID: "signer-1", encryptionPassword: "test123", email: "johnny@vultisig.com")
+        }
     }
-    
+    func startVultisignerReshare(name: String,
+                                 publicKeyECDSA: String,
+                          sessionID: String,
+                          hexEncryptionKey: String,
+                          hexChainCode: String,
+                          localPartyID:String,
+                          encryptionPassword:String,
+                          email: String){
+        let req = ReshareRequest(name: name,public_key: publicKeyECDSA, session_id: sessionID, hex_encryption_key: hexEncryptionKey, hex_chain_code: hexChainCode, local_party_id: localPartyID, encryption_password: encryptionPassword, email: email)
+        
+        let urlString = "https://api.vultisig.com/vault/reshare"
+        Utils.sendRequest(urlString: urlString, method: "POST", headers: [:], body: req){ _ in
+            print("send req to vultisigner successfully")
+        }
+        
+    }
     func startVultisigner(name:String,
                           sessionID: String,
                           hexEncryptionKey: String,
@@ -87,7 +107,7 @@ class KeygenPeerDiscoveryViewModel: ObservableObject {
                           email: String){
         let req = VaultCreateRequest(name: name, session_id: sessionID, hex_encryption_key: hexEncryptionKey, hex_chain_code: hexChainCode, local_party_id: localPartyID, encryption_password: encryptionPassword, email: email)
         
-        let urlString = "http://127.0.0.1:8080/vault/create"
+        let urlString = "https://api.vultisig.com/vault/create"
         Utils.sendRequest(urlString: urlString, method: "POST", headers: [:], body: req){ _ in
             print("send req to vultisigner successfully")
         }
