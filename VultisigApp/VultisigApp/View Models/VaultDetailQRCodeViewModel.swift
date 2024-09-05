a37a77abf966415daabaaeaa1b501ef1fa4e9380
Amol Kumar <amolkmr10@gmail.com>
2024-09-04 22:05:32 -0400

    VaultDetailQRCodeView refactored

--- VultisigApp/VultisigApp/View Models/VaultDetailQRCodeViewModel.swift
+++ VultisigApp/VultisigApp/View Models/VaultDetailQRCodeViewModel.swift
@@ -51,4 +51,46 @@ class VaultDetailQRCodeViewModel: ObservableObject {
         
         return VaultPublicKeyExport(uid: id, name: name, public_key_ecdsa: ecdsaKey, public_key_eddsa: eddsaKey, hex_chain_code: hexCode)
     }
+    
+#if os(iOS)
+    func shareImage(_ imageName: String) {
+        guard let image = renderedImage else {
+            return
+        }
+        
+        let uiImage = renderImage(image: image)
+
+        let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent(imageName)
+
+        if let pngData = uiImage.pngData() {
+            try? pngData.write(to: tempURL)
+        }
+
+        let activityViewController = UIActivityViewController(activityItems: [tempURL], applicationActivities: nil)
+
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            rootViewController.present(activityViewController, animated: true, completion: nil)
+        }
+    }
+    
+    private func renderImage(image: Image) -> UIImage {
+        let controller = UIHostingController(
+            rootView:
+                image
+                    .frame(width: 278, height: 402)
+                    .offset(y: -30)
+        )
+        let view = controller.view
+
+        let targetSize = controller.view.intrinsicContentSize
+        view?.bounds = CGRect(origin: .zero, size: targetSize)
+        view?.backgroundColor = .clear
+
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        return renderer.image { _ in
+            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+        }
+    }
+#endif
 }
--- VultisigApp/VultisigApp/Views/Vault/VaultDetailQRCodeView.swift
+++ VultisigApp/VultisigApp/Views/Vault/VaultDetailQRCodeView.swift
@@ -90,60 +90,36 @@ struct VaultDetailQRCodeView: View {
         }
     }
     
+#if os(iOS)
     var shareButton: some View {
         Button {
-            shareImage()
+            viewModel.shareImage(imageName)
         } label: {
             FilledButton(title: "share")
                 .padding(.bottom, 22)
         }
     }
+#elseif os(macOS)
+    var shareButton: some View {
+        ZStack {
+            if let renderedImage = viewModel.renderedImage {
+                ShareLink(
+                    item: renderedImage,
+                    preview: SharePreview(imageName, image: renderedImage)
+                ) {
+                    FilledButton(title: "share")
+                        .padding(.bottom, 22)
+                }
+            } else {
+                ProgressView()
+            }
+        }
+    }
+#endif
     
     private func setData() {
         imageName = viewModel.generateName(vault: vault)
         viewModel.render(vault: vault, displayScale: displayScale)
-    }
-    
-    func shareImage() {
-        guard let image = viewModel.renderedImage else {
-            return
-        }
-        
-        let uiImage = renderImage(image: image)
-
-        let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent(imageName)
-
-        // Save UIImage as PNG to temporary directory
-        if let pngData = uiImage.pngData() {
-            try? pngData.write(to: tempURL)
-        }
-
-        // Create and present the share sheet
-        let activityViewController = UIActivityViewController(activityItems: [tempURL], applicationActivities: nil)
-
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-           let rootViewController = windowScene.windows.first?.rootViewController {
-            rootViewController.present(activityViewController, animated: true, completion: nil)
-        }
-    }
-    
-    func renderImage(image: Image) -> UIImage {
-        let controller = UIHostingController(
-            rootView: 
-                image
-                    .frame(width: 278, height: 402)
-                    .offset(y: -30)
-        )
-        let view = controller.view
-
-        let targetSize = controller.view.intrinsicContentSize
-        view?.bounds = CGRect(origin: .zero, size: targetSize)
-        view?.backgroundColor = .clear
-
-        let renderer = UIGraphicsImageRenderer(size: targetSize)
-        return renderer.image { _ in
-            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
-        }
     }
 }
 
