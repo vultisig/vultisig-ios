ace8c91077e41777909d9f2b8e49b5985a05018f
Amol Kumar <amolkmr10@gmail.com>
2024-09-10 03:04:01 -0400

    ToggleSelectionCell refactored

---
+++ VultisigApp/VultisigApp/macOS/View/ToggleSelectionCell+macOS.swift
@@ -0,0 +1,18 @@
+//
+//  ToggleSelectionCell+macOS.swift
+//  VultisigApp
+//
+//  Created by Amol Kumar on 2024-09-10.
+//
+
+#if os(macOS)
+import SwiftUI
+
+extension ToggleSelectionCell {
+    var container: some View {
+        content
+            .scaleEffect(2)
+            .offset(x: -12)
+    }
+}
+#endif
