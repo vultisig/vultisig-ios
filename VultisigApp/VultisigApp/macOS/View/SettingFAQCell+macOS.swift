6ccb7837dd485f13439a1cf0742a33e8e50d7d51
Amol Kumar <amolkmr10@gmail.com>
2024-09-10 02:57:38 -0400

    SettingFAQCell refactored

---
+++ VultisigApp/VultisigApp/macOS/View/SettingFAQCell+macOS.swift
@@ -0,0 +1,17 @@
+//
+//  SettingFAQCell+macOS.swift
+//  VultisigApp
+//
+//  Created by Amol Kumar on 2024-09-10.
+//
+
+#if os(macOS)
+import SwiftUI
+
+extension SettingFAQCell {
+    var container: some View {
+        content
+            .padding(8)
+    }
+}
+#endif
