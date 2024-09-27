14e233d3eb5a40bcb51d07c69d1e1f08c477b6f3
Johnny Luo <johnnyluo1980@gmail.com>
2024-09-27 16:06:06 +1000

    add EVM test

---
+++ VultisigApp/VultisigAppTests/Chains/ERC20Test.swift
@@ -0,0 +1,49 @@
+//
+//  ERC20Test.swift
+//  VultisigApp
+//
+//  Created by Johnny Luo on 27/9/2024.
+//
+
+@testable import VultisigApp
+import WalletCore
+import XCTest
+import BigInt
+
+final class ERC20ChainsHelperTest: XCTestCase {
+    let hexPublicKey = "023e4b76861289ad4528b33c2fd21b3a5160cd37b3294234914e21efb6ed4a452b"
+    let hexChainCode = "c9b189a8232b872b8d9ccd867d0db316dd10f56e729c310fe072adf5fd204ae7"
+    
+    func testGetCoin() throws {
+        let vaultForTest = Vault(name: "TestVault")
+        vaultForTest.pubKeyECDSA = hexPublicKey
+        vaultForTest.hexChainCode = hexChainCode
+        let eth = try CoinFactory.create(asset: TokensStore.Token.ethereum, vault: vaultForTest)
+        XCTAssertEqual(eth.hexPublicKey, "03bb1adf8c0098258e4632af6c055c37135477e269b7e7eb4f600fe66d9ca9fd78")
+        XCTAssertEqual(eth.address, "0xe5F238C95142be312852e864B830daADB9B7D290")
+    }
+    
+    func testGetPreKeysignImage() throws {
+        let vaultForTest = Vault(name: "TestVault")
+        vaultForTest.pubKeyECDSA = hexPublicKey
+        vaultForTest.hexChainCode = hexChainCode
+        let usdc = try CoinFactory.create(asset: TokensStore.Token.ethereumUsdc, vault: vaultForTest)
+        let keysignPayload = KeysignPayload(coin: usdc,
+                                            toAddress: "0xfA0635a1d083D0bF377EFbD48DA46BB17e0106cA",
+                                            toAmount: 10000000,
+                                            chainSpecific: BlockChainSpecific.Ethereum(maxFeePerGasWei: BigInt(10),
+                                                                                       priorityFeeWei: BigInt(1),
+                                                                                       nonce: 0,
+                                                                                       gasLimit: BigInt(120000)),
+                                            utxos: [],
+                                            memo: "voltix",
+                                            swapPayload: nil,
+                                            approvePayload: nil,
+                                            vaultPubKeyECDSA: "ECDSAKey",
+                                            vaultLocalPartyID: "localPartyID")
+        let eRC20Helper = ERC20Helper(coinType: .ethereum)
+        let preImageHashes = try eRC20Helper.getPreSignedImageHash(keysignPayload: keysignPayload)
+        XCTAssertEqual(preImageHashes.count,1)
+        XCTAssertEqual(preImageHashes[0],"5ac8a3ccea00ecdb506d387424d68390d94623431798a2f65903aea1d6cf13c9")
+    }
+}
---
+++ VultisigApp/VultisigAppTests/Chains/EvmTest.swift
@@ -0,0 +1,49 @@
+//
+//  EvmTest.swift
+//  VultisigApp
+//
+//  Created by Johnny Luo on 27/9/2024.
+//
+
+@testable import VultisigApp
+import WalletCore
+import XCTest
+import BigInt
+
+final class EVMChainsHelperTest: XCTestCase {
+    let hexPublicKey = "023e4b76861289ad4528b33c2fd21b3a5160cd37b3294234914e21efb6ed4a452b"
+    let hexChainCode = "c9b189a8232b872b8d9ccd867d0db316dd10f56e729c310fe072adf5fd204ae7"
+    
+    func testGetCoin() throws {
+        let vaultForTest = Vault(name: "TestVault")
+        vaultForTest.pubKeyECDSA = hexPublicKey
+        vaultForTest.hexChainCode = hexChainCode
+        let eth = try CoinFactory.create(asset: TokensStore.Token.ethereum, vault: vaultForTest)
+        XCTAssertEqual(eth.hexPublicKey, "03bb1adf8c0098258e4632af6c055c37135477e269b7e7eb4f600fe66d9ca9fd78")
+        XCTAssertEqual(eth.address, "0xe5F238C95142be312852e864B830daADB9B7D290")
+    }
+    
+    func testGetPreKeysignImage() throws {
+        let vaultForTest = Vault(name: "TestVault")
+        vaultForTest.pubKeyECDSA = hexPublicKey
+        vaultForTest.hexChainCode = hexChainCode
+        let eth = try CoinFactory.create(asset: TokensStore.Token.ethereum, vault: vaultForTest)
+        let keysignPayload = KeysignPayload(coin: eth,
+                                            toAddress: "0xfA0635a1d083D0bF377EFbD48DA46BB17e0106cA",
+                                            toAmount: 10000000,
+                                            chainSpecific: BlockChainSpecific.Ethereum(maxFeePerGasWei: BigInt(10),
+                                                                                       priorityFeeWei: BigInt(1),
+                                                                                       nonce: 0,
+                                                                                       gasLimit: BigInt(24000)),
+                                            utxos: [],
+                                            memo: "voltix",
+                                            swapPayload: nil,
+                                            approvePayload: nil,
+                                            vaultPubKeyECDSA: "ECDSAKey",
+                                            vaultLocalPartyID: "localPartyID")
+        let evmHelper = EVMHelper(coinType: .ethereum)
+        let preImageHashes = try evmHelper.getPreSignedImageHash(keysignPayload: keysignPayload)
+        XCTAssertEqual(preImageHashes.count,1)
+        XCTAssertEqual(preImageHashes[0],"1e93ef6b20b01723e95128aed8786d43c7c53a12959a21ef36cf408a6d7115de")
+    }
+}
