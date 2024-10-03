276d1d5c037d61eb48df0564eb7d3027d86319fc
Johnny Luo <johnnyluo1980@gmail.com>
2024-10-03 11:28:35 +1000

    update encryption tests

--- VultisigApp/VultisigAppTests/Utils/EncryptionTest.swift
+++ VultisigApp/VultisigAppTests/Utils/EncryptionTest.swift
@@ -22,5 +22,10 @@ final class EncryptionTest: XCTestCase {
         let encryptionKey = "b5890b4dfb218e9482b429fcbb8317467211102b5890c0edddb2facd40316434"
         let result = "gd95s9igrMv9pFqPrnQwSlwZbDGpvR1X7FFxpDXHtns=".aesDecrypt(key: encryptionKey)
         XCTAssert(result == "helloworld")
+        
+        let hexPwd = "password123".data(using: .utf8)?.hexString
+        // The following is encrypted in Android , make sure we can decrypt it on IOS
+        let result1 = "zPMOwnPVMFKMf9LOIFkyqBOr8AC1SIdQ34Ruk5gmRqxZ+lIyK7zM5/1NUjXlAg==".aesDecrypt(key: hexPwd!)
+        XCTAssert(result1 == "Original Input 123")
     }
 }
