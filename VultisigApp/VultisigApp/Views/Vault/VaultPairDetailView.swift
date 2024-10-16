5d535829c2f114357d415f4220f681d942cb409b
Amol Kumar <amolkmr10@gmail.com>
2024-10-16 00:57:15 -0400

    WIP - Layout update

--- VultisigApp/VultisigApp/Views/Vault/VaultPairDetailView.swift
+++ VultisigApp/VultisigApp/Views/Vault/VaultPairDetailView.swift
@@ -11,6 +11,8 @@ struct VaultPairDetailView: View {
     let vault: Vault    
     @State var devicesInfo: [DeviceInfo] = []
     
+    @State var deviceIndex: Int = 0
+    
     var body: some View {
         content
             .onAppear {
@@ -23,18 +25,20 @@ struct VaultPairDetailView: View {
     var cells: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 12) {
-                titleCell
+                getTitleCell(for: vault.name)
+                getTitleCell(for: titlePartText())
                 
                 VaultPairDetailCell(title: NSLocalizedString("ECDSA", comment: ""), description: vault.pubKeyECDSA).frame(maxWidth: .infinity, alignment: .leading)
                 
                 VaultPairDetailCell(title: NSLocalizedString("EdDSA", comment: ""), description: vault.pubKeyEdDSA).frame(maxWidth: .infinity, alignment: .leading)
                 
-                Text("\(vault.getThreshold() + 1) of \(vault.signers.count) Vault")
+                Text(NSLocalizedString("devices", comment: ""))
                     .font(.body14MontserratMedium)
                     .foregroundColor(.neutral0)
                     .padding(.vertical, 12)
                     .padding(.horizontal, 16)
                     .frame(maxWidth: .infinity, alignment: .center)
+                    .offset(y: 8)
                 
                 ForEach(devicesInfo, id: \.Index) { device in
                     getDeviceCell(for: device)
@@ -44,8 +48,8 @@ struct VaultPairDetailView: View {
         }
     }
     
-    var titleCell: some View {
-        Text(titleText())
+    private func getTitleCell(for text: String) -> some View {
+        Text(text)
             .foregroundColor(.neutral0)
             .font(.body20MenloBold)
             .padding(.vertical, 22)
@@ -57,27 +61,36 @@ struct VaultPairDetailView: View {
             .lineLimit(3)
     }
     
-    private func titleText() -> String {
-        let name = vault.name
-        let dash = " - "
-        let part = NSLocalizedString("part", comment: "")
-        let of = NSLocalizedString("of", comment: "")
-        let space = " "
-        let vaultIndex = "\(vault.getThreshold() + 1)"
-        let totalCount = "\(vault.signers.count)"
-        
-        return name + dash + part + space + vaultIndex + space + of + space + totalCount
-    }
-    
     private func getDeviceCell(for device: DeviceInfo) -> some View {
         let part = "Part of \(device.Index+1) of \(vault.signers.count): "
         let signer = device.Signer
-        let suffix = device.Signer == vault.localPartyID ? " (This device)" : ""
         
-        return VaultPairDetailCell(
-            title: .empty,
-            description: part + signer + suffix
-        )
+        return ZStack {
+            if device.Signer == vault.localPartyID {
+                VaultPairDetailCell(
+                    title: .empty,
+                    description: part + signer + " (This device)"
+                )
+                .onAppear {
+                    deviceIndex = device.Index + 1
+                }
+            } else {
+                VaultPairDetailCell(
+                    title: .empty,
+                    description: part + signer
+                )
+            }
+        }
+    }
+    
+    private func titlePartText() -> String {
+        let part = NSLocalizedString("part", comment: "")
+        let of = NSLocalizedString("of", comment: "")
+        let space = " "
+        let vaultIndex = "\(deviceIndex)"
+        let totalCount = "\(vault.signers.count)"
+        
+        return part + space + vaultIndex + space + of + space + totalCount
     }
 }
 
