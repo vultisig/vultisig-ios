88ff45548aabdf396cae471e49ebdfce7edb6d89
Artur Guseinov <arthur.guseinov@gmail.com>
2024-10-16 23:27:28 +0800

    BiometrySettingsView

---
+++ VultisigApp/VultisigApp/Views/Settings/SettingsBiometryView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsBiometryView.swift
+//  VultisigApp
+//
+//  Created by Artur Guseinov on 16.10.2024.
+//
+
+import SwiftUI
+
+struct SettingsBiometryView: View {
+
+    var body: some View {
+        content
+    }
+
+    var main: some View {
+        VStack {
+
+        }
+    }
+}
