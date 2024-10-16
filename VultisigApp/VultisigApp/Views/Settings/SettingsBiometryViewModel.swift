dbcb5209ca5867f9d433705e7fd208b8090ea057
Artur Guseinov <arthur.guseinov@gmail.com>
2024-10-17 01:21:05 +0800

    SettingsBiometryView business logic

---
+++ VultisigApp/VultisigApp/Views/Settings/SettingsBiometryViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  SettingsBiometryViewModel.swift
+//  VultisigApp
+//
+//  Created by Artur Guseinov on 16.10.2024.
+//
+
+import Foundation
+import SwiftUI
+
+final class SettingsBiometryViewModel: ObservableObject {
+
+    @AppStorage("isBiometryEnabled") var isBiometryEnabled: Bool = true
+
+    @Published var password: String = .empty
+    @Published var isLoading: Bool = false
+    @Published var isPasswordChanged: Bool = false
+    @Published var isWrongPassword: Bool = false
+
+    private let keychain = DefaultKeychainService.shared
+    private let fastVaultService = FastVaultService.shared
+
+    func setData(vault: Vault) {
+        if let password = keychain.getFastPassword(pubKeyECDSA: vault.pubKeyECDSA) {
+            self.password = password
+        }
+    }
+
+    func passwordChanged() {
+        isPasswordChanged = true
+    }
+
+    var isSaveEnabled: Bool {
+        return isPasswordChanged && !password.isEmpty
+    }
+
+    @MainActor func validatePassword(vault: Vault) async -> Bool {
+        isLoading = true
+        defer { isLoading = false }
+
+        let isValid = await fastVaultService.get(
+            pubKeyECDSA:  vault.pubKeyECDSA,
+            password: password
+        )
+
+        guard isValid else {
+            isWrongPassword = true
+            return false
+        }
+
+        keychain.setFastPassword(
+            password.isEmpty ? nil : password,
+            pubKeyECDSA: vault.pubKeyECDSA
+        )
+
+        return true
+    }
+}
