f63883a58f90b7956f5e6221f79d63741bbb03a8
Enrique Souza <enriquesouza@live.com>
2024-11-05 02:56:52 -0300

    estimate gas

--- VultisigApp/VultisigApp/Services/BlockChainService.swift
+++ VultisigApp/VultisigApp/Services/BlockChainService.swift
@@ -55,8 +55,8 @@ final class BlockChainService {
                 action: .transfer,
                 sendMaxAmount: tx.sendMaxAmount,
                 isDeposit: tx.isDeposit,
-                transactionType: tx.transactionType, 
-                gasLimit: tx.gasLimit, 
+                transactionType: tx.transactionType,
+                gasLimit: tx.gasLimit,
                 byteFee: tx.byteFee,
                 fromAddress: tx.fromAddress,
                 toAddress: tx.toAddress,
@@ -67,34 +67,34 @@ final class BlockChainService {
 
         let service = try EvmServiceFactory.getService(forChain: tx.coin.chain)
         let (gasPrice, priorityFee, nonce) = try await service.getGasInfo(fromAddress: tx.coin.address, mode: tx.feeMode)
-        let estimateGasLimit = try await estemateERC20GasLimit(tx: tx, gasPrice: gasPrice, priorityFee: priorityFee, nonce: nonce)
+        let estimateGasLimit = tx.coin.isNativeToken ? try await estimateGasLimit(tx: tx, gasPrice: gasPrice, priorityFee: priorityFee, nonce: nonce)  :  try await estimateERC20GasLimit(tx: tx, gasPrice: gasPrice, priorityFee: priorityFee, nonce: nonce)
         let defaultGasLimit = BigInt(EVMHelper.defaultERC20TransferGasUnit)
         let gasLimit = max(defaultGasLimit, estimateGasLimit)
-
+        
         let specific = try await fetchSpecific(
-            for: tx.coin, 
+            for: tx.coin,
             action: .transfer,
             sendMaxAmount: tx.sendMaxAmount,
             isDeposit: tx.isDeposit,
             transactionType: tx.transactionType,
-            gasLimit: max(gasLimit, tx.gasLimit), 
+            gasLimit: max(gasLimit, tx.gasLimit),
             byteFee: tx.gasLimit,
             fromAddress: tx.fromAddress,
             toAddress: tx.toAddress,
             feeMode: tx.feeMode
         )
-
+        
         return specific
     }
-
+    
     func fetchSpecific(tx: SwapTransaction) async throws -> BlockChainSpecific {
         let specific = try await fetchSpecific(
             for: tx.fromCoin,
             action: .swap,
             sendMaxAmount: false,
             isDeposit: tx.isDeposit,
-            transactionType: .unspecified, 
-            gasLimit: nil, 
+            transactionType: .unspecified,
+            gasLimit: nil,
             byteFee: nil,
             fromAddress: nil,
             toAddress: nil,
@@ -128,22 +128,22 @@ private extension BlockChainService {
             _ = try await thor.getTHORChainChainID()
             let account = try await thor.fetchAccountNumber(coin.address)
             let fee = try await thor.fetchFeePrice()
-
+            
             guard let accountNumberString = account?.accountNumber, let accountNumber = UInt64(accountNumberString) else {
                 throw Errors.failToGetAccountNumber
             }
-
+            
             guard let sequence = UInt64(account?.sequence ?? "0") else {
                 throw Errors.failToGetSequenceNo
             }
             return .THORChain(accountNumber: accountNumber, sequence: sequence, fee: fee, isDeposit: isDeposit)
         case .mayaChain:
             let account = try await maya.fetchAccountNumber(coin.address)
-
+            
             guard let accountNumberString = account?.accountNumber, let accountNumber = UInt64(accountNumberString) else {
                 throw Errors.failToGetAccountNumber
             }
-
+            
             guard let sequence = UInt64(account?.sequence ?? "0") else {
                 throw Errors.failToGetSequenceNo
             }
@@ -151,90 +151,110 @@ private extension BlockChainService {
         case .solana:
             async let recentBlockHashPromise = sol.fetchRecentBlockhash()
             async let highPriorityFeePromise = sol.fetchHighPriorityFee(account: coin.address)
-
+            
             let recentBlockHash = try await recentBlockHashPromise
             let highPriorityFee = try await highPriorityFeePromise
-
+            
             guard let recentBlockHash else {
                 throw Errors.failToGetRecentBlockHash
             }
-
+            
             if let fromAddress, let toAddress, !toAddress.isEmpty, !coin.isNativeToken {
                 async let associatedTokenAddressFromPromise = sol.fetchTokenAssociatedAccountByOwner(for: fromAddress, mintAddress: coin.contractAddress)
                 async let associatedTokenAddressToPromise = sol.fetchTokenAssociatedAccountByOwner(for: toAddress, mintAddress: coin.contractAddress)
                 let associatedTokenAddressFrom = try await associatedTokenAddressFromPromise
                 let associatedTokenAddressTo = try await associatedTokenAddressToPromise
-
+                
                 return .Solana(recentBlockHash: recentBlockHash, priorityFee: BigInt(highPriorityFee), fromAddressPubKey: associatedTokenAddressFrom, toAddressPubKey: associatedTokenAddressTo)
             }
-
+            
             return .Solana(recentBlockHash: recentBlockHash, priorityFee: BigInt(highPriorityFee), fromAddressPubKey: nil, toAddressPubKey: nil)
-
+            
         case .sui:
             let (referenceGasPrice, allCoins) = try await sui.getGasInfo(coin: coin)
             return .Sui(referenceGasPrice: referenceGasPrice, coins: allCoins)
-
+            
         case .polkadot:
             let gasInfo = try await dot.getGasInfo(fromAddress: coin.address)
             return .Polkadot(recentBlockHash: gasInfo.recentBlockHash, nonce: UInt64(gasInfo.nonce), currentBlockNumber: gasInfo.currentBlockNumber, specVersion: gasInfo.specVersion, transactionVersion: gasInfo.transactionVersion, genesisHash: gasInfo.genesisHash)
-
+            
         case .ethereum, .avalanche, .bscChain, .arbitrum, .base, .optimism, .polygon, .blast, .cronosChain:
             let service = try EvmServiceFactory.getService(forChain: coin.chain)
             let baseFee = try await service.getBaseFee()
-            let (_, defaultPriorityFee, nonce) = try await service.getGasInfo(fromAddress: coin.address, mode: feeMode)
-            let gasLimit = gasLimit ?? normalizeGasLimit(coin: coin, action: action)
+            let (gasPrice, defaultPriorityFee, nonce) = try await service.getGasInfo(fromAddress: coin.address, mode: feeMode)
+            
+            var gas: BigInt? = nil
+            if action == .transfer {
+                if coin.isNativeToken {
+                    gas = try await service.estimateGasForEthTransaction(
+                        senderAddress: coin.address,
+                        recipientAddress: .anyAddress,
+                        value: coin.rawBalance.toBigInt(),
+                        memo: nil
+                    )
+                } else {
+                    gas = try await service.estimateGasForERC20Transfer(
+                        senderAddress: coin.address,
+                        contractAddress: coin.contractAddress,
+                        recipientAddress: .anyAddress,
+                        value: coin.rawBalance.toBigInt()
+                    )
+                }
+            }
+            
+            let gasLimit = gas ?? normalizeGasLimit(coin: coin, action: action)
             let priorityFeesMap = try await service.fetchMaxPriorityFeesPerGas()
             let priorityFee = priorityFeesMap[feeMode] ?? defaultPriorityFee
             let normalizedBaseFee = Self.normalizeEVMFee(baseFee, action: .transfer)
             let maxFeePerGasWei = normalizedBaseFee + priorityFee
             return .Ethereum(maxFeePerGasWei: maxFeePerGasWei, priorityFeeWei: priorityFee, nonce: nonce, gasLimit: gasLimit)
-
+            
         case .zksync:
             let service = try EvmServiceFactory.getService(forChain: coin.chain)
             let (gasLimit, _, maxFeePerGas, maxPriorityFeePerGas, nonce) = try await service.getGasInfoZk(fromAddress: coin.address, toAddress: .zeroAddress)
-
+            
             return .Ethereum(maxFeePerGasWei: maxFeePerGas, priorityFeeWei: maxPriorityFeePerGas, nonce: nonce, gasLimit: gasLimit)
-
+            
         case .gaiaChain:
             let account = try await atom.fetchAccountNumber(coin.address)
-
+            
             guard let accountNumberString = account?.accountNumber, let accountNumber = UInt64(accountNumberString) else {
                 throw Errors.failToGetAccountNumber
             }
-
+            
             guard let sequence = UInt64(account?.sequence ?? "0") else {
                 throw Errors.failToGetSequenceNo
             }
             return .Cosmos(accountNumber: accountNumber, sequence: sequence, gas: 7500, transactionType: transactionType.rawValue)
         case .kujira:
             let account = try await kuji.fetchAccountNumber(coin.address)
-
+            
             guard let accountNumberString = account?.accountNumber, let accountNumber = UInt64(accountNumberString) else {
                 throw Errors.failToGetAccountNumber
             }
-
+            
             guard let sequence = UInt64(account?.sequence ?? "0") else {
                 throw Errors.failToGetSequenceNo
             }
             return .Cosmos(accountNumber: accountNumber, sequence: sequence, gas: 7500, transactionType: transactionType.rawValue)
         case .osmosis:
             let account = try await osmo.fetchAccountNumber(coin.address)
-
+            
             guard let accountNumberString = account?.accountNumber, let accountNumber = UInt64(accountNumberString) else {
                 throw Errors.failToGetAccountNumber
             }
-
+            
             guard let sequence = UInt64(account?.sequence ?? "0") else {
                 throw Errors.failToGetSequenceNo
             }
             return .Cosmos(accountNumber: accountNumber, sequence: sequence, gas: 7500, transactionType: transactionType.rawValue)
         case .dydx:
             let account = try await dydx.fetchAccountNumber(coin.address)
-
+            
             guard let accountNumberString = account?.accountNumber, let accountNumber = UInt64(accountNumberString) else {
                 throw Errors.failToGetAccountNumber
             }
-
+            
             guard let sequence = UInt64(account?.sequence ?? "0") else {
                 throw Errors.failToGetSequenceNo
             }
@@ -244,7 +264,7 @@ private extension BlockChainService {
             return .Ton(sequenceNumber: seqno, expireAt: expireAt, bounceable: false)
         }
     }
-
+    
     func normalizeGasLimit(coin: Coin, action: Action) -> BigInt {
         switch action {
         case .transfer:
@@ -253,7 +273,7 @@ private extension BlockChainService {
             return BigInt(EVMHelper.defaultETHSwapGasUnit)
         }
     }
-
+    
     func normalizePriorityFee(_ value: BigInt,_ chain: Chain) -> BigInt {
         if chain == .ethereum || chain == .avalanche {
             // BSC is very cheap , and layer two is very low priority fee as well
@@ -265,8 +285,8 @@ private extension BlockChainService {
         }
         return value
     }
-
-    func estemateERC20GasLimit(
+    
+    func estimateERC20GasLimit(
         tx: SendTransaction,
         gasPrice: BigInt,
         priorityFee: BigInt,
@@ -278,6 +298,22 @@ private extension BlockChainService {
             contractAddress: tx.coin.contractAddress,
             recipientAddress: .anyAddress,
             value: BigInt(stringLiteral: tx.coin.rawBalance)
+        )
+        return gas
+    }
+    
+    func estimateGasLimit(
+        tx: SendTransaction,
+        gasPrice: BigInt,
+        priorityFee: BigInt,
+        nonce: Int64
+    ) async throws -> BigInt {
+        let service = try EvmServiceFactory.getService(forChain: tx.coin.chain)
+        let gas = try await service.estimateGasForEthTransaction(
+            senderAddress: tx.coin.address,
+            recipientAddress: .anyAddress,
+            value: tx.amountInRaw,
+            memo: tx.memo
         )
         return gas
     }
