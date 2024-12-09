dc3bbfdaf8d642779d427a74cd568925b8797834
Enrique <enriquesouza@live.com>
2024-12-09 19:40:33 -0300

    DER

--- VultisigApp/VultisigApp/Chains/Ripple.swift
+++ VultisigApp/VultisigApp/Chains/Ripple.swift
@@ -18,7 +18,8 @@ enum RippleHelper {
             throw HelperError.runtimeError("coin is not XRP")
         }
         
-        guard case .Cosmos(let accountNumber, let sequence , _, _, _) = keysignPayload.chainSpecific else {
+        // TODO: Create a Ripple one, for testing I am reusing this from Cosmos.
+        guard case .Cosmos(let lastLedgerSequence, let sequence , _, _, _) = keysignPayload.chainSpecific else {
             throw HelperError.runtimeError("getPreSignedInputData: fail to get account number and sequence")
         }
         
@@ -28,6 +29,17 @@ enum RippleHelper {
         
         let publicKey = try CoinFactory.publicKey(asset: keysignPayload.coin.toCoinMeta(), vault: vault)
         
+        print("")
+        print("")
+        print("")
+        print("getPreSignedInputData")
+        print("publicKey: \(publicKey)")
+        print("publicKey.data: \(publicKey.bitcoinKeyHash)")
+        print("publicKey.data.hexString: \(publicKey.data.hexString)")
+        print("")
+        print("")
+        print("")
+        
         let operation = RippleOperationPayment.with {
             $0.destination = keysignPayload.toAddress
             $0.amount = Int64(keysignPayload.toAmount.description) ?? 0
@@ -36,7 +48,7 @@ enum RippleHelper {
         let input = RippleSigningInput.with {
             $0.fee = 10
             $0.sequence = Int32(sequence) // from account info api
-            $0.lastLedgerSequence = Int32(accountNumber)
+            $0.lastLedgerSequence = Int32(lastLedgerSequence)
             $0.account = keysignPayload.coin.address
             $0.publicKey = publicKey.data
             $0.opPayment = operation
@@ -48,6 +60,16 @@ enum RippleHelper {
     
     static func getPreSignedImageHash(keysignPayload: KeysignPayload, vault: Vault) throws -> [String] {
         let inputData = try getPreSignedInputData(keysignPayload: keysignPayload, vault: vault)
+        
+        print("")
+        print("")
+        print("")
+        print("getPreSignedImageHash")
+        print("INPUT DATA HEX STRING: \(inputData.hexString)")
+        print("")
+        print("")
+        print("")
+        
         let hashes = TransactionCompiler.preImageHashes(coinType: .xrp, txInputData: inputData)
         let preSigningOutput = try TxCompilerPreSigningOutput(serializedData: hashes)
         if !preSigningOutput.errorMessage.isEmpty {
@@ -65,22 +87,49 @@ enum RippleHelper {
     ) throws -> SignedTransactionResult
     {
         
-        
         let publicKey = try CoinFactory.publicKey(asset: keysignPayload.coin.toCoinMeta(), vault: vault)
         
-        let inputData = try getPreSignedInputData(keysignPayload: keysignPayload, vault: vault)
+        print("")
+        print("")
+        print("")
+        print("getSignedTransaction")
+        print("publicKey: \(publicKey)")
+        print("publicKey.data: \(publicKey.bitcoinKeyHash)")
+        print("publicKey.data.hexString: \(publicKey.data.hexString)")
+        print("")
+        print("")
+        print("")
         
+        let inputData = try getPreSignedInputData(keysignPayload: keysignPayload, vault: vault)
         let hashes = TransactionCompiler.preImageHashes(coinType: .xrp, txInputData: inputData)
         let preSigningOutput = try TxCompilerPreSigningOutput(serializedData: hashes)
+        
+        if !preSigningOutput.errorMessage.isEmpty {
+            print(preSigningOutput.errorMessage)
+            throw HelperError.runtimeError(preSigningOutput.errorMessage)
+        }
+        
         let allSignatures = DataVector()
         let publicKeys = DataVector()
+        
         let signatureProvider = SignatureProvider(signatures: signatures)
         
-        
-        let signature = signatureProvider.getSignatureWithRecoveryID(preHash: preSigningOutput.dataHash)
-        guard publicKey.verify(signature: signature, message: preSigningOutput.dataHash) else {
-            throw HelperError.runtimeError("fail to verify signature")
+        // If I use datahash it is not finding anything
+        let signature = signatureProvider.getDerSignature(preHash: preSigningOutput.data)
+        guard publicKey.verifyAsDER(signature: signature, message: preSigningOutput.data) else {
+            let errorMessage = "Invalid signature"
+            print("\(errorMessage)")
+                  throw HelperError.runtimeError(errorMessage)
         }
+        
+        print("")
+        print("")
+        print("")
+        print("getSignedTransaction")
+        print("INPUT DATA HEX STRING: \(inputData.hexString)")
+        print("")
+        print("")
+        print("")
         
         allSignatures.add(data: signature)
         publicKeys.add(data: publicKey.data)
@@ -91,6 +140,7 @@ enum RippleHelper {
         
         let output = try RippleSigningOutput(serializedData: compileWithSignature)
         
+        // The error is HERE it accepted it as a DER previously
         if !output.errorMessage.isEmpty {
             let errorMessage = output.errorMessage
             print("errorMessage: \(errorMessage)")
