e57b1907c5501e35945e34812a4fa610c090310c
Johnny Luo <johnnyluo1980@gmail.com>
2025-01-23 08:38:56 +1100

    WIP: add DKLS fast vault

--- VultisigApp/VultisigApp/Services/FastVault/FastVaultService.swift
+++ VultisigApp/VultisigApp/Services/FastVault/FastVaultService.swift
@@ -11,7 +11,8 @@ final class FastVaultService {
 
     static let shared = FastVaultService()
 
-    private let endpoint = "https://api.vultisig.com/vault"
+    //private let endpoint = "https://api.vultisig.com/vault"
+    private let endpoint = "http://127.0.0.1:8080/vault"
 
     static func localPartyID(sessionID: String) -> String {
         guard let data = sessionID.data(using: .utf8) else {
@@ -53,10 +54,11 @@ final class FastVaultService {
         hexEncryptionKey: String,
         hexChainCode: String,
         encryptionPassword: String,
-        email: String
+        email: String,
+        lib_type: Int
     ) {
         let localPartyID = Self.localPartyID(sessionID: sessionID)
-        let req = VaultCreateRequest(name: name, session_id: sessionID, hex_encryption_key: hexEncryptionKey, hex_chain_code: hexChainCode, local_party_id: localPartyID, encryption_password: encryptionPassword, email: email)
+        let req = VaultCreateRequest(name: name, session_id: sessionID, hex_encryption_key: hexEncryptionKey, hex_chain_code: hexChainCode, local_party_id: localPartyID, encryption_password: encryptionPassword, email: email,lib_type: lib_type)
 
         Utils.sendRequest(urlString: "\(endpoint)/create", method: "POST", headers: [:], body: req) { _ in
             print("Send create request to Vultiserver successfully")
