5f1c88cca374b72d8c1acf7b6b43252d55e510d6
Enrique <enriquesouza@live.com>
2025-01-31 20:51:38 -0300

    get metadata

--- VultisigApp/VultisigApp/Services/Sui/SuiService.swift
+++ VultisigApp/VultisigApp/Services/Sui/SuiService.swift
@@ -36,12 +36,12 @@ class SuiService {
         
         let allTokens = try await getAllTokensWithMetadata(coin: coin)
         
-
+        
         guard let totalBalance = Utils.extractResultFromJson(
             fromData: data,
             path: "result.totalBalance"
         ) as? String else { throw Errors.getBalanceFailed }
-
+        
         return totalBalance
     }
     
@@ -53,12 +53,12 @@ class SuiService {
         )
         
         print(String(data: data, encoding: .utf8) ?? "")
-
+        
         guard let totalBalance = Utils.extractResultFromJson(
             fromData: data,
             path: "result.totalBalance"
         ) as? String else { throw Errors.getBalanceFailed }
-
+        
         return totalBalance
     }
     
@@ -119,37 +119,36 @@ class SuiService {
     
     func getAllTokens(coin: Coin) async throws -> [[String: String]] {
         let cacheKey = "\(coin.chain.name.lowercased())-\(coin.address)-suix_getOwnedObjects"
-
+        
         if let cachedData = await Utils.getCachedData(cacheKey: cacheKey, cache: cacheAllCoins, timeInSeconds: 60*5) {
             return cachedData
         }
-
+        
         do {
             let data = try await Utils.PostRequestRpc(rpcURL: rpcURL, method: "suix_getOwnedObjects", params: [coin.address])
-
+            
             if let objects: [[String: Any]] = Utils.extractResultFromJson(fromData: data, path: "result.data") as? [[String: Any]] {
                 var tokens: [[String: String]] = []
-
+                
                 for obj in objects {
                     if let objData = obj["data"] as? [String: Any],
                        let objectId = objData["objectId"] as? String {
-
+                        
                         // Fetch object details
                         let objectDetails = try await Utils.PostRequestRpc(rpcURL: rpcURL, method: "sui_getObject", params: [objectId, ["showContent": true]])
                         
-                        // Debugging: Print the full response
-                        // print(String(data: objectDetails, encoding: .utf8) ?? "Invalid JSON")
-
-                        // ✅ Extract `coinType` from `result.data.content.type`
                         if let coinType = Utils.extractResultFromJson(fromData: objectDetails, path: "result.data.content.type") as? String {
-                            tokens.append([
-                                "objectID": objectId,
-                                "coinType": coinType
-                            ])
+                            if let start = coinType.range(of: "<"), let end = coinType.range(of: ">") {
+                                let extractedType = String(coinType[start.upperBound..<end.lowerBound])
+                                tokens.append([
+                                    "objectID": objectId,
+                                    "coinType": extractedType
+                                ])
+                            }
                         }
                     }
                 }
-
+                
                 self.cacheAllCoins.set(cacheKey, (data: tokens, timestamp: Date()))
                 return tokens
             } else {
@@ -165,13 +164,15 @@ class SuiService {
     func getAllTokensWithMetadata(coin: Coin) async throws -> [[String: String]] {
         let allTokens = try await getAllTokens(coin: coin) // Get tokens first
         
-        
         var tokensWithMetadata: [[String: String]] = []
-
+        
         for token in allTokens {
-            if let objType = token["type"] {
+            if let objType = token["coinType"] {
                 do {
+                    
                     let metadata = try await Utils.PostRequestRpc(rpcURL: rpcURL, method: "suix_getCoinMetadata", params: [objType])
+                    
+                    print(String(data: metadata, encoding: .utf8) ?? "")
                     
                     let tokenData: [String: String] = [
                         "objectID": token["objectID"] ?? "",
@@ -211,7 +212,7 @@ class SuiService {
 }
 
 private extension SuiService {
-
+    
     enum Errors: Error {
         case getBalanceFailed
     }
