7bfa81629abc7e7720b008afaac103e94c5b66a7
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-02-18 07:30:34 +0330

    Add support for eip-712 (#1843)
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

--- VultisigApp/VultisigApp/Services/Keysign/CustomMessagePayload.swift
+++ VultisigApp/VultisigApp/Services/Keysign/CustomMessagePayload.swift
@@ -12,7 +12,14 @@ struct CustomMessagePayload: Codable, Hashable {
     let message: String
 
     var keysignMessages: [String] {
-        let data = Data(message.utf8)
+        let data: Data
+
+        if message.starts(with: "0x"){
+            data = Data(hex: message)
+        }else {
+            data = Data(message.utf8)
+        }
+
         let hash = data.sha3(.keccak256)
         return [hash.hexString]
     }
