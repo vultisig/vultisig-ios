e13d99d167adce10724febc168fd3ea9d3ebf478
Enrique <enriquesouza@live.com>
2025-03-08 19:53:20 -0300

    hint

--- VultisigApp/VultisigApp/Views/Keygen/FastVaultEnterPasswordView.swift
+++ VultisigApp/VultisigApp/Views/Keygen/FastVaultEnterPasswordView.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 struct FastVaultEnterPasswordView: View {
-
+    
     @AppStorage("isBiometryEnabled") var isBiometryEnabled: Bool = true
-
+    
     @State var isLoading: Bool = false
     @State var isWrongPassword: Bool = false
-
+    
     @Binding var password: String
-
+    
     @Environment(\.dismiss) var dismiss
-
+    
     let vault: Vault
     let onSubmit: (() -> Void)?
-
+    
     private let keychain = DefaultKeychainService.shared
     private let biometryService = BiometryService.shared
-
+    
     var view: some View {
         VStack {
             passwordField
@@ -35,29 +35,30 @@ struct FastVaultEnterPasswordView: View {
             tryAuthenticate()
         }
     }
-
+    
     var passwordField: some View {
         VStack(alignment: .leading, spacing: 12) {
             Text("FastVault password")
                 .font(.body14MontserratMedium)
                 .foregroundColor(.neutral0)
-
+            
             textfield
         }
         .padding(.horizontal, 16)
         .padding(.top, 30)
     }
-
+    
     var textfield: some View {
         HiddenTextField(placeholder: "enterPassword", password: $password)
             .padding(.top, 8)
     }
-
+    
     var disclaimer: some View {
         OutlinedDisclaimer(text: NSLocalizedString("fastVaultEnterDisclaimer", comment: ""))
             .padding(.horizontal, 16)
+        
     }
-
+    
     var buttons: some View {
         VStack(spacing: 20) {
             continueButton
@@ -66,7 +67,7 @@ struct FastVaultEnterPasswordView: View {
         .padding(.bottom, 40)
         .padding(.horizontal, 16)
     }
-
+    
     var continueButton: some View {
         Button(action: {
             Task {
@@ -78,24 +79,30 @@ struct FastVaultEnterPasswordView: View {
         .opacity(isSaveButtonDisabled ? 0.5 : 1)
         .disabled(isSaveButtonDisabled)
         .buttonStyle(.plain)
-        .alert(NSLocalizedString("wrongPassword", comment: ""), isPresented: $isWrongPassword) {
-            Button("OK", role: .cancel) { }
-        }
+        .alert(
+            isPresented: $isWrongPassword,
+            content: {
+                let hint = keychain.getFastHint(pubKeyECDSA: vault.pubKeyECDSA)
+                let message = NSLocalizedString("wrongPassword", comment: "") +
+                (hint != nil ? NSLocalizedString("wrongPasswordHint", comment: "") + (hint ?? "") : "")
+                return Alert(title: Text(message), dismissButton: .default(Text("OK")))
+            }
+        )
     }
-
+    
     var isSaveButtonDisabled: Bool {
         return password.isEmpty
     }
-
+    
     @MainActor func checkPassword() async {
         isLoading = true
         defer { isLoading = false }
-
+        
         let isValidPassword = await FastVaultService.shared.get(
             pubKeyECDSA: vault.pubKeyECDSA,
             password: password
         )
-
+        
         if isValidPassword {
             savePassword()
             onSubmit?()
@@ -104,20 +111,20 @@ struct FastVaultEnterPasswordView: View {
             isWrongPassword = true
         }
     }
-
+    
     func savePassword() {
         keychain.setFastPassword(password, pubKeyECDSA: vault.pubKeyECDSA)
     }
-
+    
     func tryAuthenticate() {
         guard let fastPassword = keychain.getFastPassword(pubKeyECDSA: vault.pubKeyECDSA) else {
             return
         }
-
+        
         guard !fastPassword.isEmpty, isBiometryEnabled else {
             return
         }
-
+        
         biometryService.authenticate(
             reason: "Authenticate to fill FastServer password",
             onSuccess: {
