2fd9912bb60f8acf355c7dff9bb78fe5070d32bd
Amol Kumar <amolkmr10@gmail.com>
2025-03-27 15:52:06 -0400

    SwapDetailsSummary updated

--- VultisigApp/VultisigApp/Views/Components/Swap/SwapDetailsSummary.swift
+++ VultisigApp/VultisigApp/Views/Components/Swap/SwapDetailsSummary.swift
@@ -11,33 +11,27 @@ struct SwapDetailsSummary: View {
     @ObservedObject var tx: SwapTransaction
     @ObservedObject var swapViewModel: SwapCryptoViewModel
     
+    @State var showFees: Bool = false
+    
     var body: some View {
         content
+            .animation(.easeInOut, value: showFees)
     }
     
     var content: some View {
         VStack(spacing: 16) {
             if let providerName = tx.quote?.displayName {
-                getSummaryCell(leadingText: "provider", trailingText: providerName, image: providerName)
-            }
-            
-            if swapViewModel.showFees(tx: tx) {
-                getSummaryCell(leadingText: "swapFee", trailingText: swapViewModel.swapFeeString(tx: tx))
-            }
-            
-            if swapViewModel.showGas(tx: tx) {
                 getSummaryCell(
-                    leadingText: "networkFee",
-                    trailingText: "\(swapViewModel.swapGasString(tx: tx))(~\(swapViewModel.approveFeeString(tx: tx)))"
+                    leadingText: "provider",
+                    trailingText: providerName
                 )
             }
             
             if swapViewModel.showTotalFees(tx: tx) {
-                getSummaryCell(
-                    leadingText: "totalFee",
-                    trailingText: "\(swapViewModel.totalFeeString(tx: tx))"
-                )
+                totalFees
             }
+            
+            otherFees
             
             if let error = swapViewModel.error {
                 Separator()
@@ -45,6 +39,70 @@ struct SwapDetailsSummary: View {
             }
         }
         .padding(.top, 8)
+    }
+    
+    var totalFees: some View {
+        Button {
+            showFees.toggle()
+        } label: {
+            totalFeesLabel
+        }
+    }
+    
+    var totalFeesLabel: some View {
+        HStack {
+            getSummaryCell(
+                leadingText: "totalFee",
+                trailingText: "\(swapViewModel.totalFeeString(tx: tx))"
+            )
+            
+            chevron
+        }
+    }
+    
+    var chevron: some View {
+        Image(systemName: "chevron.up")
+            .font(.body12BrockmannMedium)
+            .foregroundColor(.neutral0)
+            .rotationEffect(Angle(degrees: showFees ? 0 : 180))
+    }
+    
+    var otherFees: some View {
+        HStack {
+            Rectangle()
+                .frame(width: 1)
+                .foregroundColor(.persianBlue200)
+            
+            expandableFees
+        }
+        .frame(maxHeight: showFees ? nil : 0)
+        .clipped()
+    }
+    
+    var expandableFees: some View {
+        VStack(spacing: 16) {
+            if swapViewModel.showFees(tx: tx) {
+                swapFees
+            }
+            
+            if swapViewModel.showGas(tx: tx) {
+                swapGas
+            }
+        }
+    }
+    
+    var swapFees: some View {
+        getSummaryCell(
+            leadingText: "swapFee",
+            trailingText: swapViewModel.swapFeeString(tx: tx)
+        )
+    }
+    
+    var swapGas: some View {
+        getSummaryCell(
+            leadingText: "networkFee",
+            trailingText: "\(swapViewModel.swapGasString(tx: tx))(\(swapViewModel.approveFeeString(tx: tx)))"
+        )
     }
     
     func getProvider() -> String? {
@@ -60,21 +118,18 @@ struct SwapDetailsSummary: View {
         }
     }
     
-    private func getSummaryCell(leadingText: String, trailingText: String, image: String? = nil) -> some View {
+    private func getSummaryCell(leadingText: String, trailingText: String) -> some View {
         HStack {
             Text(NSLocalizedString(leadingText, comment: ""))
                 .foregroundColor(.extraLightGray)
             
             Spacer()
             
-            if let image {
-                getImage(image)
-            }
-            
             Text(trailingText)
                 .foregroundColor(.lightText)
+                .redacted(reason: swapViewModel.isLoading ? .placeholder : [])
         }
-        .font(.body12MenloBold)
+        .font(.body12BrockmannMedium)
     }
     
     private func getImage(_ image: String) -> some View {
