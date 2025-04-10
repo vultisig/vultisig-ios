d762e0dcd7964155f8fb71ac13aa6e5158775f53
Enrique <enriquesouza@live.com>
2025-04-10 18:11:29 -0300

    hydra review fixes

--- VultisigApp/VultisigApp/Services/CoinService.swift
+++ VultisigApp/VultisigApp/Services/CoinService.swift
@@ -96,7 +96,7 @@ struct CoinService {
             case .Sui:
                 tokens = try await SuiService.shared.getAllTokensWithMetadata(coin: nativeToken)
             case .THORChain:
-                tokens = try await ThorchainService.shared.fetchTokens(coin: nativeToken)
+                tokens = try await ThorchainService.shared.fetchTokens(nativeToken.address)
             default:
                 tokens = []
             }
