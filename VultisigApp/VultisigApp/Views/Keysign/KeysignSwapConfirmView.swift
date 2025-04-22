e364093d2d4ab7e2d9cbfb5a31e9c965246efdae
Amol Kumar <amolkmr10@gmail.com>
2025-04-22 17:46:46 -0400

    WIP: Crad upgrade

--- VultisigApp/VultisigApp/Views/Keysign/KeysignSwapConfirmView.swift
+++ VultisigApp/VultisigApp/Views/Keysign/KeysignSwapConfirmView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import BigInt
 
 struct KeysignSwapConfirmView: View {
     @ObservedObject var viewModel: JoinKeysignViewModel
@@ -18,28 +19,27 @@ struct KeysignSwapConfirmView: View {
     }
 
     var fields: some View {
-        ScrollView {
-            VStack(spacing: 30) {
-                summary
-            }
+        VStack {
+            Spacer()
+            summary
+            Spacer()
         }
     }
 
     var summary: some View {
         VStack(spacing: 16) {
-            getValueCell(for: "action", with: getAction())
-            Separator()
-            getValueCell(for: "provider", with: getProvider())
-            Separator()
-            getValueCell(for: "fromAsset", with: getFromAmount())
-            Separator()
-            getValueCell(for: "toAsset", with: getToAmount())
-            if showApprove {
-                Separator()
-                getValueCell(for: "Allowance spender", with: getSpender())
-                Separator()
-                getValueCell(for: "Allowance amount", with: getAmount())
-            }
+            summaryTitle
+            summaryFromToContent
+            
+            separator
+            getValueCell(
+                for: "provider",
+                with: getProvider(),
+                showIcon: true
+            )
+            
+            separator
+            getValueCell(for: "NetworkFee", with: getCalculatedNetworkFee())
         }
         .padding(16)
         .background(Color.blue600)
@@ -53,6 +53,72 @@ struct KeysignSwapConfirmView: View {
             FilledButton(title: "sign")
         }
         .padding(20)
+    }
+    
+    var summaryTitle: some View {
+        Text(NSLocalizedString("youreBridging", comment: ""))
+            .font(.body14BrockmannMedium)
+            .foregroundColor(.lightText)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    var summaryFromToContent: some View {
+        HStack {
+            summaryFromToIcons
+            summaryFromTo
+        }
+    }
+    
+    var summaryFromToIcons: some View {
+        VStack(spacing: 0) {
+            getCoinIcon(for: viewModel.keysignPayload?.swapPayload?.fromCoin)
+            verticalSeparator
+            chevronIcon
+            verticalSeparator
+            getCoinIcon(for: viewModel.keysignPayload?.swapPayload?.toCoin)
+        }
+    }
+    
+    var verticalSeparator: some View {
+        Rectangle()
+            .frame(width: 1, height: 12)
+            .foregroundColor(.blue400)
+    }
+    
+    var summaryFromTo: some View {
+        VStack(spacing: 16) {
+            let payload = viewModel.keysignPayload?.swapPayload
+            
+            getSwapAssetCell(
+                for: getFromAmount(),
+                with: payload?.fromCoin.ticker,
+                on: payload?.fromCoin.chain
+            )
+            
+            separator
+                .padding(.leading, 12)
+            
+            getSwapAssetCell(
+                for: getToAmount(),
+                with: viewModel.keysignPayload?.swapPayload?.toCoin.ticker,
+                on: viewModel.keysignPayload?.swapPayload?.toCoin.chain
+            )
+        }
+    }
+    
+    var separator: some View {
+        Separator()
+            .opacity(0.2)
+    }
+    
+    var chevronIcon: some View {
+        Image(systemName: "arrow.down")
+            .font(.body12BrockmannMedium)
+            .foregroundColor(.persianBlue200)
+            .padding(6)
+            .background(Color.blue400)
+            .cornerRadius(32)
+            .bold()
     }
 
     func getAction() -> String {
@@ -111,19 +177,142 @@ struct KeysignSwapConfirmView: View {
         }
     }
 
-    func getValueCell(for title: String, with value: String) -> some View {
-        VStack(alignment: .leading, spacing: 8) {
+    func getValueCell(
+        for title: String,
+        with value: String,
+        bracketValue: String? = nil,
+        showIcon: Bool = false
+    ) -> some View {
+        HStack(spacing: 4) {
             Text(NSLocalizedString(title, comment: ""))
-                .font(.body20MontserratSemiBold)
-                .foregroundColor(.neutral0)
-
+                .foregroundColor(.extraLightGray)
+            
+            Spacer()
+            
+            if showIcon {
+                Image(value)
+                    .resizable()
+                    .frame(width: 16, height: 16)
+            }
+            
             Text(value)
-                .font(.body12Menlo)
-                .foregroundColor(.turquoise600)
+                .foregroundColor(.neutral0)
+            
+            if let bracketValue {
+                Text(bracketValue)
+                    .foregroundColor(.extraLightGray)
+            }
+            
+        }
+        .font(.body14BrockmannMedium)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private func getSwapAssetCell(
+        for amount: String?,
+        with ticker: String?,
+        on chain: Chain? = nil
+    ) -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Group {
+                Text(amount ?? "")
+                    .foregroundColor(.neutral0) +
+                Text(" ") +
+                Text(ticker ?? "")
+                    .foregroundColor(.extraLightGray)
+            }
+            .font(.body18BrockmannMedium)
+            
+            if let chain {
+                HStack(spacing: 2) {
+                    Text(NSLocalizedString("on", comment: ""))
+                        .foregroundColor(.extraLightGray)
+                        .padding(.trailing, 4)
+                    
+                    Image(chain.logo)
+                        .resizable()
+                        .frame(width: 12, height: 12)
+                    
+                    Text(chain.name)
+                        .foregroundColor(.neutral0)
+                    
+                    Spacer()
+                }
+                .font(.body10BrockmannMedium)
+                .offset(x: 2)
+            }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
     }
 
+    private func getCoinIcon(for coin: Coin?) -> some View {
+        AsyncImageView(
+            logo: coin?.logo ?? "",
+            size: CGSize(width: 28, height: 28),
+            ticker: coin?.ticker ?? "",
+            tokenChainLogo: nil
+        )
+        .overlay(
+            Circle()
+                .stroke(Color.blue400, lineWidth: 2)
+        )
+    }
+    
+    func getCalculatedNetworkFee() -> String {
+        guard let payload = viewModel.keysignPayload else {
+            return .zero
+        }
+
+        guard let nativeToken = TokensStore.TokenSelectionAssets.first(where: {
+            $0.isNativeToken && $0.chain == payload.coin.chain
+        }) else {
+            return .zero
+        }
+
+        if payload.coin.chainType == .EVM {
+            let gas = payload.chainSpecific.gas
+
+            guard let weiPerGWeiDecimal = Decimal(string: EVMHelper.weiPerGWei.description),
+                  let gasDecimal = Decimal(string: gas.description) else {
+                return .empty
+            }
+
+            let gasGwei = gasDecimal / weiPerGWeiDecimal
+            let gasInReadable = gasGwei.formatToDecimal(digits: nativeToken.decimals)
+
+            var feeInReadable = feesInReadable(coin: payload.coin, fee: payload.chainSpecific.fee)
+            feeInReadable = feeInReadable.nilIfEmpty.map { " (~\($0))" } ?? ""
+
+            return "\(gasInReadable) \(payload.coin.chain.feeUnit)\(feeInReadable)"
+        }
+
+        let gasAmount = Decimal(payload.chainSpecific.gas) / pow(10, nativeToken.decimals)
+        let gasInReadable = gasAmount.formatToDecimal(digits: nativeToken.decimals)
+
+        var feeInReadable = feesInReadable(coin: payload.coin, fee: payload.chainSpecific.gas)
+        feeInReadable = feeInReadable.nilIfEmpty.map { " (~\($0))" } ?? ""
+
+        return "\(gasInReadable) \(payload.coin.chain.feeUnit)\(feeInReadable)"
+    }
+    
+    func feesInReadable(coin: Coin, fee: BigInt) -> String {
+        var nativeCoinAux: Coin?
+        
+        if coin.isNativeToken {
+            nativeCoinAux = coin
+        } else {
+            nativeCoinAux = ApplicationState.shared.currentVault?.coins.first(where: { $0.chain == coin.chain && $0.isNativeToken })
+        }
+        
+        
+        
+        guard let nativeCoin = nativeCoinAux else {
+            return ""
+        }
+        
+        let fee = nativeCoin.decimal(for: fee)
+        return RateProvider.shared.fiatBalanceString(value: fee, coin: nativeCoin)
+    }
 }
 
 #Preview {
