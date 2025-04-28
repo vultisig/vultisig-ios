a6cf70fc48f82c7c0f0710ad5ffcadbce728d6e3
Amol Kumar <amolkmr10@gmail.com>
2025-04-28 14:30:11 -0400

    SwapChainPickerView window size fix

---
+++ VultisigApp/VultisigApp/iOS/View/SwapChainPickerView+iOS.swift
@@ -0,0 +1,16 @@
+//
+//  SwapChainPickerView+iOS.swift
+//  VultisigApp
+//
+//  Created by Amol Kumar on 2025-04-28.
+//
+
+#if os(iOS)
+import SwiftUI
+
+extension SwapChainPickerView {
+    var body: some View {
+        content
+    }
+}
+#endif
