f63d6188c9d880ad2f1bff1920baaea6eec31ab9
hailhydra <johnnyluo1980@gmail.com>
2025-05-02 13:37:06 +1000

    Fix TCY (#2151)
    * Fix TCY
    
    * upper case ticker

--- VultisigApp/VultisigApp/macOS/View/Send/SendCryptoDetailsView+macOS.swift
+++ VultisigApp/VultisigApp/macOS/View/Send/SendCryptoDetailsView+macOS.swift
@@ -36,7 +36,7 @@ extension SendCryptoDetailsView {
                     fromField
                     toField
                     
-                    if tx.coin.isNativeToken || tx.coin.chainType == .Cosmos {
+                    if tx.coin.isNativeToken || tx.coin.chainType == .Cosmos || tx.coin.ticker == "TCY" {
                         memoField
                     }
                     
