50adff9b5de555e18efc7e152a9c8a9f36f9e050
Johnny Luo <johnnyluo1980@gmail.com>
2025-05-16 09:34:46 +1000

    Enable MoonPay Buy

---
+++ VultisigApp/VultisigApp/Views/Components/MoonPaySignature.swift
@@ -0,0 +1,35 @@
+//
+//  MoonPaySignature.swift
+//  VultisigApp
+//
+//  Created by Johnny Luo on 16/5/2025.
+//
+import Foundation
+struct MoonPaySignatureResp : Codable {
+    let signature: String
+}
+struct MoonPaySignatureReq : Codable {
+    let url: String
+}
+struct MoonPaySignatureHelper {
+    var keysignUrl = "https://moonpay-sign-delta.vercel.app/api/sign"
+    func getSignature(url: String) async -> String {
+        do{
+            print("url to get signature \(url)")
+            let req = MoonPaySignatureReq(url: url)
+            var request = URLRequest(url: URL(string: keysignUrl)!)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            let result = try JSONEncoder().encode(req)
+            request.httpBody = result
+            let (data,resp) = try await URLSession.shared.data(for: request)
+            if let resp = resp as? HTTPURLResponse, resp.statusCode == 200 {
+                let res = try JSONDecoder().decode(MoonPaySignatureResp.self, from: data)
+                return res.signature
+            }
+        } catch {
+            print("fail to get moonpay signature \(error)")
+        }
+        return ""
+    }
+}
