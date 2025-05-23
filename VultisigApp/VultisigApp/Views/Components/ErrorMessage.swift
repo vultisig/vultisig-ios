fa87c1cd2f084a1ba8d36b09fa4ff38df2e734ec
Amol Kumar <amolkmr10@gmail.com>
2025-05-23 02:33:58 -0400

    Support added to upload vault backup from 3rd party app (Google Drive) (#2281)
    * Support added to upload vault backup from 3rd party app (Google Drive)
    
    * Vault file import check fix
    
    ---------
    
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

--- VultisigApp/VultisigApp/Views/Components/ErrorMessage.swift
+++ VultisigApp/VultisigApp/Views/Components/ErrorMessage.swift
@@ -7,28 +7,64 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ErrorMessage: View {
     let text: String
     var width: CGFloat = 200
-    
+    var typewriterSpeed: Double = 0.03
+
+    @State private var revealedText: String = ""
+    @State private var timer: Timer? = nil
+
     var body: some View {
         VStack(spacing: 24) {
-            logo
-            title
+            Image(systemName: "exclamationmark.circle.fill")
+                .font(.body24MontserratBold)
+                .foregroundColor(.alertYellow)
+            Text(revealedText)
+                .font(.body16MenloBold)
+                .foregroundColor(.alertYellow)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: width)
+        }
+        .onAppear {
+            startTypewriterEffect()
+        }
+        .onDisappear {
+            timer?.invalidate()
+            timer = nil
+            revealedText = ""
         }
     }
-    
+
+    private func startTypewriterEffect() {
+        revealedText = ""
+        let fullText = NSLocalizedString(text, comment: "")
+        var currentIndex = 0
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: typewriterSpeed, repeats: true) { t in
+            if currentIndex < fullText.count {
+                let idx = fullText.index(fullText.startIndex, offsetBy: currentIndex + 1)
+                revealedText = String(fullText[..<idx])
+                currentIndex += 1
+            } else {
+                t.invalidate()
+                timer = nil
+            }
+        }
+    }
+
     var logo: some View {
         Image(systemName: "exclamationmark.circle.fill")
             .font(.body24MontserratBold)
             .foregroundColor(.alertYellow)
     }
-    
+
     var title: some View {
         Text(NSLocalizedString(text, comment: ""))
             .font(.body16MenloBold)
             .foregroundColor(.alertYellow)
-            .frame(maxWidth: width)
             .multilineTextAlignment(.center)
     }
 }
