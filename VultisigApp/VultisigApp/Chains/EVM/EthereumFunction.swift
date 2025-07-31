cc7e8a590088a2830ad221301d4fc036737d9581
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-07-31 09:11:15 -0300

    fix: remove force unwrap from EthereumFunction

--- VultisigApp/VultisigApp/Chains/EVM/EthereumFunction.swift
+++ VultisigApp/VultisigApp/Chains/EVM/EthereumFunction.swift
@@ -11,26 +11,26 @@ import WalletCore
 enum EthereumFunction {
     static func transferErc20Encoder(address: String, amount: BigInt) throws -> String {
         guard amount >= .zero else { throw HelperError.runtimeError("Amount must be non-negative") }
-        guard address.isNotEmpty else { throw HelperError.runtimeError("Address cannot be blank") }
+        guard address.isNotEmpty, let destinationAddress = AnyAddress(string: address, coin: CoinType.ethereum) else {
+            throw HelperError.runtimeError("Address is not valid")
+        }
         
-        let destinationAddress = AnyAddress(string: address, coin: CoinType.ethereum)
         let amountOut = amount.serializeForEvm()
-        
         let encodedFunction = EthereumAbiFunction(name: "transfer")
-        encodedFunction.addParamAddress(val: destinationAddress!.data, isOutput: false)
+        encodedFunction.addParamAddress(val: destinationAddress.data, isOutput: false)
         encodedFunction.addParamUInt256(val: amountOut, isOutput: false)
         return EthereumAbi.encode(fn: encodedFunction).toHexString().add0x
     }
 
     static func approvalErc20Encoder(address: String, amount: BigInt) throws -> String {
         guard amount >= .zero else { throw HelperError.runtimeError("Amount must be non-negative") }
-        guard address.isNotEmpty else { throw HelperError.runtimeError("Address cannot be blank") }
+        guard address.isNotEmpty, let destinationAddress = AnyAddress(string: address, coin: CoinType.ethereum) else {
+            throw HelperError.runtimeError("Address is not valid")
+        }
 
-        let destinationAddress = AnyAddress(string: address, coin: CoinType.ethereum)
         let amountOut = amount.serializeForEvm()
-
         let encodedFunction = EthereumAbiFunction(name: "approve")
-        encodedFunction.addParamAddress(val: destinationAddress!.data, isOutput: false)
+        encodedFunction.addParamAddress(val: destinationAddress.data, isOutput: false)
         encodedFunction.addParamUInt256(val: amountOut, isOutput: false)
         return EthereumAbi.encode(fn: encodedFunction).toHexString().add0x
     }
