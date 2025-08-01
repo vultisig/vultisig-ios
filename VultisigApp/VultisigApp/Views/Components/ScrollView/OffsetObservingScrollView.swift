9ef93a0478e68fde5a99aa64e81e7b5bd6769b7d
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-08-01 19:10:58 -0300

    feat: Swap Improvements Follow up #1 (#2656)
    * fix: updated fiat balance color
    
    * feat: added chain and balance headers
    
    * feat: removed checkmark and added proper separator
    
    * feat: updated chain cell colors
    
    * feat: added clear button to chain input
    
    * feat: added SearchTextField for swaps
    
    * fix: removed horizontal padding
    
    * fix: adjusted layout to make chain selector keyboard aware
    
    * feat: display selected token always on top
    
    * feat: carousel with animation
    
    * fix: adjusted for macOS
    
    * fix: screen on ios
    
    * fix: macOS issues with scrollview
    
    * fix: coderabbit comments
    
    ---------
    
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

---
+++ VultisigApp/VultisigApp/Views/Components/ScrollView/OffsetObservingScrollView.swift
@@ -0,0 +1,82 @@
+//
+//  OffsetObservingScrollView.swift
+//  VultisigApp
+//
+//  Created by Gaston Mazzeo on 01/08/2025.
+//
+
+import SwiftUI
+
+/// ScrollView with content offset reading capabilities
+struct OffsetObservingScrollView<Content: View>: View {
+    var axes: Axis.Set = .vertical
+    var showsIndicators = true
+    var contentInset: CGFloat
+    @Binding var scrollOffset: CGFloat
+    @ViewBuilder var content: () -> Content
+
+    private let coordinateSpaceName = UUID()
+
+    init(
+        axes: Axis.Set = .vertical,
+        showsIndicators: Bool = true,
+        contentInset: CGFloat = 0,
+        scrollOffset: Binding<CGFloat>,
+        content: @escaping () -> Content
+    ) {
+        self.axes = axes
+        self.showsIndicators = showsIndicators
+        self.contentInset = contentInset
+        self._scrollOffset = scrollOffset
+        self.content = content
+    }
+
+    var body: some View {
+        ScrollView(axes, showsIndicators: showsIndicators) {
+            contentContainer {
+                insetView
+                content()
+                    .background(GeometryReader { proxy in
+                        Color.clear
+                            .preference(key: ScrollOffsetPreferenceKey.self, value: preferenceValue(proxy: proxy))
+                    })
+                insetView
+            }
+        }
+        .coordinateSpace(name: coordinateSpaceName)
+        .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
+            scrollOffset = value
+        }
+    }
+}
+
+private extension OffsetObservingScrollView {
+    @ViewBuilder
+    func contentContainer<ScrollViewContent: View>(@ViewBuilder content: () -> ScrollViewContent) -> some View {
+        if axes == .vertical {
+            VStack(spacing: 0) { content() }
+        } else {
+            HStack(spacing: 0) { content() }
+        }
+    }
+    var insetView: some View {
+        if axes == .vertical {
+            Color.clear.frame(height: contentInset)
+        } else {
+            Color.clear.frame(width: contentInset)
+        }
+    }
+    
+    func preferenceValue(proxy: GeometryProxy) -> CGFloat {
+        let frame = proxy.frame(in: .named(coordinateSpaceName))
+        return axes == .vertical ? frame.minY : frame.minX
+    }
+}
+
+private struct ScrollOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value += nextValue()
+    }
+}
