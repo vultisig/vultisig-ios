bee8633a5bb7b04cce39c45a383d28f0a980df54
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-08-12 14:22:32 -0300

    feat: unified swap payloads for 1inch, kyber & lifi into GenericSwapPayload and pass provider through proto mappable

---
+++ VultisigApp/VultisigApp/Services/Keysign/Swap/SwapProviderId.swift
@@ -0,0 +1,23 @@
+//
+//  SwapProviderId.swift
+//  VultisigApp
+//
+//  Created by Gaston Mazzeo on 12/08/2025.
+//
+
+enum SwapProviderId: String, Codable {
+    case oneInch = "1inch"
+    case lifi = "li.fi"
+    case kyberSwap = "kyber"
+    
+    var name: String {
+        switch self {
+        case .oneInch:
+            return "1Inch"
+        case .kyberSwap:
+            return "KyberSwap"
+        case .lifi:
+            return "LI.FI"
+        }
+    }
+}
