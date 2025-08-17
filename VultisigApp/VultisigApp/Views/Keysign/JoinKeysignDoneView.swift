384631b376589a18110eaf27329034879009bd44
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-08-17 17:06:47 -0300

    feat: added done button for overview tx on pair devices

--- VultisigApp/VultisigApp/Views/Keysign/JoinKeysignDoneView.swift
+++ VultisigApp/VultisigApp/Views/Keysign/JoinKeysignDoneView.swift
@@ -11,7 +11,6 @@ struct JoinKeysignDoneView: View {
     let vault: Vault
     @ObservedObject var viewModel: KeysignViewModel
     @Binding var showAlert: Bool
-    
     @State var moveToHome: Bool = false
         
     var body: some View {
@@ -22,26 +21,11 @@ struct JoinKeysignDoneView: View {
                 showAlert: $showAlert,
                 moveToHome: $moveToHome
             )
-            
-            if viewModel.keysignPayload?.swapPayload == nil {
-                continueButton
-            }
         }
         .redacted(reason: viewModel.showRedacted ? .placeholder : [])
         .navigationDestination(isPresented: $moveToHome) {
             HomeView(selectedVault: vault, showVaultsList: false)
         }
-    }
-
-    var continueButton: some View {
-        PrimaryButton(title: "done") {
-            handleTap()
-        }
-        .id(UUID())
-    }
-    
-    private func handleTap() {
-        moveToHome = true
     }
 }
 
