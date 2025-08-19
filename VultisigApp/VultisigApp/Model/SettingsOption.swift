eb426268b251235fddd99fa8ff94099b5956d22b
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-08-19 14:13:52 -0300

    feat: updated settings main screen to new design

---
+++ VultisigApp/VultisigApp/Model/SettingsOption.swift
@@ -0,0 +1,134 @@
+//
+//  SettingsOption.swift
+//  VultisigApp
+//
+//  Created by Gaston Mazzeo on 19/08/2025.
+//
+
+import Foundation
+
+struct SettingsOptionGroup: Identifiable {
+    var id: String { title }
+    let title: String
+    let options: [SettingsOption]
+}
+
+enum SettingsOptionType {
+    case navigation
+    case link(url: URL)
+    case shareLink(url: URL)
+}
+
+enum SettingsOption: Hashable {
+    case vaultSettings
+    case registerVaults
+    case language
+    case currency
+    case addressBook
+    case referralCode
+    case faq
+    case education
+    case checkForUpdates
+    case shareApp
+    case twitter
+    case discord
+    case github
+    case website
+    case privacyPolicy
+    case termsOfService
+    
+    var title: String {
+        switch self {
+        case .vaultSettings:
+            return "vaultSettings"
+        case .registerVaults:
+            return "registerYourVaults"
+        case .language:
+            return "language"
+        case .currency:
+            return "currency"
+        case .addressBook:
+            return "addressBook"
+        case .referralCode:
+            return "referralCode"
+        case .faq:
+            return "faq"
+        case .education:
+            return "vultisigEducation"
+        case .checkForUpdates:
+            return "checkForUpdates"
+        case .shareApp:
+            return "shareTheApp"
+        case .twitter:
+            return "twitter"
+        case .discord:
+            return "discord"
+        case .github:
+            return "github"
+        case .website:
+            return "vultisigWebsite"
+        case .privacyPolicy:
+            return "privacyPolicy"
+        case .termsOfService:
+            return "termsOfService"
+        }
+    }
+    
+    var icon: String? {
+        switch self {
+        case .vaultSettings:
+            return "settings"
+        case .registerVaults:
+            return "logo-outline"
+        case .language:
+            return "languages"
+        case .currency:
+            return "circle-dollar-sign"
+        case .addressBook:
+            return "book-marked"
+        case .referralCode:
+            return "megaphone"
+        case .faq:
+            return "message-circle-question"
+        case .education:
+            return nil
+        case .checkForUpdates:
+            return "cloud-upload"
+        case .shareApp:
+            return "share-2"
+        case .twitter:
+            return "twitter"
+        case .discord:
+            return "discord"
+        case .github:
+            return "github"
+        case .website:
+            return "globe"
+        case .privacyPolicy:
+            return "secure"
+        case .termsOfService:
+            return "notebook-text"
+        }
+    }
+    
+    var type: SettingsOptionType {
+        switch self {
+        case .twitter:
+            return .link(url: StaticURL.XVultisigURL)
+        case .discord:
+            return .link(url: StaticURL.DiscordVultisigURL)
+        case .website:
+            return .link(url: StaticURL.VultisigWeb)
+        case .github:
+            return .link(url: StaticURL.GithubVultisigURL)
+        case .shareApp:
+            return .shareLink(url: StaticURL.AppStoreVultisigURL)
+        case .privacyPolicy:
+            return .link(url: StaticURL.PrivacyPolicyURL)
+        case .termsOfService:
+            return .link(url: StaticURL.TermsOfServiceURL)
+        default:
+            return .navigation
+        }
+    }
+}
---
+++ VultisigApp/VultisigApp/Views/Settings/SettingsMainScreen/SettingsMainScreen.swift
@@ -0,0 +1,247 @@
+//
+//  SettingsMainScreen.swift
+//  VultisigApp
+//
+//  Created by Amol Kumar on 2024-04-05.
+//
+
+import SwiftUI
+
+struct SettingsMainScreen: View {
+    @EnvironmentObject var settingsViewModel: SettingsViewModel
+    @EnvironmentObject var homeViewModel: HomeViewModel
+    
+    @State var tapCount = 0
+    @State var scale: CGFloat = 1
+    @State var showAdvancedSettings: Bool = false
+    
+    @State var selectedOption: SettingsOption?
+
+    let groups: [SettingsOptionGroup] = [
+        SettingsOptionGroup(
+            title: "vault",
+            options: [
+                .vaultSettings,
+                .registerVaults
+            ]
+        ),
+        SettingsOptionGroup(
+            title: "general",
+            options: [
+                .language,
+                .currency,
+                .addressBook,
+                .referralCode
+            ]
+        ),
+        SettingsOptionGroup(
+            title: "support",
+            options: [
+                .faq,
+                .education,
+                .checkForUpdates,
+                .shareApp
+            ]
+        ),
+        SettingsOptionGroup(
+            title: "vultisigCommunity",
+            options: [
+                .twitter,
+                .discord,
+                .github,
+                .website
+            ]
+        ),
+        SettingsOptionGroup(
+            title: "legal",
+            options: [
+                .privacyPolicy,
+                .termsOfService
+            ]
+        )
+    ]
+    
+    var body: some View {
+        Screen(title: "settings".localized) {
+            ScrollView(showsIndicators: false) {
+                LazyVStack(spacing: 14) {
+                    ForEach(groups) { group in
+                        groupView(for: group)
+                    }
+                    appVersion
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                NavigationLink {
+                    if let vault = homeViewModel.selectedVault {
+                        VaultDetailQRCodeView(vault: vault)
+                    }
+                } label: {
+                    NavigationQRCodeButton()
+                }
+            }
+        }
+        .navigationDestination(item: $selectedOption) { option in
+            switch selectedOption {
+            case .vaultSettings:
+                if let vault = homeViewModel.selectedVault {
+                    EditVaultView(vault: vault)
+                } else {
+                    ErrorMessage(text: "errorFetchingVault")
+                }
+            case .registerVaults:
+                if let vault = homeViewModel.selectedVault {
+                    RegisterVaultView(vault: vault)
+                } else {
+                    ErrorMessage(text: "errorFetchingVault")
+                }
+            case .language:
+                SettingsLanguageSelectionView()
+            case .currency:
+                SettingsCurrencySelectionView()
+            case .addressBook:
+                AddressBookView(
+                    shouldReturnAddress: false,
+                    returnAddress: .constant("")
+                )
+            case .referralCode:
+                ReferralView()
+            case .faq:
+                SettingsFAQView()
+            case .education:
+                // TODO: - Check
+                EmptyView()
+            case .checkForUpdates:
+                checkUpdateView
+            default:
+                EmptyView()
+            }
+        }
+        .navigationDestination(isPresented: $showAdvancedSettings) {
+            SettingsAdvancedView()
+        }
+    }
+    
+    func groupView(for group: SettingsOptionGroup) -> some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(group.title.localized)
+                .font(Theme.fonts.caption12)
+                .foregroundStyle(Theme.colors.textExtraLight)
+            
+            VStack(spacing: .zero) {
+                ForEach(group.options, id: \.self) { option in
+                    optionView(for: option)
+                    GradientListSeparator()
+                        .showIf(option != group.options.last)
+                }
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Theme.colors.bgSecondary)
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
+    }
+    
+    func optionView(for option: SettingsOption) -> some View {
+        optionContainerView(for: option) {
+            HStack(spacing: 12) {
+                if let icon = option.icon {
+                    Icon(named: icon, color: Theme.colors.primaryAccent4, size: 20)
+                }
+                
+                Text(option.title.localized)
+                    .font(Theme.fonts.footnote)
+                    .foregroundStyle(Theme.colors.textPrimary)
+                
+                Spacer()
+                
+                if let description = description(for: option) {
+                    Text(description)
+                        .font(Theme.fonts.footnote)
+                        .foregroundStyle(Theme.colors.textPrimary)
+                }
+                
+                Icon(
+                    named: "chevron-right",
+                    color: Theme.colors.textExtraLight,
+                    size: 16
+                )
+            }
+            .padding(.horizontal, 12)
+            .padding(.vertical, 16)
+        }
+    }
+    
+    @ViewBuilder
+    func optionContainerView<Content: View>(for option: SettingsOption, content: () -> Content) -> some View {
+        switch option.type {
+        case .navigation:
+            Button {
+                selectedOption = option
+            } label: {
+                content()
+            }
+        case .link(let url):
+            Link(destination: url, label: content)
+        case .shareLink(let url):
+            ShareLink(item: url, label: content)
+        }
+    }
+    
+    func description(for option: SettingsOption) -> String? {
+        switch option {
+        case .language:
+            return settingsViewModel.selectedLanguage.rawValue
+        case .currency:
+            return settingsViewModel.selectedCurrency.rawValue
+        default:
+            return nil
+        }
+    }
+    
+    var appVersion: some View {
+        Text(Bundle.main.appVersionString)
+            .font(Theme.fonts.caption12)
+            .foregroundColor(Theme.colors.textExtraLight)
+            .scaleEffect(scale)
+            .onTapGesture {
+                handleVersionTap()
+            }
+    }
+    
+    private func handleVersionTap() {
+        tapCount += 1
+        
+        withAnimation(.spring(duration: 0.1)) {
+            scale = 1.1
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            withAnimation(.easeInOut) {
+                scale = 1.0
+            }
+            
+            if tapCount > 4 {
+                tapCount = 0
+                showAdvancedSettings = true
+            }
+        }
+    }
+    
+    var checkUpdateView: some View {
+        #if os(macOS)
+            MacCheckUpdateView()
+        #else
+            PhoneCheckUpdateView()
+        #endif
+    }
+}
+
+#Preview {
+    SettingsMainScreen()
+        .environmentObject(SettingsViewModel())
+        .environmentObject(HomeViewModel())
+}
