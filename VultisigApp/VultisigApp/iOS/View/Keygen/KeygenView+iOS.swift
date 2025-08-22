172ab6f63fdd72d9c0f54d9d06c8e49ad6708601
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-08-22 19:36:26 -0300

    fix: adjusted layout for vault creation error (#2817)
    Co-authored-by: hailhydra <johnnyluo1980@gmail.com>

--- VultisigApp/VultisigApp/iOS/View/Keygen/KeygenView+iOS.swift
+++ VultisigApp/VultisigApp/iOS/View/Keygen/KeygenView+iOS.swift
@@ -30,27 +30,6 @@ extension KeygenView {
             }
     }
     
-    var fields: some View {
-        VStack(spacing: 12) {
-            Spacer()
-            if showProgressRing {
-                if progressCounter<4 {
-                    title
-                }
-                states
-            }
-            Spacer()
-            
-            if progressCounter < 4 {
-                if viewModel.status == .KeygenFailed {
-                    retryButton
-                } else {
-                    progressContainer
-                }
-            }
-        }
-    }
-    
     var progressContainer: some View {
         KeygenProgressContainer(progressCounter: progressCounter)
             .padding(.bottom, idiom == .phone ? 10 : 50)
