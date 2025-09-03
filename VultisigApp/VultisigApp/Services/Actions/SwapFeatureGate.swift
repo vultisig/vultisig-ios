f8ebb6c43e930bab4905fcbbdfb1fa35a7d44afd
hailhydra <johnnyluo1980@gmail.com>
2025-09-04 07:40:38 +1000

    block swap feature for JP,GB and MY (#2891)
    * block swap feature for JP,GB and MY
    
    * remove a function that is not used
    
    * remove unused method

---
+++ VultisigApp/VultisigApp/Services/Actions/SwapFeatureGate.swift
@@ -0,0 +1,17 @@
+//
+//  SwapFeatureGate.swift
+//  VultisigApp
+//
+//  Created by Johnny Luo on 3/9/2025.
+//
+
+import Foundation
+struct SwapFeatureGate {
+    static func canSwap() -> Bool {
+        let localeCode = Locale.current.region?.identifier
+        if localeCode == "GB" || localeCode == "JP" || localeCode == "MY"{
+            return false
+        }
+        return true
+    }
+}
