ba4dcae283b7565ded3f6e2ecc00bb77b963b82f
Johnny Luo <johnnyluo1980@gmail.com>
2025-09-03 20:33:07 +1000

    remove a function that is not used

--- VultisigApp/VultisigApp/Services/Actions/SwapFeatureGate.swift
+++ VultisigApp/VultisigApp/Services/Actions/SwapFeatureGate.swift
@@ -7,11 +7,6 @@
 
 import Foundation
 struct SwapFeatureGate {
-    static func isOutsideAppStoreDistribution() -> Bool {
-        let profilePath = Bundle.main.path(forResource: "embedded", ofType: "provisionprofile")
-        return profilePath != nil
-    }
-    
     static func canSwap() -> Bool {
         let localeCode = Locale.current.region?.identifier
         if localeCode == "GB" || localeCode == "JP" || localeCode == "MY"{
