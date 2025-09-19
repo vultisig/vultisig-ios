ca5450d5153d5060a6914a7cd6047fbfa1533fd2
Johnny Luo <johnnyluo1980@gmail.com>
2025-09-19 16:48:25 +1000

    Change set method to use synchronous execution for thread safety

--- VultisigApp/VultisigApp/Utils/ThreadSafeDictionary.swift
+++ VultisigApp/VultisigApp/Utils/ThreadSafeDictionary.swift
@@ -11,7 +11,7 @@ final class ThreadSafeDictionary<Key: Hashable & Sendable, Value: Sendable> : @u
     }
     
     func set(_ key: Key, _ value: Value) {
-        queue.async(flags: .barrier) {
+        queue.sync {
             self.dictionary[key] = value
         }
     }
