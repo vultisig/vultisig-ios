b38aa13d6f7676aa1696f8439dc6e42e0cd40774
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-10-08 16:16:49 -0300

    feat: moved to separate file

--- VultisigApp/VultisigApp/Views/Components/ImageView/AsyncImageView.swift
+++ VultisigApp/VultisigApp/Views/Components/ImageView/AsyncImageView.swift
@@ -71,19 +71,3 @@ struct AsyncImageView: View {
             .cornerRadius(100)
     }
 }
-
-struct ChainIconView: View {
-    let icon: String
-    let size: CGFloat
-    
-    var body: some View {
-        Image(icon)
-            .resizable()
-            .aspectRatio(1, contentMode: .fit)
-            .frame(width: size, height: size)
-            .foregroundStyle(Theme.colors.bgSecondary)
-            .padding(size / 2)
-            .background(Circle().fill(Theme.colors.textPrimary))
-            .overlay(Circle().inset(by: -1).stroke(Theme.colors.bgSecondary, lineWidth: 2))
-    }
-}
