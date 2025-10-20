3308c7520218d1d8dee0c25f50e10a50096c1ae1
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-10-20 17:37:06 -0300

    feat: added UI for LPs (#3236)

--- VultisigApp/VultisigApp/Features/Defi/DefiTHORChain/View/LPs/DefiTHORChainLPsView.swift
+++ VultisigApp/VultisigApp/Features/Defi/DefiTHORChain/View/LPs/DefiTHORChainLPsView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct DefiTHORChainLPsView: View {
+    @ObservedObject var vault: Vault
+    var onRemove: (LPPosition) -> Void
+    var onAdd: (LPPosition) -> Void
+    
+    // TODO: - Fetch from RPC
+    let lpPositions: [LPPosition] = [
+        LPPosition(
+            coin1: TokensStore.TokenSelectionAssets.first(where: { $0.ticker == "RUNE" && $0.isNativeToken }) ?? .example,
+            coin1Amount: 800,
+            coin2: TokensStore.TokenSelectionAssets.first(where: { $0.ticker == "ETH" && $0.isNativeToken && $0.chain == .ethereum }) ?? .example,
+            coin2Amount: 2,
+            apr: 0.05
+        ),
+        LPPosition(
+            coin1: TokensStore.TokenSelectionAssets.first(where: { $0.ticker == "RUNE" && $0.isNativeToken }) ?? .example,
+            coin1Amount: 800,
+            coin2: TokensStore.TokenSelectionAssets.first(where: { $0.ticker == "USDC" && $0.chain == .ethereum }) ?? .example,
+            coin2Amount: 2,
+            apr: 0.1
+        )
+    ]
+    
     var body: some View {
-        EmptyView()
+        LazyVStack(spacing: 14) {
+            ForEach(lpPositions) { position in
+                DefiTHORChainLPPositionView(
+                    vault: vault,
+                    position: position,
+                    onRemove: { onRemove(position) },
+                    onAdd: { onAdd(position) }
+                )
+            }
+        }
     }
 }
 
 #Preview {
-    DefiTHORChainLPsView()
+    DefiTHORChainLPsView(
+        vault: .example,
+        onRemove: { _ in },
+        onAdd: { _ in },
+    )
 }
