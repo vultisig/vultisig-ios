6cd70736533c6e1c62e33c539b17d6f4fd003ddd
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-10-24 10:01:58 -0300

    feat: updated stake for different coins

--- VultisigApp/VultisigApp/Features/Defi/DefiTHORChain/ViewModel/DefiTHORChainStakeViewModel.swift
+++ VultisigApp/VultisigApp/Features/Defi/DefiTHORChain/ViewModel/DefiTHORChainStakeViewModel.swift
@@ -9,35 +9,12 @@ import Foundation
 
 final class DefiTHORChainStakeViewModel: ObservableObject {
     @Published private(set) var vault: Vault
-    
-    // TODO: - Fetch from RPC
-    let stakePositions: [StakePosition] = [
-        StakePosition(
-            coin: TokensStore.TokenSelectionAssets.first(where: { $0.ticker == "TCY" }) ?? .example,
-            type: .stake,
-            amount: 500,
-            apr: 0.1,
-            estimatedReward: 200,
-            nextPayout: Date().timeIntervalSince1970 + 300,
-            rewards: 0,
-            rewardCoin: TokensStore.TokenSelectionAssets.first(where: { $0.ticker == "RUNE" }) ?? .example
-        ),
-        StakePosition(
-            coin: TokensStore.TokenSelectionAssets.first(where: { $0.ticker == "sTCY" }) ?? .example,
-            type: .stake,
-            amount: 500,
-            apr: 0.1,
-            estimatedReward: 200,
-            nextPayout: Date().timeIntervalSince1970 + 300,
-            rewards: 300,
-            rewardCoin: TokensStore.TokenSelectionAssets.first(where: { $0.ticker == "RUNE" }) ?? .example
-        )
-    ]
-    
+    @Published private(set) var stakePositions: [StakePosition] = []
+
     var hasStakePositions: Bool {
-        !vaultStakePositions.isEmpty
+        !stakePositions.isEmpty
     }
-    
+
     var vaultStakePositions: [CoinMeta] {
         vault.defiPositions.first { $0.chain == .thorChain }?.staking ?? []
     }
@@ -46,12 +23,95 @@ final class DefiTHORChainStakeViewModel: ObservableObject {
 
     init(vault: Vault) {
         self.vault = vault
+        loadStakePositions()
     }
 
     func update(vault: Vault) {
         self.vault = vault
+        loadStakePositions()
     }
 
     func refresh() async {
+        loadStakePositions()
+    }
+
+    private func loadStakePositions() {
+        stakePositions = vaultStakePositions.compactMap { coinMeta -> StakePosition? in
+            guard let coin = vault.coins.first(where: { $0.ticker == coinMeta.ticker && $0.chain == coinMeta.chain }) else {
+                return nil
+            }
+
+            return createStakePosition(for: coin, coinMeta: coinMeta)
+        }
+        .sorted { $0.amount > $1.amount }
+    }
+
+    private func createStakePosition(for coin: Coin, coinMeta: CoinMeta) -> StakePosition {
+        let ticker = coin.ticker.uppercased()
+
+        switch ticker {
+        case "TCY":
+            // TODO: Fetch APR from API
+            return StakePosition(
+                coin: coinMeta,
+                type: .stake,
+                amount: coin.stakedBalanceDecimal,
+                apr: nil, // TODO: Fill with actual APR
+                estimatedReward: nil, // TODO: Fill with actual estimated reward
+                nextPayout: nil, // TODO: Fill with actual next payout
+                rewards: nil, // TCY doesn't generate rewards
+                rewardCoin: nil
+            )
+
+        case "RUJI":
+            // TODO: Fetch APR from API
+            return StakePosition(
+                coin: coinMeta,
+                type: .stake,
+                amount: coin.stakedBalanceDecimal,
+                apr: nil, // TODO: Fill with actual APR
+                estimatedReward: nil,
+                nextPayout: nil,
+                rewards: nil, // RUJI doesn't generate rewards
+                rewardCoin: nil
+            )
+
+        case "YRUNE", "YTCY":
+            return StakePosition(
+                coin: coinMeta,
+                type: .index,
+                amount: coin.balanceDecimal,
+                apr: nil,
+                estimatedReward: nil,
+                nextPayout: nil,
+                rewards: nil,
+                rewardCoin: nil
+            )
+
+        case "STCY":
+            return StakePosition(
+                coin: coinMeta,
+                type: .compound,
+                amount: coin.balanceDecimal,
+                apr: nil,
+                estimatedReward: nil,
+                nextPayout: nil,
+                rewards: nil,
+                rewardCoin: nil
+            )
+
+        default:
+            // Default case for other stake positions
+            return StakePosition(
+                coin: coinMeta,
+                type: .stake,
+                amount: coin.stakedBalanceDecimal,
+                apr: nil,
+                estimatedReward: nil,
+                nextPayout: nil,
+                rewards: nil,
+                rewardCoin: nil
+            )
+        }
     }
 }
