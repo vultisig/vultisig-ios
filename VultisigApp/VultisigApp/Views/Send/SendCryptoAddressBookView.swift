25190eebadb89347693cd5bb2d2d7c28fb477b6c
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-10-30 16:36:37 -0300

    fix: removed print

--- VultisigApp/VultisigApp/Views/Send/SendCryptoAddressBookView.swift
+++ VultisigApp/VultisigApp/Views/Send/SendCryptoAddressBookView.swift
@@ -20,8 +20,7 @@ struct SendCryptoAddressBookView: View {
     @Query var savedAddresses: [AddressBookItem]
     
     var filteredSavedAddresses: [AddressBookItem] {
-        print("Coin is", coin.ticker, coin.chain)
-        return savedAddresses
+        savedAddresses
             .filter { (AddressBookChainType(coinMeta: $0.coinMeta) == AddressBookChainType(coinMeta: coin.toCoinMeta()) || $0.coinMeta.chain == coin.chain) }
     }
     
