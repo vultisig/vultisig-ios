9ec6d51050f0c6f6c4c40a6e4d33cc217678b3bf
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2025-11-18 12:18:00 -0300

    feat: removed functions moved to Defi Tab

--- VultisigApp/VultisigApp/Model/FunctionCall/FunctionCallInstance.swift
+++ VultisigApp/VultisigApp/Model/FunctionCall/FunctionCallInstance.swift
@@ -11,17 +11,13 @@ import SwiftUI
 import VultisigCommonData
 
 enum FunctionCallInstance {
-    case bond(FunctionCallBond)
     case rebond(FunctionCallReBond)
-    case unbond(FunctionCallUnbond)
     case bondMaya(FunctionCallBondMayaChain)
     case unbondMaya(FunctionCallUnbondMayaChain)
     case leave(FunctionCallLeave)
     case custom(FunctionCallCustom)
     case vote(FunctionCallVote)
     case stake(FunctionCallStake)
-    case stakeTcy(FunctionCallStakeTCY)
-    case unstakeTcy(FunctionCallUnstakeTCY)
     case unstake(FunctionCallUnstake)
     case addPool(FunctionCallAddLiquidityMaya)
     case removePool(FunctionCallRemoveLiquidityMaya)
@@ -29,25 +25,13 @@ enum FunctionCallInstance {
     case merge(FunctionCallCosmosMerge)
     case unmerge(FunctionCallCosmosUnmerge)
     case theSwitch(FunctionCallCosmosSwitch)
-    case mintYRune(FunctionCallCosmosYVault)
-    case mintYTCY(FunctionCallCosmosYVault)
-    case redeemRune(FunctionCallCosmosYVault)
-    case redeemTCY(FunctionCallCosmosYVault)
     case addThorLP(FunctionCallAddThorLP)
-    case removeThorLP(FunctionCallRemoveThorLP)
-    case stakeRuji(FunctionCallStakeRuji)
-    case unstakeRuji(FunctionCallUnstakeRuji)
-    case withdrawRujiRewards(FunctionCallWithdrawRujiRewards)
     case securedAsset(FunctionCallSecuredAsset)
     case withdrawSecuredAsset(FunctionCallWithdrawSecuredAsset)
     
     var view: AnyView {
         switch self {
-        case .bond(let memo):
-            return memo.getView()
         case .rebond(let memo):
-            return memo.getView()
-        case .unbond(let memo):
             return memo.getView()
         case .bondMaya(let memo):
             return memo.getView()
@@ -60,10 +44,6 @@ enum FunctionCallInstance {
         case .vote(let memo):
             return memo.getView()
         case .stake(let memo):
-            return memo.getView()
-        case .stakeTcy(let memo):
-            return memo.getView()
-        case .unstakeTcy(let memo):
             return memo.getView()
         case .unstake(let memo):
             return memo.getView()
@@ -79,23 +59,7 @@ enum FunctionCallInstance {
             return memo.getView()
         case .theSwitch(let memo):
             return memo.getView()
-        case .mintYRune(let memo):
-            return memo.getView()
-        case .mintYTCY(let memo):
-            return memo.getView()
-        case .redeemRune(let memo):
-            return memo.getView()
-        case .redeemTCY(let memo):
-            return memo.getView()
         case .addThorLP(let memo):
-            return memo.getView()
-        case .removeThorLP(let memo):
-            return memo.getView()
-        case .stakeRuji(let memo):
-            return memo.getView()
-        case .unstakeRuji(let memo):
-            return memo.getView()
-        case .withdrawRujiRewards(let memo):
             return memo.getView()
         case .securedAsset(let memo):
             return memo.getView()
@@ -106,11 +70,7 @@ enum FunctionCallInstance {
     
     var description: String {
         switch self {
-        case .bond(let memo):
-            return memo.description
         case .rebond(let memo):
-            return memo.description
-        case .unbond(let memo):
             return memo.description
         case .bondMaya(let memo):
             return memo.description
@@ -123,10 +83,6 @@ enum FunctionCallInstance {
         case .vote(let memo):
             return memo.description
         case .stake(let memo):
-            return memo.description
-        case .stakeTcy(let memo):
-            return memo.description
-        case .unstakeTcy(let memo):
             return memo.description
         case .unstake(let memo):
             return memo.description
@@ -142,23 +98,7 @@ enum FunctionCallInstance {
             return memo.description
         case .theSwitch(let memo):
             return memo.description
-        case .mintYRune(let memo):
-            return memo.description
-        case .mintYTCY(let memo):
-            return memo.description
-        case .redeemRune(let memo):
-            return memo.description
-        case .redeemTCY(let memo):
-            return memo.description
         case .addThorLP(let memo):
-            return memo.description
-        case .removeThorLP(let memo):
-            return memo.description
-        case .stakeRuji(let memo):
-            return memo.description
-        case .unstakeRuji(let memo):
-            return memo.description
-        case .withdrawRujiRewards(let memo):
             return memo.description
         case .securedAsset(let memo):
             return memo.description
@@ -169,12 +109,8 @@ enum FunctionCallInstance {
     
     var amount: Decimal {
         switch self {
-        case .bond(let memo):
-            return memo.amount
         case .rebond:
             return 0  // REBOND must send 0 RUNE in the transaction
-        case .unbond:
-            return .zero
         case .bondMaya(let memo):
             return memo.amount
         case .unbondMaya:
@@ -187,10 +123,6 @@ enum FunctionCallInstance {
             return .zero
         case .stake(let memo):
             return memo.amount
-        case .stakeTcy(let memo):
-            return memo.amount
-        case .unstakeTcy(_):
-            return .zero // The amount goes in the memo
         case .unstake(let memo):
             return memo.amount  // You must send 1 TON to unstake with a "w" memo
         case .addPool(let memo):
@@ -205,23 +137,7 @@ enum FunctionCallInstance {
             return memo.amount  // Now amount contains the shares as Decimal
         case .theSwitch(let memo):
             return memo.amount
-        case .mintYRune(let memo):
-            return memo.amount
-        case .mintYTCY(let memo):
-            return memo.amount
-        case .redeemRune(let memo):
-            return memo.amount
-        case .redeemTCY(let memo):
-            return memo.amount
         case .addThorLP(let memo):
-            return memo.amount
-        case .removeThorLP(let removeLP):
-            return removeLP.dustAmount // Use the dust amount from the instance
-        case .stakeRuji(let memo):
-            return memo.amount
-        case .unstakeRuji(_):
-            return .zero  // The amount goes in the memo
-        case .withdrawRujiRewards(let memo):
             return memo.amount
         case .securedAsset(let memo):
             return memo.amount
@@ -244,24 +160,10 @@ enum FunctionCallInstance {
             return memo.destinationAddress
         case .theSwitch(let memo):
             return memo.destinationAddress
-        case .mintYRune(let memo):
-            return memo.destinationAddress
-        case .mintYTCY(let memo):
-            return memo.destinationAddress
-        case .redeemRune(let memo):
-            return memo.destinationAddress
-        case .redeemTCY(let memo):
-            return memo.destinationAddress
         case .addThorLP(let memo):
             // For addThorLP, return the inbound address that was set by fetchInboundAddress()
             // This is essential for Bitcoin and other chains to know where to send funds
             return memo.tx.toAddress.isEmpty ? nil : memo.tx.toAddress
-        case .stakeRuji(let memo):
-            return memo.destinationAddress
-        case .unstakeRuji(let memo):
-            return memo.destinationAddress
-        case .withdrawRujiRewards(let memo):
-            return memo.destinationAddress
         case .securedAsset(let memo):
             // For secured assets (MINT), return the inbound address
             return memo.tx.toAddress.isEmpty ? nil : memo.tx.toAddress
@@ -274,11 +176,7 @@ enum FunctionCallInstance {
     
     func toDictionary() -> ThreadSafeDictionary<String, String> {
         switch self {
-        case .bond(let memo):
-            return memo.toDictionary()
         case .rebond(let memo):
-            return memo.toDictionary()
-        case .unbond(let memo):
             return memo.toDictionary()
         case .bondMaya(let memo):
             return memo.toDictionary()
@@ -291,10 +189,6 @@ enum FunctionCallInstance {
         case .vote(let memo):
             return memo.toDictionary()
         case .stake(let memo):
-            return memo.toDictionary()
-        case .stakeTcy(let memo):
-            return memo.toDictionary()
-        case .unstakeTcy(let memo):
             return memo.toDictionary()
         case .unstake(let memo):
             return memo.toDictionary()
@@ -310,23 +204,7 @@ enum FunctionCallInstance {
             return memo.toDictionary()
         case .theSwitch(let memo):
             return memo.toDictionary()
-        case .mintYRune(let memo):
-            return memo.toDictionary()
-        case .mintYTCY(let memo):
-            return memo.toDictionary()
-        case .redeemRune(let memo):
-            return memo.toDictionary()
-        case .redeemTCY(let memo):
-            return memo.toDictionary()
         case .addThorLP(let memo):
-            return memo.toDictionary()
-        case .removeThorLP(let memo):
-            return memo.toDictionary()
-        case .stakeRuji(let memo):
-            return memo.toDictionary()
-        case .unstakeRuji(let memo):
-            return memo.toDictionary()
-        case .withdrawRujiRewards(let memo):
             return memo.toDictionary()
         case .securedAsset(let memo):
             return memo.toDictionary()
@@ -345,14 +223,6 @@ enum FunctionCallInstance {
             return VSTransactionType.thorMerge
         case .unmerge(_):
             return VSTransactionType.thorUnmerge
-        case .mintYRune(_), .mintYTCY(_), .redeemRune(_), .redeemTCY(_):
-            return VSTransactionType.genericContract
-        case .stakeRuji, .unstakeRuji, .withdrawRujiRewards:
-            return VSTransactionType.genericContract
-        case .stakeTcy(let call):
-            return call.isAutoCompound ? VSTransactionType.genericContract : .unspecified
-        case .unstakeTcy(let call):
-            return call.isAutoCompound ? VSTransactionType.genericContract : .unspecified
         default:
             return .unspecified
         }
@@ -360,11 +230,7 @@ enum FunctionCallInstance {
     
     var isTheFormValid: Bool {
         switch self {
-        case .bond(let memo):
-            return memo.isTheFormValid
         case .rebond(let memo):
-            return memo.isTheFormValid
-        case .unbond(let memo):
             return memo.isTheFormValid
         case .bondMaya(let memo):
             return memo.isTheFormValid
@@ -377,10 +243,6 @@ enum FunctionCallInstance {
         case .vote(let memo):
             return memo.isTheFormValid
         case .stake(let memo):
-            return memo.isTheFormValid
-        case .stakeTcy(let memo):
-            return memo.isTheFormValid
-        case .unstakeTcy(let memo):
             return memo.isTheFormValid
         case .unstake(let memo):
             return memo.isTheFormValid
@@ -396,23 +258,7 @@ enum FunctionCallInstance {
             return memo.isTheFormValid
         case .theSwitch(let memo):
             return memo.isTheFormValid
-        case .mintYRune(let memo):
-            return memo.isTheFormValid
-        case .mintYTCY(let memo):
-            return memo.isTheFormValid
-        case .redeemRune(let memo):
-            return memo.isTheFormValid
-        case .redeemTCY(let memo):
-            return memo.isTheFormValid
         case .addThorLP(let memo):
-            return memo.isTheFormValid
-        case .removeThorLP(let memo):
-            return memo.isTheFormValid
-        case .stakeRuji(let memo):
-            return memo.isTheFormValid
-        case .unstakeRuji(let memo):
-            return memo.isTheFormValid
-        case .withdrawRujiRewards(let memo):
             return memo.isTheFormValid
         case .securedAsset(let memo):
             return memo.isTheFormValid
@@ -426,18 +272,6 @@ enum FunctionCallInstance {
         case .rebond(let memo):
             return memo.customErrorMessage
         case .addThorLP(let memo):
-            return memo.customErrorMessage
-        case .stakeTcy(let memo):
-            return memo.customErrorMessage
-        case .stakeRuji(let memo):
-            return memo.customErrorMessage
-        case .mintYRune(let memo):
-            return memo.customErrorMessage
-        case .mintYTCY(let memo):
-            return memo.customErrorMessage
-        case .redeemRune(let memo):
-            return memo.customErrorMessage
-        case .redeemTCY(let memo):
             return memo.customErrorMessage
         case .securedAsset(let memo):
             return memo.customErrorMessage
@@ -454,7 +288,7 @@ enum FunctionCallInstance {
             if coin.ticker.uppercased() == "TCY" {
                 return .custom(FunctionCallCustom(tx: tx, vault: vault))
             }
-            return .bond(FunctionCallBond(tx: tx, vault: vault))
+            return .rebond(FunctionCallReBond(tx: tx, vault: vault))
         case .mayaChain:
             return .bondMaya(FunctionCallBondMayaChain(assets: nil))
         case .dydx:
@@ -474,24 +308,6 @@ enum FunctionCallInstance {
     
     var wasmContractPayload: WasmExecuteContractPayload? {
         switch self {
-        case .stakeRuji(let call):
-            return call.wasmContractPayload
-        case .unstakeRuji(let call):
-            return call.wasmContractPayload
-        case .withdrawRujiRewards(let call):
-            return call.wasmContractPayload
-        case .stakeTcy(let call):
-            return call.wasmContractPayload
-        case .unstakeTcy(let call):
-            return call.wasmContractPayload
-        case .mintYRune(let call):
-            return call.wasmContractPayload
-        case .mintYTCY(let call):
-            return call.wasmContractPayload
-        case .redeemRune(let call):
-            return call.wasmContractPayload
-        case .redeemTCY(let call):
-            return call.wasmContractPayload
         case .securedAsset(_):
             return nil // Secured assets don't use WASM contracts
         case .withdrawSecuredAsset(_):
