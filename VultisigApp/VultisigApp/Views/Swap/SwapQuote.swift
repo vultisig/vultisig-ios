2141bc19c099479fcee9228014b12675cf9d382d
Johnny Luo <johnnyluo1980@gmail.com>
2026-01-21 16:36:31 +1100

    fine tune the swiftlint rules

--- VultisigApp/VultisigApp/Views/Swap/SwapQuote.swift
+++ VultisigApp/VultisigApp/Views/Swap/SwapQuote.swift
@@ -8,7 +8,7 @@
 import Foundation
 import BigInt
 
-enum SwapQuote {
+enum SwapQuote: Hashable {
     case thorchain(ThorchainSwapQuote)
     case thorchainStagenet(ThorchainSwapQuote)
     case mayachain(ThorchainSwapQuote)
@@ -100,20 +100,25 @@ enum SwapQuote {
             return nil
         }
     }
-    var hashValue: Int {
+
+    func hash(into hasher: inout Hasher) {
         switch self {
         case .thorchain(let quote):
-            return quote.hashValue
+            hasher.combine(quote)
         case .thorchainStagenet(let quote):
-            return quote.hashValue
+            hasher.combine(quote)
         case .mayachain(let quote):
-            return quote.hashValue
+            hasher.combine(quote)
         case .oneinch(let quote, let fee):
-            return quote.hashValue ^ fee.hashValue
+            hasher.combine(quote)
+            hasher.combine(fee)
         case .kyberswap(let quote, let fee):
-            return quote.hashValue ^ fee.hashValue
-        case .lifi(let quote, let fee, _):
-            return quote.hashValue ^ fee.hashValue
+            hasher.combine(quote)
+            hasher.combine(fee)
+        case .lifi(let quote, let fee, let integratorFee):
+            hasher.combine(quote)
+            hasher.combine(fee)
+            hasher.combine(integratorFee)
         }
     }
 }
