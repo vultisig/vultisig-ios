d6b5c0c550df26aebf254e3564dba82e70881f33
Enrique <enriquesouza@live.com>
2026-02-03 21:19:44 -0300

    style: reformat SwapCryptoVerifyViewModel and SwapCryptoViewModel files.

--- VultisigApp/VultisigApp/View Models/SwapCryptoVerifyViewModel.swift
+++ VultisigApp/VultisigApp/View Models/SwapCryptoVerifyViewModel.swift
@@ -10,19 +10,19 @@ import SwiftUI
 @MainActor
 class SwapCryptoVerifyViewModel: ObservableObject {
     let securityScanViewModel = SecurityScannerViewModel()
-
+    
     @Published var isAmountCorrect = false
     @Published var isFeeCorrect = false
     @Published var isApproveCorrect = false
-
+    
     @Published var showSecurityScannerSheet: Bool = false
     @Published var securityScannerState: SecurityScannerState = .idle
-
+    
     func onLoad() {
         securityScanViewModel.$state
             .assign(to: &$securityScannerState)
     }
-
+    
     func isValidForm(shouldApprove: Bool) -> Bool {
         if shouldApprove {
             return isAmountCorrect && isFeeCorrect && isApproveCorrect
@@ -30,11 +30,11 @@ class SwapCryptoVerifyViewModel: ObservableObject {
             return isAmountCorrect && isFeeCorrect
         }
     }
-
+    
     func scan(transaction: SwapTransaction) async {
         await securityScanViewModel.scan(transaction: transaction)
     }
-
+    
     func validateSecurityScanner() -> Bool {
         showSecurityScannerSheet = securityScannerState.shouldShowWarning
         return !securityScannerState.shouldShowWarning
