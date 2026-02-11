f6b540e2252791b05b4b8bb3d021cd6b0da3feff
Gaston Mazzeo <7192634+gastonm5@users.noreply.github.com>
2026-02-11 13:47:35 -0300

    feat: updated server backup verification screen

--- VultisigApp/VultisigApp/Views/Vault/FastBackupVaultOverview.swift
+++ VultisigApp/VultisigApp/Views/Vault/FastBackupVaultOverview.swift
@@ -29,7 +29,7 @@ struct FastBackupVaultOverview: View {
             container
         }
         .crossPlatformSheet(isPresented: $isVerificationLinkActive) {
-            ServerBackupVerificationView(
+            ServerBackupVerificationScreen(
                 tssType: tssType,
                 vault: vault,
                 email: email,
